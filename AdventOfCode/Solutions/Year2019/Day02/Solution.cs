b3d760d8908c50b251ab51466a9ffd5a137bf7e8
Drew <vsts@internal.cloudapp.net>
2020-11-23 15:42:16 -0600

    Implemeted solution for 2019 Day 02

--- AdventOfCode/Solutions/Year2019/Day02/Solution.cs
+++ AdventOfCode/Solutions/Year2019/Day02/Solution.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+using AdventOfCode.Solutions.Year2019;
+
 namespace AdventOfCode.Solutions.Year2019
 {
 
@@ -15,12 +17,38 @@ namespace AdventOfCode.Solutions.Year2019
 
         protected override string SolvePartOne()
         {
-            return null;
+            long[] input = Input.ToLongArray(",");
+
+            input[1] = 12;
+            input[2] = 2;
+
+            //input = new long[] {1,0,0,0,99};
+            //input = new long[] {2,3,0,3,99};
+            //input = new long[] {2,4,4,5,99,0};
+            //input = new long[] {1,1,1,4,99,5,6,0,99};
+
+            Intcode intcode = new Intcode(input, 0);
+            intcode.Run();
+            
+            return intcode.memory[0].ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            long[] input = Input.ToLongArray(",");
+
+            input[1] = 57;
+            input[2] = 41;
+
+            //input = new long[] {1,0,0,0,99};
+            //input = new long[] {2,3,0,3,99};
+            //input = new long[] {2,4,4,5,99,0};
+            //input = new long[] {1,1,1,4,99,5,6,0,99};
+
+            Intcode intcode = new Intcode(input, 0);
+            intcode.Run();
+            
+            return intcode.memory[0].ToString();
         }
     }
 }
