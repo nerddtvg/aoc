e013910f9103bb7fb44a5e255222a5d2153e5062
Drew <vsts@internal.cloudapp.net>
2020-11-23 14:42:34 -0600

    Testing Day01 with the provided solution

--- AdventOfCode/Solutions/Year2019/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2019/Day01/Solution.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+using System.Linq;
+
 namespace AdventOfCode.Solutions.Year2019
 {
 
@@ -13,14 +15,16 @@ namespace AdventOfCode.Solutions.Year2019
 
         }
 
-        protected override string SolvePartOne()
-        {
-            return null;
-        }
+        protected override string SolvePartOne() => Input.ToIntArray("\n").Select(Fuel).Sum().ToString();
 
-        protected override string SolvePartTwo()
+        protected override string SolvePartTwo() => Input.ToIntArray("\n").Select(FuelFuel).Sum().ToString();
+
+        int Fuel(int module) => module / 3 - 2;
+
+        int FuelFuel(int module)
         {
-            return null;
+            int fuel = Fuel(module);
+            return fuel <= 0 ? 0 : fuel + FuelFuel(fuel);
         }
     }
 }
