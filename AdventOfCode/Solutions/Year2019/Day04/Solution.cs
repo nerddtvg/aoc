1f9fa272c560578478af9550c1ea8da602ca0849
Drew <vsts@internal.cloudapp.net>
2020-11-23 16:27:12 -0600

    Day 4 reimplemented

--- AdventOfCode/Solutions/Year2019/Day04/Solution.cs
+++ AdventOfCode/Solutions/Year2019/Day04/Solution.cs
@@ -2,25 +2,46 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+// Source: https://www.reddit.com/r/adventofcode/comments/e5u5fv/2019_day_4_solutions/f9mpyrj/
+using System.Linq;
+
 namespace AdventOfCode.Solutions.Year2019
 {
 
     class Day04 : ASolution
     {
+        private int start;
+        private int end;
 
         public Day04() : base(04, 2019, "")
         {
-
+            start = int.Parse(Input.Split('-')[0]);
+            end = int.Parse(Input.Split('-')[1]);
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            // part 1
+            int part1 = Enumerable.Range(start, end - start)
+                .Select(p => p.ToString())
+                .Where(p => string.Join("", p.OrderBy(d => d)) == p // ascending order
+                            && p.Distinct().Count() < p.Length)		// at least one duplicate
+                .Count();
+            
+            return string.Format("{0:N}", part1);
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+                
+            // part 2
+            int part2 = Enumerable.Range(start, end - start)
+                .Select(p => p.ToString())
+                .Where(p => string.Join("", p.OrderBy(d => d)) == p    // ascending order
+                            && p.GroupBy(d=>d).Any(g=>g.Count() == 2)) // two digit group
+                .Count();
+            
+            return string.Format("{0:N}", part2);
         }
     }
 }
