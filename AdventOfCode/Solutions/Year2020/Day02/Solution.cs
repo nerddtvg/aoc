f4a81c4c8705be4e36bcdd7549cd4c36003afc7b
Drew <vsts@internal.cloudapp.net>
2020-12-01 23:06:47 -0600

    2020 Day 2 Part 1

--- AdventOfCode/Solutions/Year2020/Day02/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day02/Solution.cs
@@ -6,18 +6,42 @@ using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2020
 {
+    class Password {
+        public int min {get;set;}
+        public int max {get;set;}
+        public char c {get;set;}
+        public string password {get;set;}
+
+        public Password(string line) {
+            string[] a = line.Split(" ");
+
+            this.min = Convert.ToInt32(a[0].Split("-")[0]);
+            this.max = Convert.ToInt32(a[0].Split("-")[1]);
+
+            this.c = a[1].ToCharArray()[0];
+
+            this.password = a[2];
+        }
+
+        public bool IsValid() {
+            int reqCharCount = password.ToCharArray().Where(a => a == this.c).Count();
+
+            return (this.min <= reqCharCount && reqCharCount <= this.max);
+        }
+    }
 
     class Day02 : ASolution
     {
+        List<Password> passwords = new List<Password>();
 
         public Day02() : base(02, 2020, "")
         {
-
+            Input.SplitByNewline().ToList().ForEach(a => passwords.Add(new Password(a)));
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            return passwords.Count(a => a.IsValid()).ToString();
         }
 
         protected override string SolvePartTwo()
