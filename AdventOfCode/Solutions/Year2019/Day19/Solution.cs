090aa1c77432bc4628122020d6bab3778f7118a2
Drew <vsts@internal.cloudapp.net>
2020-12-01 22:41:58 -0600

    2019 Day 19 Part 1

--- AdventOfCode/Solutions/Year2019/Day19/Solution.cs
+++ AdventOfCode/Solutions/Year2019/Day19/Solution.cs
@@ -7,21 +7,46 @@ using System.Linq;
 namespace AdventOfCode.Solutions.Year2019
 {
 
+    class BeamTile {
+        public int x {get;set;}
+        public int y {get;set;}
+        public int state {get;set;}
+    }
+
     class Day19 : ASolution
     {
+        List<BeamTile> map = new List<BeamTile>();
 
         public Day19() : base(19, 2019, "")
         {
+            // Need to find Santa's ship (100x100 square inside the beam)
+            for(int y=0; y<100; y++) {
+                for(int x=0; x<100; x++) {
+                    Intcode intcode = new Intcode(Input, 2);
+                    intcode.SetInput(x);
+                    intcode.SetInput(y);
+                    intcode.Run();
 
+                    // Gather the output
+                    map.Add(new BeamTile() { x = x, y = y, state = Convert.ToInt32(intcode.output_register) });
+                }
+            }
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            // How many are in state == 1
+            return map.Count(a => a.x < 50 && a.y < 50 && a.state == 1).ToString();
         }
 
         protected override string SolvePartTwo()
         {
+            // Find the furthest right point in the tractor beam
+            BeamTile fr = map.Where(a => a.state == 1).OrderBy(a => a.x).First();
+            
+            // Find the furthest left/bottom point in the tractor beam
+            BeamTile fl = map.Where(a => a.state == 1).OrderBy(a => a.y).ThenByDescending(a => a.x).First();
+
             return null;
         }
     }
