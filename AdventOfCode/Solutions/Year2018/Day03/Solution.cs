431cb365f4eb6023e88855754d615a43ed58e29d
Drew <vsts@internal.cloudapp.net>
2020-12-03 09:50:54 -0600

    2018 Day 3 Part 1

--- AdventOfCode/Solutions/Year2018/Day03/Solution.cs
+++ AdventOfCode/Solutions/Year2018/Day03/Solution.cs
@@ -16,9 +16,16 @@ namespace AdventOfCode.Solutions.Year2018
         public int id {get;set;}
     }
 
+    class FabricTile {
+        public int x {get; set;}
+        public int y {get; set;}
+        public int count {get; set;}
+    }
+
     class Day03 : ASolution
     {
         List<SantaFabric> claims = new List<SantaFabric>();
+        List<FabricTile> tiles = new List<FabricTile>();
 
         public Day03() : base(03, 2018, "")
         {
@@ -50,21 +57,80 @@ namespace AdventOfCode.Solutions.Year2018
                 claim.h = Int32.Parse(wh[1]);
 
                 // Claim end points
-                claim.x2 = claim.x + claim.w;
-                claim.y2 = claim.y + claim.h;
+                // Reduce the w,h by 1 so we don't go into the next square
+                claim.x2 = claim.x + claim.w - 1;
+                claim.y2 = claim.y + claim.h - 1;
 
                 claims.Add(claim);
             });
+            
+            // Now we need to count our tiles
+            int minX = claims.Min(a => a.x);
+            int minY = claims.Min(a => a.y);
+            int maxX = claims.Max(a => a.x2);
+            int maxY = claims.Max(a => a.y2);
+
+            // Allows us to draw the overlaps for a nice visual
+            bool draw = false;
+
+            Console.WriteLine($"Min X: {minX}");
+            Console.WriteLine($"Min Y: {minY}");
+
+            for(int y=minY; y<=maxY; y++) {
+                if (draw)
+                    Console.Write(y.ToString("0000") + ": ");
+                
+                for(int x=minX; x<=maxX; x++) {
+                    var tile = new FabricTile() {
+                        x = x,
+                        y = y,
+                        count = claims.Count(a =>
+                            a.x <= x
+                            &&
+                            a.y <= y
+                            &&
+                            x <= a.x2
+                            &&
+                            y <= a.y2
+                        )
+                    };
+
+                    tiles.Add(tile);
+
+                    // Draw this if desired
+                    if (draw)
+                        Console.Write(tile.count > 1 ? "#" : (tile.count == 1 ? "O" : "."));
+                }
+
+                if (draw)
+                    Console.WriteLine();
+            }
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            return tiles.Count(a => a.count > 1).ToString();;
         }
 
         protected override string SolvePartTwo()
         {
             return null;
+        }
+
+        private bool TilesOverlap(SantaFabric claim1, SantaFabric claim2) {
+            // claim1 is too far left to overlap
+            if (claim1.x2 < claim2.x) return false;
+
+            // claim1 is too far right to overlap
+            if (claim1.x > claim2.x2) return false;
+
+            // claim1 is too far high to overlap
+            if (claim1.y2 < claim2.y) return false;
+
+            // claim1 is too far low to overlap
+            if (claim1.y > claim2.y2) return false;
+
+            return true;
         }
     }
 }
