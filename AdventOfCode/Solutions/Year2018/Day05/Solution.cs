70776c4c14109d5d6e8a74a0b603eaab89454031
Drew <vsts@internal.cloudapp.net>
2020-12-03 16:03:09 -0600

    2018 Day 5 Part 1

--- AdventOfCode/Solutions/Year2018/Day05/Solution.cs
+++ AdventOfCode/Solutions/Year2018/Day05/Solution.cs
@@ -17,12 +17,50 @@ namespace AdventOfCode.Solutions.Year2018
 
         protected override string SolvePartOne()
         {
-            return null;
+            return RunPolymer(Input).Length.ToString();
         }
 
         protected override string SolvePartTwo()
         {
             return null;
+        }
+
+        protected string RunPolymer(string input) {
+            // Any adjoining positions of opposite polarity (upper versus lower-case) remove each other
+            // Loop this until no more can be done
+            List<char> inputArr = input.ToCharArray().ToList();
+
+            while(true) {
+                // Track if we have removed anything
+                bool removed = false;
+                int i;
+                List<int> removePos = new List<int>();
+                
+                for(i=0; i<inputArr.Count-1; i++) {
+                    // Check this letter and the next one
+                    // Integers are 65-90 [A-Z] and 97-122 [a-z]
+                    if (Math.Abs((int) inputArr[i] - (int) inputArr[i+1]) == 32) {
+                        removed = true;
+                        
+                        // Add this to the list of those to remove
+                        removePos.Add(i);
+                        removePos.Add(i+1);
+
+                        // Skip ahead
+                        i = i + 1;
+                    }
+                }
+
+                if (removed) {
+                    // Remove i and i+1
+                    removePos.Reverse();
+                    removePos.ForEach(a => inputArr.RemoveAt(a));
+                } else {
+                    break;
+                }
+            }
+
+            return string.Join("", inputArr);
         }
     }
 }
