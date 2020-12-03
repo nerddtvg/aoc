f87f0ecdfbeb6f9af76972c248777e96ceb19d7c
Drew <vsts@internal.cloudapp.net>
2020-12-03 06:23:01 -0600

    2020 Day 2 Part 2

--- AdventOfCode/Solutions/Year2020/Day03/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day03/Solution.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 
 using System.Linq;
+using System.Numerics;
 
 namespace AdventOfCode.Solutions.Year2020
 {
@@ -36,7 +37,33 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartTwo()
         {
-            return null;
+            int[] x = new int[] { 0, 0, 0, 0, 0 };
+            int[] mx = new int[] { 1, 3, 5, 7, 1 };
+            int[] my = new int[] { 1, 1, 1, 1, 2 };
+
+            BigInteger[] trees = new BigInteger[] { 0, 0, 0, 0, 0 };
+
+            int lc = 0;
+
+            foreach(string line in Input.SplitByNewline()) {
+                for(int i=0; i<x.Length; i++) {
+                    // Do we check this line?
+                    if (lc % my[i] != 0) continue;
+
+                    // Reset x if we are past the length
+                    if (x[i] >= line.Length) x[i] -= line.Length;
+
+                    // Is this a tree?
+                    if (line.Substring(x[i], 1) == "#") trees[i]++;
+
+                    // Increment
+                    x[i] += mx[i];
+                }
+
+                lc++;
+            }
+
+            return trees.Aggregate((x, y) => x * y).ToString();
         }
     }
 }
