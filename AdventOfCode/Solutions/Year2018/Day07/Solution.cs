a048e3d47317e271cf62d1440c74ccd542c65017
Drew <vsts@internal.cloudapp.net>
2020-12-04 12:34:55 -0600

    2018 Day7 Part 2

--- AdventOfCode/Solutions/Year2018/Day07/Solution.cs
+++ AdventOfCode/Solutions/Year2018/Day07/Solution.cs
@@ -12,6 +12,26 @@ namespace AdventOfCode.Solutions.Year2018
         public List<string> prereq {get;set;}
         public bool completed {get;set;}
         public bool start {get;set;}
+
+        public int timeRequried {
+            get {
+                // Add 60 + 1 to account for the shift of 'A'
+                return 61 + ((int) (((int) id.ToCharArray()[0]) - ((int) 'A')));
+            }
+        }
+    }
+
+    class Worker {
+        public string id {get;set;}
+        public int tick {get;set;}
+        public int timeRequired {get;set;}
+
+        public bool IsIdle() => string.IsNullOrEmpty(this.id);
+
+        public override string ToString()
+        {
+            return $"{id}: {tick} / {timeRequired}";
+        }
     }
 
     class Day07 : ASolution
@@ -20,7 +40,7 @@ namespace AdventOfCode.Solutions.Year2018
 
         public Day07() : base(07, 2018, "")
         {
-            ParseInput();
+
         }
 
         private void ParseInput() {
@@ -73,6 +93,7 @@ namespace AdventOfCode.Solutions.Year2018
 
         protected override string SolvePartOne()
         {
+            ParseInput();
             string order = "";
 
             // Go through and "process" the order
@@ -101,9 +122,87 @@ namespace AdventOfCode.Solutions.Year2018
             return order;
         }
 
+        private List<string> GetNextAvailable() =>
+            steps.Where(a => a.completed == false && a.prereq.Count == 0).OrderBy(a => a.id).Select(a => a.id).ToList();
+
         protected override string SolvePartTwo()
         {
-            return null;
+            ParseInput();
+
+            // Need Queues for the workers
+            // id of step it is on
+            // tick is the number of seconds they've been working on it
+            // Setup with 5 workers
+            var workers = new List<Worker>() {
+                new Worker() { id = string.Empty, tick = 0 },
+                new Worker() { id = string.Empty, tick = 0 },
+                new Worker() { id = string.Empty, tick = 0 },
+                new Worker() { id = string.Empty, tick = 0 },
+                new Worker() { id = string.Empty, tick = 0 }
+            };
+
+            bool start = true;
+
+            // Offset because our first step is to increment this
+            int seconds = -1;
+
+            while(start || workers.Count(a => !a.IsIdle()) > 0) {
+                // Tick, tick, tick!
+                start = false;
+                List<string> completed = new List<string>();
+                seconds++;
+
+                // Increment the worker times
+                workers.ForEach(worker => {
+                    if (!worker.IsIdle()) {
+                        worker.tick++;
+
+                        // Are they done?!
+                        if (worker.tick == steps.Where(a => a.id == worker.id).First().timeRequried) {
+                            completed.Add(worker.id);
+
+                            // Reset this worker
+                            worker.id = string.Empty;
+                            worker.tick = 0;
+                        }
+                    }
+                });
+
+                // Go through what was completed and get things ready
+                completed.ForEach(complete => {
+                    // Find all of the steps this "completes"
+                    steps.Where(a => a.id == complete).ToList().ForEach(a => a.completed = true);
+
+                    // Go through and update the prereqs
+                    steps.ForEach(a => {
+                        if (a.prereq.Contains(complete))
+                            a.prereq.Remove(complete);
+                    });
+                });
+                
+                // Get a list of what is available to work on (or being worked on)
+                List<string> available = GetNextAvailable();
+
+                // Find any idle workers and MAKE THEM WORK
+                // Need to do this through a manual loop because Linq ForEach didn't update variables
+                int i = 0;
+                workers.Where(worker => worker.IsIdle()).ToList().ForEach(worker => {
+                    // Get a current list of what's in progress (refresh each loop to account for anything new)
+                    List<string> inProgress = workers.Where(worker => !string.IsNullOrEmpty(worker.id)).Select(a => a.id).ToList();
+
+                    // Find the first one in the list that is not being worked on
+                    worker.id = available.FirstOrDefault(a => !inProgress.Contains(a));
+
+                    // This is only set to help debugging
+                    if (!string.IsNullOrEmpty(worker.id))
+                        worker.timeRequired = steps.Where(a => a.id == worker.id).First().timeRequried;
+                });
+
+                // Check if we have done everything
+                if (available.Count == 0 && workers.Count(a => a.IsIdle()) == workers.Count) break;
+            }
+
+            return seconds.ToString();
         }
     }
 }
