f34aaacd43d245d7f93031e9eb35831418bfa81f
Drew <vsts@internal.cloudapp.net>
2020-12-04 09:54:41 -0600

    Added support for not trimming new lines

--- AdventOfCode/Solutions/Utilities.cs
+++ AdventOfCode/Solutions/Utilities.cs
@@ -162,11 +162,11 @@ namespace AdventOfCode.Solutions
             return string.Join("", items);
         }
 
-        public static string[] SplitByNewline(this string input, bool shouldTrim = false)
+        public static string[] SplitByNewline(this string input, bool shouldTrim = false, bool shouldExcludeEmpty = true)
         {
             return input
                 .Split(new[] { "\r", "\n", "\r\n" }, StringSplitOptions.None)
-                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Where(s => (!shouldExcludeEmpty || !string.IsNullOrWhiteSpace(s)))
                 .Select(s => shouldTrim ? s.Trim() : s)
                 .ToArray();
         }
