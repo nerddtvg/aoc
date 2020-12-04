c2e3f6602d892c128eb5de4ecec492a941d46c02
Drew <vsts@internal.cloudapp.net>
2020-12-04 11:06:21 -0600

    2020 Day 4 Part 2

--- AdventOfCode/Solutions/Year2020/Day04/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day04/Solution.cs
@@ -4,6 +4,8 @@ using System.Text;
 
 using System.Linq;
 
+using System.Text.RegularExpressions;
+
 #nullable enable
 
 namespace AdventOfCode.Solutions.Year2020
@@ -21,6 +23,73 @@ namespace AdventOfCode.Solutions.Year2020
         public bool IsValidSet() =>
             // We don't care if ONLY cid is missing
             byr != null && iyr != null && eyr != null && hgt != null && hcl != null && ecl != null && pid != null;
+        
+        public bool IsValidPart2() {
+            /*
+            byr (Birth Year) - four digits; at least 1920 and at most 2002.
+            iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+            eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+            hgt (Height) - a number followed by either cm or in:
+                If cm, the number must be at least 150 and at most 193.
+                If in, the number must be at least 59 and at most 76.
+            hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+            ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+            pid (Passport ID) - a nine-digit number, including leading zeroes.
+            cid (Country ID) - ignored, missing or not.
+            */
+
+            if (byr == null || byr.Length != 4)
+                return false;
+            try {
+                int year = Int32.Parse(byr);
+                if (year < 1920 || 2002 < year)
+                    return false;
+            } catch { return false; }
+
+            if (iyr == null || iyr.Length != 4)
+                return false;
+            try {
+                int year = Int32.Parse(iyr);
+                if (year < 2010 || 2020 < year)
+                    return false;
+            } catch { return false; }
+
+            if (eyr == null || eyr.Length != 4)
+                return false;
+            try {
+                int year = Int32.Parse(eyr);
+                if (year < 2020 || 2030 < year)
+                    return false;
+            } catch { return false; }
+
+            if (hgt == null || (hgt.Substring(hgt.Length-2) != "cm" && hgt.Substring(hgt.Length-2) != "in"))
+                return false;
+            try {
+                int height = Int32.Parse(hgt.Substring(0, hgt.Length-2));
+                switch(hgt.Substring(hgt.Length-2)) {
+                    case "cm":
+                        if (height < 150 || 193 < height)
+                            return false;
+                        break;
+                        
+                    case "in":
+                        if (height < 59 || 76 < height)
+                            return false;
+                        break;
+                }
+            } catch { return false; }
+
+            if (hcl == null || !(new Regex("^#[a-fA-F0-9]{6}$")).IsMatch(hcl))
+                return false;
+
+            if (ecl == null || !(new string[] {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}).Contains(ecl))
+                return false;
+
+            if (pid == null || !(new Regex("^[0-9]{9}$")).IsMatch(pid))
+                return false;
+
+            return true;
+        }
     }
 
     class Day04 : ASolution
@@ -72,7 +141,7 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartTwo()
         {
-            return null;
+            return passports.Count(a => a.IsValidPart2()).ToString();
         }
     }
 }
