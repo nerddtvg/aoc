fda16db10f4512dd0369c7dd843712c32ba702d2
Drew <vsts@internal.cloudapp.net>
2020-12-04 23:18:39 -0600

    2020 Day 5 Part 1

--- AdventOfCode/Solutions/Year2020/Day05/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day05/Solution.cs
@@ -6,18 +6,82 @@ using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2020
 {
+    class BoardingPass {
+        public string value {get;set;}
+
+        public BoardingPass(string input) {
+            this.value = input;
+        }
+
+        public int Row {
+            get {
+                // Return value
+                int row = 0;
+
+                // Power of 2
+                int step = 6;
+
+                foreach(char c in value.Substring(0, 7).ToCharArray()) {
+                    // Move to the back half by adding the power of 2 required
+                    if (c == 'B')
+                        row += (int) Math.Pow(2, step);
+                    
+                    step--;
+                }
+
+                return row;
+            }
+        }
+
+        public int Column {
+            get {
+                // Return value
+                int col = 0;
+
+                // Power of 2
+                int step = 2;
+
+                foreach(char c in value.Substring(7, 3).ToCharArray()) {
+                    // Move to the back half by adding the power of 2 required
+                    if (c == 'R')
+                        col += (int) Math.Pow(2, step);
+                    
+                    step--;
+                }
+
+                return col;
+            }
+        }
+
+        public int seatId {
+            get {
+                return (Row * 8) + Column;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{value}: row {Row}, column {Column}, seat ID {seatId}";
+        }
+    }
 
     class Day05 : ASolution
     {
+        List<BoardingPass> passes = new List<BoardingPass>();
 
         public Day05() : base(05, 2020, "")
         {
+            Console.WriteLine((new BoardingPass("BFFFBBFRRR")).ToString());
+            Console.WriteLine((new BoardingPass("FFFBBBFRRR")).ToString());
+            Console.WriteLine((new BoardingPass("BBFFBBFRLL")).ToString());
 
+            foreach(string line in Input.SplitByNewline(true))
+                passes.Add(new BoardingPass(line));
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            return passes.Max(a => a.seatId).ToString();
         }
 
         protected override string SolvePartTwo()
