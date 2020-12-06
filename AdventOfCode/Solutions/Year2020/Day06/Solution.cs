8203873ea9717f1533eacf650656793d7d2bdc8c
Drew <vsts@internal.cloudapp.net>
2020-12-06 10:38:48 -0600

    2020 Day 6 Part 1

--- AdventOfCode/Solutions/Year2020/Day06/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day06/Solution.cs
@@ -9,15 +9,35 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day06 : ASolution
     {
+        List<string> groups = new List<string>();
 
         public Day06() : base(06, 2020, "")
         {
+            // Go through each group and combine them
+            List<string> group = new List<string>();
 
+            foreach(string line in Input.SplitByNewline(true, false)) {
+                // If the line is blank, process the group
+                if (string.IsNullOrWhiteSpace(line)) {
+                    if (group.Count == 0) continue;
+
+                    processGroup(group);
+                    group = new List<string>();;
+                }
+
+                group.Add(line);
+            }
+
+            if (group.Count > 0) processGroup(group);
+        }
+
+        private void processGroup(List<string> group) {
+            groups.Add(string.Join("", group.SelectMany(b => b.ToCharArray()).OrderBy(a => a).Distinct()));
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            return groups.Sum(a => a.Length).ToString();
         }
 
         protected override string SolvePartTwo()
