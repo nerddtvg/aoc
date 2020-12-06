6794c786a2743fa54821365afe012dd32e672565
Drew <vsts@internal.cloudapp.net>
2020-12-06 10:55:44 -0600

    2020 Day 6 Part 2

--- AdventOfCode/Solutions/Year2020/Day06/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day06/Solution.cs
@@ -10,6 +10,7 @@ namespace AdventOfCode.Solutions.Year2020
     class Day06 : ASolution
     {
         List<string> groups = new List<string>();
+        List<int> groupsCount = new List<int>();
 
         public Day06() : base(06, 2020, "")
         {
@@ -22,7 +23,8 @@ namespace AdventOfCode.Solutions.Year2020
                     if (group.Count == 0) continue;
 
                     processGroup(group);
-                    group = new List<string>();;
+                    group = new List<string>();
+                    continue;
                 }
 
                 group.Add(line);
@@ -32,7 +34,27 @@ namespace AdventOfCode.Solutions.Year2020
         }
 
         private void processGroup(List<string> group) {
-            groups.Add(string.Join("", group.SelectMany(b => b.ToCharArray()).OrderBy(a => a).Distinct()));
+            string g = string.Join("", group.SelectMany(b => b.ToCharArray()).OrderBy(a => a).Distinct());
+            groups.Add(g);
+
+            // We need to search everything in List<> to see how many of g are included
+            int gCount = 0;
+
+            foreach(string c in g.ToCharArray().Select(a => a.ToString())) {
+                bool found = true;
+
+                foreach(string person in group) {
+                    if (!person.Contains(c)) {
+                        // Someone didn't have it, don't count it
+                        found = false;
+                        break;
+                    }
+                }
+
+                if (found) gCount++;
+            }
+
+            groupsCount.Add(gCount);
         }
 
         protected override string SolvePartOne()
@@ -42,7 +64,7 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartTwo()
         {
-            return null;
+            return groupsCount.Sum(a => a).ToString();
         }
     }
 }
