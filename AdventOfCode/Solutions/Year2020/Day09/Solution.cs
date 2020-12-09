3ffc82ce2b298e3548fbb757cd8724b421be3c32
Drew <vsts@internal.cloudapp.net>
2020-12-09 08:34:31 -0600

    2020 Day 9 Part 1

--- AdventOfCode/Solutions/Year2020/Day09/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day09/Solution.cs
@@ -9,14 +9,36 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day09 : ASolution
     {
+        List<int> puzzle = new List<int>();
 
         public Day09() : base(09, 2020, "")
         {
-
+            puzzle = Input.ToIntArray("\n").ToList();
         }
 
         protected override string SolvePartOne()
         {
+            for(int i=25; i<puzzle.Count; i++) {
+                int sum = puzzle[i];
+                bool found = false;
+
+                // Search through the previous 25 numbers
+                foreach(int q in Enumerable.Range(i-25, 25)) {
+                    foreach(int p in Enumerable.Range(q+1, i-q+1)) {
+                        if (puzzle[q] + puzzle[p] == sum) {
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (found) break;
+                }
+
+                if (found == false) {
+                    return sum.ToString();
+                }
+            }
+
             return null;
         }
 
