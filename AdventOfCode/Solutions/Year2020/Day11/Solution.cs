ec85e700d7a04c7f6daf5f7d492ab576bb61da1d
Drew <vsts@internal.cloudapp.net>
2020-12-11 08:25:54 -0600

    2020 Day 11 Incomplete

--- AdventOfCode/Solutions/Year2020/Day11/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day11/Solution.cs
@@ -10,7 +10,8 @@ namespace AdventOfCode.Solutions.Year2020
     enum WaitingSpotType {
         Empty,
         Occupied,
-        Floor
+        Floor,
+        None
     }
 
     class Day11 : ASolution
@@ -19,8 +20,14 @@ namespace AdventOfCode.Solutions.Year2020
 
         public Day11() : base(11, 2020, "")
         {
+            loadMap();
+        }
+
+        private void loadMap() {
             int y = 0;
             int x;
+
+            map = new Dictionary<(int x, int y), WaitingSpotType>();
 
             foreach(string line in Input.SplitByNewline()) {
                 x = 0;
@@ -31,6 +38,57 @@ namespace AdventOfCode.Solutions.Year2020
 
                 y++;
             }
+        }
+
+        // If it is empty and no seats adjacent are occupied, then yes
+        private WaitingSpotType ToBeOccupied(int x, int y) => GetAdjacentSpots(x, y).Count(a => a == WaitingSpotType.Occupied) == 0 ? WaitingSpotType.Occupied : WaitingSpotType.Empty;
+        private WaitingSpotType ToBeEmptied(int x, int y) => GetAdjacentSpots(x, y).Count(a => a == WaitingSpotType.Occupied) >= 4 ? WaitingSpotType.Empty : WaitingSpotType.Occupied;
+
+        private List<WaitingSpotType> GetAdjacentSpots(int x, int y) => 
+            new List<WaitingSpotType>() {
+                GetSpotType(x-1, y-1),
+                GetSpotType(x  , y-1),
+                GetSpotType(x+1, y-1),
+                GetSpotType(x-1, y ),
+                GetSpotType(x+1, y ),
+                GetSpotType(x-1, y+1),
+                GetSpotType(x  , y+1),
+                GetSpotType(x+1, y+1),
+            };
+
+        private WaitingSpotType GetSpotType(int x, int y) => map.ContainsKey((x, y)) ? map[(x, y)] : WaitingSpotType.None;
+
+        private int runMap() {
+            var newMap = new Dictionary<(int x, int y), WaitingSpotType>();
+            int changes = 0;
+            
+            foreach(var kvp in map) {
+                // If this spot is a floor tile, skip it
+                if (kvp.Value == WaitingSpotType.Floor) {
+                    newMap[kvp.Key] = kvp.Value;
+                    continue;
+                }
+
+                switch (kvp.Value) {
+                    case WaitingSpotType.Floor:
+                        newMap[kvp.Key] = kvp.Value;
+                        break;
+                    
+                    case WaitingSpotType.Empty:
+                        newMap[kvp.Key] = ToBeOccupied(kvp.Key.x, kvp.Key.y);
+                        changes += (newMap[kvp.Key] == kvp.Value ? 0 : 1);
+                        break;
+                    
+                    case WaitingSpotType.Occupied:
+                        newMap[kvp.Key] = ToBeEmptied(kvp.Key.x, kvp.Key.y);
+                        changes += (newMap[kvp.Key] == kvp.Value ? 0 : 1);
+                        break;
+                }
+            }
+
+            // Set the map
+            this.map = newMap;
+            return changes;
         }
 
         protected override string SolvePartOne()
