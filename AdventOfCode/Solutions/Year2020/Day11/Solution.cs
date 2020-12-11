0e7b9a01ef4ccd160ca7efb77e957fdaf3cb9fdc
Drew <vsts@internal.cloudapp.net>
2020-12-11 08:14:00 -0600

    2020 Day 11 Part 1 incompelte

--- AdventOfCode/Solutions/Year2020/Day11/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day11/Solution.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using System.Text;
 
 using System.Linq;
+using System.Collections;
 
 namespace AdventOfCode.Solutions.Year2020
 {
+    enum WaitingSpotType {
+        Empty,
+        Occupied,
+        Floor
+    }
 
     class Day11 : ASolution
     {
+        Dictionary<(int x, int y), WaitingSpotType> map;
 
         public Day11() : base(11, 2020, "")
         {
+            int y = 0;
+            int x;
 
+            foreach(string line in Input.SplitByNewline()) {
+                x = 0;
+
+                foreach(char c in line) {
+                    map[(c, y)] = (c == '.' ? WaitingSpotType.Floor : (c == 'L' ? WaitingSpotType.Empty : WaitingSpotType.Occupied));
+                }
+
+                y++;
+            }
         }
 
         protected override string SolvePartOne()
