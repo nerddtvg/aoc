7f9cf4164cf35f191e6622fa00d475463392cc66
Drew <vsts@internal.cloudapp.net>
2020-12-13 19:40:58 -0600

    2020 Day 13 Part 2 incomplete again, too low

--- AdventOfCode/Solutions/Year2020/Day13/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day13/Solution.cs
@@ -49,6 +49,43 @@ namespace AdventOfCode.Solutions.Year2020
             return null;
         }
 
+        // Returns modulo inverse of a with respect to m using extended 
+        // Euclid Algorithm. Refer below post for details: 
+        // https://www.geeksforgeeks.org/multiplicative-inverse-under-modulo-m/ 
+        private ulong inv(ulong a, ulong m) {
+            ulong m0 = m, t, q; 
+            ulong x0 = 0, x1 = 1; 
+        
+            if (m == 1) 
+            return 0; 
+        
+            // Apply extended Euclid Algorithm 
+            while (a > 1) 
+            { 
+                // q is quotient 
+                q = a / m; 
+        
+                t = m; 
+        
+                // m is remainder now, process same as 
+                // euclid's algo 
+                m = a % m;
+                a = t;
+        
+                t = x0; 
+        
+                x0 = x1 - q * x0; 
+        
+                x1 = t; 
+            } 
+        
+            // Make x1 positive 
+            if (x1 < 0) 
+            x1 += m0; 
+        
+            return x1; 
+        }
+
         protected override string SolvePartTwo()
         {
             // Chinese Remainder Theorem
@@ -70,24 +107,16 @@ namespace AdventOfCode.Solutions.Year2020
                         pp[i] with respect to num[i]
             */
 
-            ulong prod = 1;
-            buses.ForEach(a => prod = prod * a.bus);
+            ulong prod = buses.Select(bus => bus.bus).Aggregate((a,b) => a*b);
 
-            // Generate the pp[] array
-            List<ulong> pp = new List<ulong>();
-            for(int i=0; i<buses.Count; i++)
-                pp.Add(prod / buses[i].bus);
-            
+            ulong result = 0;
 
-            // Generate the inv[] array
-            List<ulong> inv = new List<ulong>();
-            for(int i=0; i<buses.Count; i++)
-                inv.Add((pp[i] * buses[i].remainder) % buses[i].bus);
+            for(int i=0; i<buses.Count; i++) {
+                ulong pp = prod / buses[i].bus;
+                result += buses[i].remainder * inv(pp, buses[i].bus) * pp;
+            }
 
-            ulong sum = 0;
-            Enumerable.Range(0, buses.Count).ToList().ForEach(i => sum += buses[i].remainder * pp[i] * inv[i]);
-
-            return (sum % prod).ToString();
+            return (result % prod).ToString();
         }
     }
 }
