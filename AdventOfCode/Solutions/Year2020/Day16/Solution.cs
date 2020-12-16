9a94cf1abf04b0e66949bc2cff0c248f1a34e103
Drew <vsts@internal.cloudapp.net>
2020-12-16 09:17:53 -0600

    2020 Day 16 Part 2 Parsing and Reduction

--- AdventOfCode/Solutions/Year2020/Day16/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day16/Solution.cs
@@ -91,6 +91,26 @@ namespace AdventOfCode.Solutions.Year2020
             }
         }
 
+        private bool IsValidTicket(Ticket t) {
+            foreach(var v in t.values) {
+                bool valid = false;
+
+                // Check this field against all rules given
+                foreach(var f in this.fields) {
+                    if (f.Value.IsValid(v)) {
+                        // Found one rule that matched, return here
+                        valid = true;
+                        break;
+                    }
+                }
+
+                if (!valid) return false;
+            }
+
+            // All values match something
+            return true;
+        }
+
         private int GetInvalidSum(Ticket t) {
             // Go through each value and identify if it is invalid
             // If so, add to the sum
@@ -114,7 +134,73 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartTwo()
         {
-            return null;
+            // Remove any tickets with invalid values
+            Console.WriteLine($"Before Count {this.tickets.Count}");
+            List<Ticket> remove = new List<Ticket>();
+
+            foreach(var t in this.tickets)
+                if (!IsValidTicket(t)) remove.Add(t);
+
+            // Remove these from the list (after we've iterated through it)
+            remove.ForEach(a => this.tickets.Remove(a));
+            Console.WriteLine($"After Count {this.tickets.Count}");
+
+            // So now we need to work through all of the values in all tickets
+            // Identify which field types they have in common
+            Dictionary<int, List<string>> possibles = new Dictionary<int, List<string>>();
+
+            // Go through each field and figure out what matches, could be multiple
+            for(int i=0; i<this.fields.Count; i++) {
+                // Getting a list of values in this position across all tickets
+                List<int> values = this.tickets.Select(a => a.values[i]).ToList();
+
+                // Go through every field:
+                // Is every value given valid for this field? Yes, it should match the list of given values (count)
+                // Add it to the list
+                List<string> poss = this.fields.Where(field => values.Count(v => field.Value.IsValid(v)) == values.Count).Select(a => a.Key).ToList();
+
+                possibles.Add(i, poss);
+            }
+
+            // MANUAL: We stop here to identify if we have duplicates
+            /*
+            foreach (var kvp in possibles)
+                Console.WriteLine($"{kvp.Key}: {string.Join(", ", kvp.Value)}");
+            */
+
+            // We identified we have many fields with duplicate possible values
+            // So we need to reduce this down to figure out what is truly the only possible value for each
+            // Start by finding all single possible fields and remove that value from any other
+            bool removed = false;
+            do {
+                // Reset
+                removed = false;
+
+                possibles.Where(a => a.Value.Count == 1).ToList().ForEach(v => {
+                    for(int i=0; i<this.fields.Count; i++) {
+                        int before = possibles[i].Count;
+
+                        if (before > 1) {
+                            // Remove this value from the list
+                            possibles[i].Remove(v.Value.First());
+
+                            // We removed something so loop again
+                            if (possibles[i].Count != before) removed = true;
+                        }
+                    }
+                });
+            } while(removed);
+
+            // MANUAL: We stop here to identify if we have duplicates
+            /*
+            foreach (var kvp in possibles)
+                Console.WriteLine($"{kvp.Key}: {string.Join(", ", kvp.Value)}");
+            */
+
+            // We found the above reduction simplified everything down to a single possible key for each value
+            
+
+            return remove.Count.ToString();
         }
     }
 }
