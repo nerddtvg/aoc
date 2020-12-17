407b7e868dfd53d850710f274f0290e606eab69a
Drew <vsts@internal.cloudapp.net>
2020-12-17 07:30:59 -0600

    Work to get the dimensino to run

--- AdventOfCode/Solutions/Year2020/Day17/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day17/Solution.cs
@@ -13,7 +13,17 @@ namespace AdventOfCode.Solutions.Year2020
 
         public Day17() : base(17, 2020, "")
         {
+            // Load the initial active cubes
+            // Initial state is x,y with z=0
+            int y = 0;
+            foreach(var line in Input.SplitByNewline()) {
+                for(int x=0; x<line.Length; x++) {
+                    if (line.Substring(x, 1) == "#")
+                        dim[(x, y, 0)] = true;
+                }
 
+                y++;
+            }
         }
 
         private bool GetCubeState((int x, int y, int z) a) =>
@@ -33,6 +43,55 @@ namespace AdventOfCode.Solutions.Year2020
                     }
                 }
             }
+        }
+
+        private bool CurrentlyInactiveCheck((int x, int y, int z) a) {
+            // Cube is currently inactive
+            // If a cube is inactive but exactly 3 of its neighbors are active, the cube becomes active
+            var c = GetNeighbors(a).Count(a => a == true);
+
+            if (c ==3) return true;
+
+            return false;
+        }
+
+        private bool CurrentlyActiveCheck((int x, int y, int z) a) {
+            // Cube is currently active
+            // If exactly 2 or 3 neighbors are active, cube stays active
+            var c = GetNeighbors(a).Count(a => a == true);
+
+            if (c == 2 || c ==3) return true;
+
+            return false;
+        }
+
+        private void RunGeneration() {
+            int minX = this.dim.Min(a => a.Key.x);
+            int maxX = this.dim.Max(a => a.Key.x);
+            int minY = this.dim.Min(a => a.Key.y);
+            int maxY = this.dim.Max(a => a.Key.y);
+            int minZ = this.dim.Min(a => a.Key.z);
+            int maxZ = this.dim.Max(a => a.Key.z);
+
+            // Make a new dimension
+            var newDim = new Dictionary<(int x, int y, int z), bool>();
+
+            for(int x=minX-1; x<=maxX+1; x++) {
+                for(int y=minY-1; y<=maxY+1; y++) {
+                    for(int z=minZ-1; z<=maxZ+1; z++) {
+                        var addr = (x, y, z);
+                        var cube = GetCubeState(addr);
+
+                        if (cube)
+                            newDim[addr] = CurrentlyActiveCheck(addr);
+                        else
+                            newDim[addr] = CurrentlyInactiveCheck(addr);
+                    }
+                }
+            }
+
+            // At the end, replace the old dimension
+            this.dim = newDim;
         }
 
         protected override string SolvePartOne()
