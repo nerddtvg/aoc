283fec507ea132dfad85be7af10e760c047e40a8
Drew <vsts@internal.cloudapp.net>
2020-12-17 07:21:29 -0600

    2020 Day 17 initial work

--- AdventOfCode/Solutions/Year2020/Day17/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day17/Solution.cs
@@ -9,10 +9,30 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day17 : ASolution
     {
+        Dictionary<(int x, int y, int z), bool> dim = new Dictionary<(int x, int y, int z), bool>();
 
         public Day17() : base(17, 2020, "")
         {
 
+        }
+
+        private bool GetCubeState((int x, int y, int z) a) =>
+            dim.ContainsKey(a) ? dim[a] : false;
+
+        private bool GetCubeState(int x, int y, int z) =>
+            GetCubeState((x, y, z));
+        
+        private IEnumerable<bool> GetNeighbors((int x, int y, int z) a) {
+            for(int dx=-1; dx<=1; dx++) {
+                for(int dy=-1; dy<=1; dy++) {
+                    for(int dz=-1; dz<=1; dz++) {
+                        // Only neighbors
+                        if (dx == 0 && dy == 0 && dz == 0) continue;
+
+                        yield return GetCubeState((a.x+dx, a.y+dy, a.z+dz));
+                    }
+                }
+            }
         }
 
         protected override string SolvePartOne()
