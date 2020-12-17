afa80f364cd8910d1f762240a6af8cd384ba774c
Drew <vsts@internal.cloudapp.net>
2020-12-17 07:41:42 -0600

    2020 Day 17 Part 2 incomplete

--- AdventOfCode/Solutions/Year2020/Day17/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day17/Solution.cs
@@ -11,15 +11,27 @@ namespace AdventOfCode.Solutions.Year2020
     {
         Dictionary<(int x, int y, int z), bool> dim = new Dictionary<(int x, int y, int z), bool>();
 
+        // Part 2
+        Dictionary<(int x, int y, int z, int w), bool> dim2 = new Dictionary<(int x, int y, int z, int w), bool>();
+
         public Day17() : base(17, 2020, "")
         {
+            DebugInput = @"
+            .#.
+            ..#
+            ###
+            ";
+
             // Load the initial active cubes
             // Initial state is x,y with z=0
             int y = 0;
-            foreach(var line in Input.SplitByNewline()) {
+            foreach(var line in Input.SplitByNewline(true, true)) {
                 for(int x=0; x<line.Length; x++) {
                     if (line.Substring(x, 1) == "#")
                         dim[(x, y, 0)] = true;
+                        
+                        // Part 2
+                        dim2[(x, y, 0, 0)] = true;
                 }
 
                 y++;
@@ -29,8 +41,8 @@ namespace AdventOfCode.Solutions.Year2020
         private bool GetCubeState((int x, int y, int z) a) =>
             dim.ContainsKey(a) ? dim[a] : false;
 
-        private bool GetCubeState(int x, int y, int z) =>
-            GetCubeState((x, y, z));
+        private bool GetCubeState2((int x, int y, int z, int w) a) =>
+            dim2.ContainsKey(a) ? dim2[a] : false;
         
         private IEnumerable<bool> GetNeighbors((int x, int y, int z) a) {
             for(int dx=-1; dx<=1; dx++) {
@@ -44,13 +56,28 @@ namespace AdventOfCode.Solutions.Year2020
                 }
             }
         }
+        
+        private IEnumerable<bool> GetNeighbors2((int x, int y, int z, int w) a) {
+            for(int dx=-1; dx<=1; dx++) {
+                for(int dy=-1; dy<=1; dy++) {
+                    for(int dz=-1; dz<=1; dz++) {
+                        for(int dw=-1; dw<=1; dw++) {
+                            // Only neighbors
+                            if (dx == 0 && dy == 0 && dz == 0 && dw == 0) continue;
+
+                            yield return GetCubeState2((a.x+dx, a.y+dy, a.z+dz, a.w+dw));
+                        }
+                    }
+                }
+            }
+        }
 
         private bool CurrentlyInactiveCheck((int x, int y, int z) a) {
             // Cube is currently inactive
             // If a cube is inactive but exactly 3 of its neighbors are active, the cube becomes active
             var c = GetNeighbors(a).Count(a => a == true);
 
-            if (c ==3) return true;
+            if (c == 3) return true;
 
             return false;
         }
@@ -60,7 +87,27 @@ namespace AdventOfCode.Solutions.Year2020
             // If exactly 2 or 3 neighbors are active, cube stays active
             var c = GetNeighbors(a).Count(a => a == true);
 
-            if (c == 2 || c ==3) return true;
+            if (c == 2 || c == 3) return true;
+
+            return false;
+        }
+
+        private bool CurrentlyInactiveCheck2((int x, int y, int z, int w) a) {
+            // Cube is currently inactive
+            // If a cube is inactive but exactly 3 of its neighbors are active, the cube becomes active
+            var c = GetNeighbors2(a).Count(a => a == true);
+
+            if (c == 3) return true;
+
+            return false;
+        }
+
+        private bool CurrentlyActiveCheck2((int x, int y, int z, int w) a) {
+            // Cube is currently active
+            // If exactly 2 or 3 neighbors are active, cube stays active
+            var c = GetNeighbors2(a).Count(a => a == true);
+
+            if (c == 2 || c == 3) return true;
 
             return false;
         }
@@ -94,6 +141,39 @@ namespace AdventOfCode.Solutions.Year2020
             this.dim = newDim;
         }
 
+        private void RunGeneration2() {
+            int minX = this.dim2.Min(a => a.Key.x);
+            int maxX = this.dim2.Max(a => a.Key.x);
+            int minY = this.dim2.Min(a => a.Key.y);
+            int maxY = this.dim2.Max(a => a.Key.y);
+            int minZ = this.dim2.Min(a => a.Key.z);
+            int maxZ = this.dim2.Max(a => a.Key.z);
+            int minW = this.dim2.Min(a => a.Key.w);
+            int maxW = this.dim2.Max(a => a.Key.w);
+
+            // Make a new dimension
+            var newDim = new Dictionary<(int x, int y, int z, int w), bool>();
+
+            for(int x=minX-1; x<=maxX+1; x++) {
+                for(int y=minY-1; y<=maxY+1; y++) {
+                    for(int z=minZ-1; z<=maxZ+1; z++) {
+                        for(int w=minW-1; w<=maxW+1; w++) {
+                            var addr = (x, y, z, w);
+                            var cube = GetCubeState2(addr);
+
+                            if (cube)
+                                newDim[addr] = CurrentlyActiveCheck2(addr);
+                            else
+                                newDim[addr] = CurrentlyInactiveCheck2(addr);
+                        }
+                    }
+                }
+            }
+
+            // At the end, replace the old dimension
+            this.dim2 = newDim;
+        }
+
         protected override string SolvePartOne()
         {
             for(int i=0; i<6; i++)
@@ -104,7 +184,10 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartTwo()
         {
-            return null;
+            for(int i=0; i<6; i++)
+                RunGeneration2();
+            
+            return this.dim2.Count(a => a.Value == true).ToString();
         }
     }
 }
