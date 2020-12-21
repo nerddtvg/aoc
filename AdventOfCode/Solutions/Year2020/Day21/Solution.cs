fbf187e3f4eee79777da39a836a8cbbe6f883a45
Drew <vsts@internal.cloudapp.net>
2020-12-21 12:16:10 -0600

    2020 Day 21 Part 1 incomplete

--- AdventOfCode/Solutions/Year2020/Day21/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day21/Solution.cs
@@ -6,13 +6,29 @@ using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2020
 {
+    class Food {
+        public List<string> ingredients {get;set;}
+        public List<string> allergens {get;set;}
+    }
 
     class Day21 : ASolution
     {
+        List<Food> foods = new List<Food>();
 
         public Day21() : base(21, 2020, "")
         {
+            foreach(string line in Input.SplitByNewline(true)) {
+                // Ingredients: parts[0]
+                // Allergens: parts[1]
+                var parts = line.Split("(", StringSplitOptions.TrimEntries);
 
+                var food = new Food();
+
+                food.ingredients = parts[0].Split(" ", StringSplitOptions.TrimEntries).ToList();
+                food.allergens = parts[1].Replace("contains ", "").Replace(")", "").Split(",", StringSplitOptions.TrimEntries).ToList();
+
+                foods.Add(food);
+            }
         }
 
         protected override string SolvePartOne()
