26d2b113fc7081cbcfcd255b49f785d9eb3a417a
Drew <vsts@internal.cloudapp.net>
2020-12-23 10:56:47 -0600

    2020 Day 20 Part 2 implemented tile variants

--- AdventOfCode/Solutions/Year2020/Day20/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day20/Solution.cs
@@ -6,10 +6,26 @@ using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2020
 {
+    class SatTileVariant {
+        public string tile {get;set;}
+        public List<string> edges {get;set;}
+
+        public SatTileVariant(string[] input) {
+            // Input is an array of strings (one per line of the tile)
+            this.tile = string.Join("\n", input);
+
+            // Set the edges (top, right, bottom, left)
+            this.edges = new List<string>();
+            this.edges.Add(input[0]);
+            this.edges.Add(string.Join("", input.Select(a => a.Substring(a.Length-1, 1))));
+            this.edges.Add(input[9]);
+            this.edges.Add(string.Join("", input.Select(a => a.Substring(0, 1))));
+        }
+    }
+
     class SatTile {
         public int id {get;set;}
         public string tile {get;set;}
-        public List<string> edges {get;set;}
 
         public int x {get;set;}
         public int y {get;set;}
@@ -17,14 +33,19 @@ namespace AdventOfCode.Solutions.Year2020
 
         public bool searched {get;set;}
 
+        // Let's keep a list of all rotated and flipped tile variants
+        // This will make matching/string manipulation easier later
+        public List<SatTileVariant> variants {get;set;}
+
+        // A quick list of our edges
+        public List<string> edges {get;set;}
+
         public override string ToString()
         {
-            return $"{this.id}: {string.Join(", ", this.edges)}";
+            return $"{this.id}: {string.Join(", ", this.variants[0].edges)}";
         }
 
         public SatTile(string[] input) {
-            this.edges = new List<string>();
-
             this.x = Int32.MinValue;
             this.y = Int32.MinValue;
             this.corner = false;
@@ -34,13 +55,54 @@ namespace AdventOfCode.Solutions.Year2020
             this.id = Int32.Parse(input[0].Split(" ", StringSplitOptions.TrimEntries)[1].Replace(":", ""));
 
             // The rest of the input...
-            this.tile = string.Join("\n", input.Skip(1));
+            input = input.Skip(1).ToArray();
+            this.tile = string.Join("\n", input);
 
-            // Set the edges (top, right, bottom, left)
-            this.edges.Add(input[1]);
-            this.edges.Add(string.Join("", input.Skip(1).Select(a => a.Substring(a.Length-1, 1))));
-            this.edges.Add(input[10]);
-            this.edges.Add(string.Join("", input.Skip(1).Select(a => a.Substring(0, 1))));
+            // Set our edges for part 1
+            this.edges = new List<string>();
+            this.edges.Add(input[0]);
+            this.edges.Add(string.Join("", input.Select(a => a.Substring(a.Length-1, 1))));
+            this.edges.Add(input[9]);
+            this.edges.Add(string.Join("", input.Select(a => a.Substring(0, 1))));
+
+            // Get all variants
+            this.variants = new List<SatTileVariant>();
+
+            for(int flip=0; flip<2; flip++) {
+                for(int rotate=0; rotate<4; rotate++) {
+                    // Load the variant
+                    var v = (string[]) input.Clone();
+
+                    // We need to work on this
+                    // Flip all of the lines around
+                    if (flip > 0)
+                        for(int i=0; i<v.Length; i++)
+                            v[i] = v[i].Reverse();
+                    
+                    // Now we need to (maybe) rotate
+                    for(int i=0; i<rotate; i++)
+                        v = rotateArray(v);
+
+                    this.variants.Add(new SatTileVariant(v));
+                }
+            }
+        }
+
+        public string[] rotateArray(string[] input) {
+            // Take the input array and rotate it 90 degrees
+            string[] outArr = new string[input.Length];
+
+            // Rotating 90 degrees is "simply" reading bottom to top, left to right
+            for(int line=0; line<input[0].Length; line++) {
+                // line is the index for outArr
+                outArr[line] = "";
+
+                // line is also the 'x' value in input
+                for(int y=input.Length-1; y>=0; y--)
+                    outArr[line] += input[y].Substring(line, 1);
+            }
+
+            return outArr;
         }
 
         /*
@@ -283,7 +345,7 @@ namespace AdventOfCode.Solutions.Year2020
                 var matches = tile.edges.Intersect(allEdges);
                 var count = matches.Count();
 
-                Console.WriteLine($"{tile.id}: {count}");
+                //Console.WriteLine($"{tile.id}: {count}");
 
                 // Check if we only have two matching
                 if (count == 2)
@@ -298,6 +360,10 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartTwo()
         {
+            // We need to count the '#' that are not a part of the sea monsters
+            // Sea monsters are 15 '#' characters
+            // We need to remove the edges from each tile before stitching them together
+
             return null;
         }
     }
