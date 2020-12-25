0483d3ce2a141238b5a57528a763fd3e5d4fb5b8
Drew <vsts@internal.cloudapp.net>
2020-12-25 11:44:26 -0600

    2020 Day 25 Working on the loop size

--- AdventOfCode/Solutions/Year2020/Day25/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day25/Solution.cs
@@ -4,6 +4,8 @@ using System.Text;
 
 using System.Linq;
 
+using System.Numerics;
+
 namespace AdventOfCode.Solutions.Year2020
 {
 
@@ -12,7 +14,38 @@ namespace AdventOfCode.Solutions.Year2020
 
         public Day25() : base(25, 2020, "")
         {
+            DebugInput = @"5764801
+                17807724";
 
+            var lines = Input.SplitByNewline(true, true);
+            int cardPubKey = Int32.Parse(lines[0]);
+            int doorPubKey = Int32.Parse(lines[1]);
+
+            int pubKeySubject = 7;
+
+            var cardLoopSize = getLoopSize(pubKeySubject, cardPubKey);
+            var doorLoopSize = getLoopSize(pubKeySubject, doorPubKey);
+
+            Console.WriteLine(cardLoopSize);
+            Console.WriteLine(doorLoopSize);
+        }
+
+        private int getLoopSize(int subject, int remainder) {
+            // Figure out how many times we have to loop through the subject to get the remainder
+            int i = 0;
+            var biS = new BigInteger(subject);
+            var biR = new BigInteger(remainder);
+            var biD = new BigInteger(20201227);
+
+            for(i=1; i<50; i++) {
+                biS *= subject;
+                BigInteger r = biS % biD;
+
+                // Did we find it?
+                if (r == biR) break;
+            }
+
+            return i+1;
         }
 
         protected override string SolvePartOne()
