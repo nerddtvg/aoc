2cb9f6ba03476d1d4750608d39ce4a086b5e278b
Drew <vsts@internal.cloudapp.net>
2021-07-01 20:56:13 -0500

    2015 Day 1

--- AdventOfCode/Solutions/Year2015/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2015/Day01/Solution.cs
@@ -17,12 +17,30 @@ namespace AdventOfCode.Solutions.Year2015
 
         protected override string SolvePartOne()
         {
-            return null;
+            int floor = 0;
+            Input.ToCharArray().ToList().ForEach(a =>
+            {
+                if (a == '(') floor++;
+                else if (a == ')') floor--;
+            });
+
+            return floor.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            int floor = 0;
+            int pos = 1;
+            foreach(var a in Input.ToCharArray())
+            {
+                if (a == '(') floor++;
+                else if (a == ')') floor--;
+
+                if (floor < 0) break;
+                pos++;
+            }
+
+            return pos.ToString();
         }
     }
 }
