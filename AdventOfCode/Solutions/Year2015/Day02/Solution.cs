c11a92f94e6dad440e46fb5f66e34a0f86335305
Drew <vsts@internal.cloudapp.net>
2021-07-01 21:06:23 -0500

    2015 Day 2

--- AdventOfCode/Solutions/Year2015/Day02/Solution.cs
+++ AdventOfCode/Solutions/Year2015/Day02/Solution.cs
@@ -17,12 +17,51 @@ namespace AdventOfCode.Solutions.Year2015
 
         protected override string SolvePartOne()
         {
-            return null;
+            uint totalSq = 0;
+
+            // Get present dimensions
+            foreach(var line in Input.SplitByNewline())
+            {
+                if (string.IsNullOrEmpty(line.Trim())) continue;
+
+                var size = line.Split('x', 3, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+                if (size.Length != 3) continue;
+
+                var sizes = size.Select(a => UInt32.Parse(a)).ToArray();
+
+                var sides = new uint[] { sizes[0] * sizes[1], sizes[1] * sizes[2], sizes[0] * sizes[2] };
+
+                totalSq += (2 * sides[0]) + (2 * sides[1]) + (2 * sides[2]) + Math.Min(sides[0], Math.Min(sides[1], sides[2]));
+            }
+
+            return totalSq.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            uint totalLen = 0;
+
+            // Get present dimensions
+            foreach(var line in Input.SplitByNewline())
+            {
+                if (string.IsNullOrEmpty(line.Trim())) continue;
+
+                var size = line.Split('x', 3, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+                if (size.Length != 3) continue;
+
+                var sizes = size.Select(a => UInt32.Parse(a)).ToList();
+
+                // Get the two smallest
+                var max = sizes.Max();
+                sizes.Remove(max);
+
+                // Calculate the shortest perimeter and volume
+                totalLen += (sizes[0] + sizes[0] + sizes[1] + sizes[1]) + (sizes[0] * sizes[1] * max);
+            }
+
+            return totalLen.ToString();
         }
     }
 }
