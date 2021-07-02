d6f9d500b2ba68ec8c84f6c339173d1f04d8317c
Drew <vsts@internal.cloudapp.net>
2021-07-01 21:32:13 -0500

    2015 Day 4

--- AdventOfCode/Solutions/Year2015/Day04/Solution.cs
+++ AdventOfCode/Solutions/Year2015/Day04/Solution.cs
@@ -4,6 +4,8 @@ using System.Text;
 
 using System.Linq;
 
+using System.Security.Cryptography;
+
 namespace AdventOfCode.Solutions.Year2015
 {
 
@@ -17,12 +19,36 @@ namespace AdventOfCode.Solutions.Year2015
 
         protected override string SolvePartOne()
         {
-            return null;
+            uint i;
+
+            for (i = 1; i < uint.MaxValue; i++)
+            {
+                var str = $"{Input}{i}";
+                var md5 = string.Join("", MD5.HashData(Encoding.ASCII.GetBytes(str)).SelectMany(a => a.ToString("X2")));
+
+                // Find the MD5 hash that stats with five zeros
+                if (md5.StartsWith("00000"))
+                    break;
+            }
+
+            return i.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            uint i;
+
+            for (i = 1; i < uint.MaxValue; i++)
+            {
+                var str = $"{Input}{i}";
+                var md5 = string.Join("", MD5.HashData(Encoding.ASCII.GetBytes(str)).SelectMany(a => a.ToString("X2")));
+
+                // Find the MD5 hash that stats with six zeros
+                if (md5.StartsWith("000000"))
+                    break;
+            }
+
+            return i.ToString();
         }
     }
 }
