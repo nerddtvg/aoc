3c06d2299192810b9843cfc994e12714ee55bd49
Drew <vsts@internal.cloudapp.net>
2021-07-02 08:47:57 -0500

    2015 Day 5

--- AdventOfCode/Solutions/Year2015/Day05/Solution.cs
+++ AdventOfCode/Solutions/Year2015/Day05/Solution.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 using System.Linq;
 
@@ -9,20 +10,51 @@ namespace AdventOfCode.Solutions.Year2015
 
     class Day05 : ASolution
     {
+        private char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
 
         public Day05() : base(05, 2015, "")
         {
-
+            
         }
+
+        private bool IsValidPart1(string str) =>
+            !(str.Contains("ab") || str.Contains("cd") || str.Contains("pq") || str.Contains("xy"))
+            &&
+            (new Regex(@"([a-z])\1").IsMatch(str))
+            &&
+            str.ToCharArray().GroupBy(a => a).Where(a => vowels.Contains(a.Key)).Sum(a => a.Count()) >= 3;
+
+        private bool IsValidPart2(string str) =>
+            // One letter that repeats with exactly one letter between them
+            (new Regex(@"([a-z])[a-z]\1").IsMatch(str))
+            &&
+            // Any pair of two letters that repeats itself
+            (new Regex(@"([a-z][a-z]).*\1").IsMatch(str));
 
         protected override string SolvePartOne()
         {
-            return null;
+            int count = 0;
+            foreach(var line in Input.SplitByNewline())
+            {
+                //Console.WriteLine($"{line}: {IsValidPart1(line)}");
+
+                if (IsValidPart1(line))
+                    count++;
+            }
+            return count.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            int count = 0;
+            foreach(var line in Input.SplitByNewline())
+            {
+                //Console.WriteLine($"{line}: {IsValidPart2(line)}");
+
+                if (IsValidPart2(line))
+                    count++;
+            }
+            return count.ToString();
         }
     }
 }
