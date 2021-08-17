1577e037ab1e7db177f6a2ef2248593b1dd61e2e
Drew <vsts@internal.cloudapp.net>
2021-08-17 15:56:50 -0500

    2015 Day 12

--- AdventOfCode/Solutions/Year2015/Day12/Solution.cs
+++ AdventOfCode/Solutions/Year2015/Day12/Solution.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
 using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2015
@@ -9,15 +12,19 @@ namespace AdventOfCode.Solutions.Year2015
 
     class Day12 : ASolution
     {
+        private object _doc;
 
         public Day12() : base(12, 2015, "")
         {
-
+            this._doc = JsonSerializer.Deserialize<object>(Input);
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var matches = Regex.Matches(Input, "(-|)[0-9]+");
+
+            // We just need to find all numbers in the string, nothing fancy
+            return matches.Select(a => Int32.Parse(a.Value)).Sum().ToString();
         }
 
         protected override string SolvePartTwo()
