2f1a8a806eb59e0e45d32be190eb8a98f34750f7
Drew <vsts@internal.cloudapp.net>
2021-08-18 07:36:07 -0500

    2015 Day 17 Part 1

--- AdventOfCode/Solutions/Year2015/Day17/Solution.cs
+++ AdventOfCode/Solutions/Year2015/Day17/Solution.cs
@@ -9,15 +9,68 @@ namespace AdventOfCode.Solutions.Year2015
 
     class Day17 : ASolution
     {
+        private List<int> _containers = new List<int>();
 
         public Day17() : base(17, 2015, "")
         {
+            // Input it is a list of container sizes
+            foreach(var line in Input.SplitByNewline())
+            {
+                if (line.Trim().Length == 0) continue;
 
+                int a;
+                if (Int32.TryParse(line.Trim(), out a))
+                {
+                    _containers.Add(a);
+                }
+            }
+        }
+
+        private List<List<int>> GetOptions(int required_size, int[] sizes)
+        {
+            // Is this the end of the list?
+            if (sizes.Length == 1)
+                if (sizes[0] != required_size)
+                    return new List<List<int>>();
+                else
+                    return new List<List<int>> { new List<int> { required_size } };
+
+            // Otherwise, take another step down
+            var ret = new List<List<int>>();
+            if (required_size >= sizes[0])
+            {
+                // We can work with and without this container
+                var temp_size = required_size - sizes[0];
+                if (temp_size > 0)
+                {
+                    var tRet = GetOptions(temp_size, sizes.Skip(1).ToArray());
+
+                    // We have supposed combos that work, prepend our size
+                    tRet.ForEach(a => a.Insert(0, temp_size));
+                    ret.AddRange(tRet);
+                }
+                else
+                {
+                    ret.Add(new List<int>() { sizes[0] });
+                }
+            }
+
+            // Now try it without the first one
+            {
+                var tRet = GetOptions(required_size, sizes.Skip(1).ToArray());
+                ret.AddRange(tRet);
+            }
+
+            return ret;
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            // Order our containers
+            this._containers = this._containers.OrderBy(a => a).ToList();
+
+            // For each container in the list, go through and figure out if we can make it work or not
+            return GetOptions(150, this._containers.ToArray()).Count.ToString();
         }
 
         protected override string SolvePartTwo()
