558a12e6fab1788b63a190dffffedeb6d3559a0d
Drew <vsts@internal.cloudapp.net>
2021-09-15 10:24:27 -0500

    2015 Day 20 Part 2

--- AdventOfCode/Solutions/Year2015/Day20/Solution.cs
+++ AdventOfCode/Solutions/Year2015/Day20/Solution.cs
@@ -9,6 +9,7 @@ namespace AdventOfCode.Solutions.Year2015
 
     class Day20 : ASolution
     {
+        private int StartPart2 = 0;
 
         public Day20() : base(20, 2015, "")
         {
@@ -24,6 +25,19 @@ namespace AdventOfCode.Solutions.Year2015
             return elves * 10;
         }
 
+        private int GetHousePresentsPart2(int HouseNumber)
+        {
+            // Every elf number that is a divisor, and it is within the first 50 houses for that elf,
+            // has delivered (elf number * 10 presents) to this house
+            var elves = HouseNumber.GetDivisors();
+
+            // We need to determine if this house falls in the elve's first 50 houses (50 * elf number)
+            elves = elves.Where(a => HouseNumber <= a * 50).ToArray();
+
+            // Each elf delivered 11*elf number presents
+            return elves.Sum() * 11;
+        }
+
         protected override string SolvePartOne()
         {
             // Figure out what house gives us at least the Input number of presents
@@ -37,6 +51,9 @@ namespace AdventOfCode.Solutions.Year2015
                 if (presents >= input)
                 {
                     System.Console.WriteLine($"Found: {presents} at house {HouseNumber}");
+
+                    StartPart2 = HouseNumber;
+
                     return HouseNumber.ToString();
                 }
 
@@ -49,7 +66,26 @@ namespace AdventOfCode.Solutions.Year2015
 
         protected override string SolvePartTwo()
         {
-            return null;
+            // Figure out what house gives us at least the Input number of presents
+            var input = Int32.Parse(Input);
+
+            // We skip ahead because we know this has to be after Part 1's finish
+            int HouseNumber = StartPart2;
+            do
+            {
+                // Check this house
+                var presents = GetHousePresentsPart2(HouseNumber);
+                if (presents >= input)
+                {
+                    System.Console.WriteLine($"Found: {presents} at house {HouseNumber}");
+                    return HouseNumber.ToString();
+                }
+
+                // Lots of output here
+                //System.Console.WriteLine($"House {HouseNumber} got {presents} presents.");
+
+                HouseNumber++;
+            } while (true);
         }
     }
 }
