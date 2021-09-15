2289dec245e4ba2b5085df3e6a2199fedf28401f
Drew <vsts@internal.cloudapp.net>
2021-09-15 10:15:40 -0500

    Bruteforce 2015 Day 19 Part 2, unsolved

--- AdventOfCode/Solutions/Year2015/Day19/Solution.cs
+++ AdventOfCode/Solutions/Year2015/Day19/Solution.cs
@@ -11,6 +11,7 @@ namespace AdventOfCode.Solutions.Year2015
     class Day19 : ASolution
     {
         public Dictionary<string, List<string>> replacements = new Dictionary<string, List<string>>();
+        public Dictionary<string, string> reverseReplacements = new Dictionary<string, string>();
 
         public string originalFormula = string.Empty;
 
@@ -43,6 +44,12 @@ namespace AdventOfCode.Solutions.Year2015
             {
                 replacements[key] = new List<string>() { replacement };
             }
+
+            // Add our reverse as well
+            if (reverseReplacements.ContainsKey(replacement))
+                throw new Exception($"Duplicate replacement found: {replacement}");
+
+            reverseReplacements[replacement] = key;
         }
 
         /// <summary>
@@ -61,14 +68,8 @@ namespace AdventOfCode.Solutions.Year2015
             }
         }
 
-        private HashSet<string> ProcessFormulaStep(string formula)
+        private (string currentElement, string currentFormula) GetCurrentElement(string formula)
         {
-            if (formula.Length == 0)
-                return new HashSet<string>();
-
-            // We need to take the first element off this formula and:
-            // 1. Add all of the possible replacements of this element + the rest of the formula appended (one replacement)
-            // 2. Don't replace it, use this element + all possible iterations of the rest of the formula from ProcessFormulaStep()
             var currentElement = formula.Substring(0, 1);
             var tempFormula = formula.Substring(1);
             while(tempFormula.Length > 0)
@@ -93,6 +94,19 @@ namespace AdventOfCode.Solutions.Year2015
                 break;
             }
 
+            return (currentElement, tempFormula);
+        }
+
+        private HashSet<string> ProcessFormulaStep(string formula)
+        {
+            if (formula.Length == 0)
+                return new HashSet<string>();
+
+            // We need to take the first element off this formula and:
+            // 1. Add all of the possible replacements of this element + the rest of the formula appended (one replacement)
+            // 2. Don't replace it, use this element + all possible iterations of the rest of the formula from ProcessFormulaStep()
+            (var currentElement, var tempFormula) = GetCurrentElement(formula);
+
             // Return array
             var ret = new HashSet<string>();
 
@@ -103,10 +117,6 @@ namespace AdventOfCode.Solutions.Year2015
                 {
                     ret.Add(r + tempFormula);
                 }
-            }
-            else
-            {
-                System.Console.WriteLine($"No transformation: {currentElement}");
             }
 
             // Now we handle not replacing this element
@@ -128,9 +138,125 @@ namespace AdventOfCode.Solutions.Year2015
             return replacements.Count.ToString();
         }
 
+        /// <summary>
+        /// Returns a Regex similar to: (ABc|Dfa|G|DFasF) based on all of the replacements possible
+        /// </summary>
+        private Regex GetPart2Regex()
+        {
+            var regexStart = string.Join("|", this.reverseReplacements.Keys.Select(a => a));
+
+            // This is a case-sensitive search
+            return new Regex($"({regexStart})");
+        }
+
         protected override string SolvePartTwo()
         {
-            return null;
+            // Start with 'e' and see how many steps it takes to make the medicine in originalFormula
+            // We're going to reverse process this
+            // We know that none of the replacements are duplicates, so we can find all possible reverse replacements in the formula
+            var regex = GetPart2Regex();
+            var currentStepResults = new HashSet<string>() { originalFormula };
+            var historySteps = new SortedSet<string>() { originalFormula };
+            int steps = 1;
+
+            var breakFormula = "e";
+
+            // Now we need to go through each replacement and process it as a possibility
+            while(true)
+            {
+                var tempStepResults = new HashSet<string>();
+                bool found = false;
+
+                foreach(var thisFormula in currentStepResults)
+                {
+                    var matches = regex.Matches(thisFormula);
+
+                    // For each match, we can (a) replace it or (b) ignore it, so let's find all of our possible replacements
+                    foreach(Match match in matches)
+                    {
+                        // Get the before and after of this match
+                        var beginFormula = match.Index > 0 ? thisFormula.Substring(0, match.Index) : string.Empty;
+                        var endFormula = match.Index + match.Length < thisFormula.Length ? thisFormula.Substring(match.Index + match.Length) : string.Empty;
+
+                        // Replace it and ensure we haven't tried this one before
+                        var tempFormula = $"{beginFormula}{this.reverseReplacements[match.Value]}{endFormula}";
+
+                        // See if we have found the stopping point
+                        found = string.Equals(tempFormula, breakFormula);
+                        if (found) break;
+
+                        // Saves on memory, put into a sortedset will make searching a little faster
+                        var md5 = GetMD5Hash(tempFormula);
+
+                        if (!historySteps.Contains(md5))
+                        {
+                            tempStepResults.Add(tempFormula);
+                            historySteps.Add(md5);
+                        }
+                    }
+
+                    // Going to shortcut this instead of scanning 1 million+
+                    // Assume we need to find some of the shortest results after replacement
+                    tempStepResults = tempStepResults.OrderBy(a => a.Length).Take(2000).ToHashSet();
+                }
+
+                // If we have found a match, we're done
+                if (found)
+                {
+                    break;
+                }
+
+                // Start new
+                currentStepResults = tempStepResults;
+
+                // Increment
+                steps++;
+            }
+
+            return steps.ToString();
+
+            var currentFormula = new HashSet<string>() { "e" };
+            while(true)
+            {
+                var tempFormulas = new HashSet<string>();
+
+                foreach (var formula in currentFormula)
+                {
+                    var stepList = ProcessFormulaStep(formula);
+                    
+                    foreach(var t in stepList)
+                        tempFormulas.Add(t);
+                }
+
+                // Identify if we have created the formula or not
+                if (tempFormulas.Contains(originalFormula))
+                {
+                    break;
+                }
+
+                // It does not, continue on with new formulas
+                currentFormula = tempFormulas;
+                steps++;
+            }
+
+            return steps.ToString();
+        }
+
+        private string GetMD5Hash(string input)
+        {
+            using (var md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
         }
     }
 }
