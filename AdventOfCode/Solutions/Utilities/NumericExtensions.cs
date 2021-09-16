6bd11ce33a7778811093124a8fa9e911f9b9a2b8
Drew <vsts@internal.cloudapp.net>
2021-09-16 10:29:52 -0500

    Reworked the GetDigits functions

--- AdventOfCode/Solutions/Utilities/NumericExtensions.cs
+++ AdventOfCode/Solutions/Utilities/NumericExtensions.cs
@@ -118,5 +118,102 @@ namespace AdventOfCode.Solutions
                 .Select(a => (Int64)a)
                 .ToArray();
         }
+
+        /// <summary>
+        /// Get the individual digits that make up this number (must be positive)
+        /// </summary>
+        /// <param name="source">The source number</param>
+        /// <returns>An array of digits in order</returns>
+        public static UInt64[] GetDigits(this UInt64 source)
+        {
+            // What we will return
+            var digits = new Stack<UInt64>();
+
+            while(source > 0)
+            {
+                // Add this digit
+                digits.Push(source % 10);
+
+                // Move on
+                source = source / 10;
+            }
+
+            return digits.ToArray();
+        }
+
+        /// <summary>
+        /// Get the individual digits that make up this number (must be positive)
+        /// </summary>
+        /// <param name="source">The source number</param>
+        /// <returns>An array of digits in order</returns>
+        public static UInt16[] GetDigits(this UInt16 input) =>
+            // We can use the uint function and down-cast them since we know the digits are really just 0-9
+            ((UInt64)input)
+                .GetDigits()
+                .Select(a => (UInt16)a)
+                .ToArray();
+
+        /// <summary>
+        /// Get the individual digits that make up this number (must be positive)
+        /// </summary>
+        /// <param name="source">The source number</param>
+        /// <returns>An array of digits in order</returns>
+        public static UInt32[] GetDigits(this UInt32 input) =>
+            // We can use the uint function and down-cast them since we know the digits are really just 0-9
+            ((UInt64)input)
+                .GetDigits()
+                .Select(a => (UInt32)a)
+                .ToArray();
+
+        /// <summary>
+        /// Get the individual digits that make up this number (must be positive)
+        /// </summary>
+        /// <param name="source">The source number</param>
+        /// <returns>An array of digits in order</returns>
+        public static Int16[] GetDigits(this Int16 input)
+        {
+            if (input < 0)
+                throw new ArgumentException($"The given input must be positive.", nameof(input));
+                
+            // We can use the uint function and down-cast them since we know the digits are really just 0-9
+            return ((UInt64)input)
+                .GetDigits()
+                .Select(a => (Int16)a)
+                .ToArray();
+        }
+
+        /// <summary>
+        /// Get the individual digits that make up this number (must be positive)
+        /// </summary>
+        /// <param name="source">The source number</param>
+        /// <returns>An array of digits in order</returns>
+        public static Int32[] GetDigits(this Int32 input)
+        {
+            if (input < 0)
+                throw new ArgumentException($"The given input must be positive.", nameof(input));
+                
+            // We can use the uint function and down-cast them since we know the digits are really just 0-9
+            return ((UInt64)input)
+                .GetDivisors()
+                .Select(a => (Int32)a)
+                .ToArray();
+        }
+
+        /// <summary>
+        /// Get the individual digits that make up this number (must be positive)
+        /// </summary>
+        /// <param name="source">The source number</param>
+        /// <returns>An array of digits in order</returns>
+        public static Int64[] GetDigits(this Int64 input)
+        {
+            if (input < 0)
+                throw new ArgumentException($"The given input must be positive.", nameof(input));
+
+            // We can use the uint function and down-cast them since we know the digits are really just 0-9
+            return ((UInt64)input)
+                .GetDigits()
+                .Select(a => (Int64)a)
+                .ToArray();
+        }
     }
 }
