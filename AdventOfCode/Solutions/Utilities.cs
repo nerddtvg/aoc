bb171efc7552ab0e95aa948e66d5028d89e832e2
Drew <vsts@internal.cloudapp.net>
2021-09-16 10:35:05 -0500

    Added a lot of comments

--- AdventOfCode/Solutions/Utilities.cs
+++ AdventOfCode/Solutions/Utilities.cs
@@ -24,25 +24,60 @@ namespace AdventOfCode.Solutions
             }
         }
 
+        /// <summary>
+        /// Get the Manhatten Distance between two (x, y) points
+        /// </summary>
+        /// <param name="a">A tuple of integer (x, y) values</param>
+        /// <param name="b">A tuple of integer (x, y) values</param>
+        /// <returns>An <see cref="System.Int32"/> distance</returns>
         public static int ManhattanDistance((int x, int y) a, (int x, int y) b)
         {
             return Math.Abs(a.x - b.x) + Math.Abs(a.y - b.y);
         }
 
+        /// <summary>
+        /// Get the Manhatten Distance between two (x, y, z) points
+        /// </summary>
+        /// <param name="a">A tuple of integer (x, y, z) values</param>
+        /// <param name="b">A tuple of integer (x, y, z) values</param>
+        /// <returns>An <see cref="System.Int32"/> distance</returns>
         public static int ManhattanDistance((int x, int y, int z) a, (int x, int y, int z) b)
         {
             return Math.Abs(a.x - b.x) + Math.Abs(a.y - b.y) + Math.Abs(a.z - b.z);
         }
 
+        /// <summary>
+        /// Finds the Greatest Common Denominator between two numbers
+        /// </summary>
+        /// <param name="a">The first number</param>
+        /// <param name="b">The second number</param>
+        /// <returns>The discovered GCD</returns>
         public static double FindGCD(double a, double b) => (a % b == 0) ? b : FindGCD(b, a % b);
 
+        /// <summary>
+        /// Finds the Lowest Common Multiple between two numbers
+        /// </summary>
+        /// <param name="a">The first number</param>
+        /// <param name="b">The second number</param>
+        /// <returns>The discovered LCM</returns>
         public static double FindLCM(double a, double b) => a * b / FindGCD(a, b);
 
+        /// <summary>
+        /// Repeats a given <paramref name="action"/> <paramref name="count"/> times
+        /// </summary>
+        /// <param name="action">The action to repeat</param>
+        /// <param name="count">The number of times to repeat the given <paramref name="action"/></param>
         public static void Repeat(this Action action, int count)
         {
             for(int i = 0; i < count; i++) action();
         }
 
+        /// <summary>
+        /// Add two (x, y) points together
+        /// </summary>
+        /// <param name="a">A tuple of integer (x, y) values</param>
+        /// <param name="b">A tuple of integer (x, y) values</param>
+        /// <returns>A tuple of integer (x, y) values</returns>
         public static (int, int) Add(this (int x, int y) a, (int x, int y) b) => (a.x + b.x, a.y + b.y);
     }
 }
