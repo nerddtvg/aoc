1a785b6aa20fd14dfc4cea2d7c3e81c4e7b216de
Drew <vsts@internal.cloudapp.net>
2021-09-20 14:04:38 -0500

    2015 Day 19 Part 2

--- AdventOfCode/Solutions/Year2015/Day19/Solution.cs
+++ AdventOfCode/Solutions/Year2015/Day19/Solution.cs
@@ -138,125 +138,30 @@ namespace AdventOfCode.Solutions.Year2015
             return replacements.Count.ToString();
         }
 
-        /// <summary>
-        /// Returns a Regex similar to: (ABc|Dfa|G|DFasF) based on all of the replacements possible
-        /// </summary>
-        private Regex GetPart2Regex()
-        {
-            var regexStart = string.Join("|", this.reverseReplacements.Keys.Select(a => a));
-
-            // This is a case-sensitive search
-            return new Regex($"({regexStart})");
-        }
-
         protected override string SolvePartTwo()
         {
             // Start with 'e' and see how many steps it takes to make the medicine in originalFormula
             // We're going to reverse process this
             // We know that none of the replacements are duplicates, so we can find all possible reverse replacements in the formula
-            var regex = GetPart2Regex();
-            var currentStepResults = new HashSet<string>() { originalFormula };
-            var historySteps = new SortedSet<string>() { originalFormula };
-            int steps = 1;
 
-            var breakFormula = "e";
+            // The process going forward is impossible (way too many combinations)
+            // The process going backwards is possible, however with this code it takes too long
+            // I went to the reddit megathread and found this:
+            // https://old.reddit.com/r/adventofcode/comments/3xflz8/day_19_solutions/cy4etju/
+            // You can think of Rn Y Ar as the characters ( , ):
+            // The new formula is count(tokens) - count("(" or ")") - 2*count(",") - 1.
 
-            // Now we need to go through each replacement and process it as a possibility
-            while(true)
-            {
-                var tempStepResults = new HashSet<string>();
-                bool found = false;
+            // First we need to count all of our tokens in the original formula
+            var regex = new Regex("([A-Z][a-z]*)");
+            var matches = regex.Matches(originalFormula);
 
-                foreach(var thisFormula in currentStepResults)
-                {
-                    var matches = regex.Matches(thisFormula);
+            var tokensTotal = matches.Count;
+            var RnTotal = matches.Count(m => m.Value == "Rn");
+            var YTotal = matches.Count(m => m.Value == "Y");
+            var ArTotal = matches.Count(m => m.Value == "Ar");
 
-                    // For each match, we can (a) replace it or (b) ignore it, so let's find all of our possible replacements
-                    foreach(Match match in matches)
-                    {
-                        // Get the before and after of this match
-                        var beginFormula = match.Index > 0 ? thisFormula.Substring(0, match.Index) : string.Empty;
-                        var endFormula = match.Index + match.Length < thisFormula.Length ? thisFormula.Substring(match.Index + match.Length) : string.Empty;
-
-                        // Replace it and ensure we haven't tried this one before
-                        var tempFormula = $"{beginFormula}{this.reverseReplacements[match.Value]}{endFormula}";
-
-                        // See if we have found the stopping point
-                        found = string.Equals(tempFormula, breakFormula);
-                        if (found) break;
-
-                        // Saves on memory, put into a sortedset will make searching a little faster
-                        var md5 = GetMD5Hash(tempFormula);
-
-                        if (!historySteps.Contains(md5))
-                        {
-                            tempStepResults.Add(tempFormula);
-                            historySteps.Add(md5);
-                        }
-                    }
-
-                    // Going to shortcut this instead of scanning 1 million+
-                    // Assume we need to find some of the shortest results after replacement
-                    tempStepResults = tempStepResults.OrderBy(a => a.Length).Take(2000).ToHashSet();
-                }
-
-                // If we have found a match, we're done
-                if (found)
-                {
-                    break;
-                }
-
-                // Start new
-                currentStepResults = tempStepResults;
-
-                // Increment
-                steps++;
-            }
-
-            return steps.ToString();
-
-            var currentFormula = new HashSet<string>() { "e" };
-            while(true)
-            {
-                var tempFormulas = new HashSet<string>();
-
-                foreach (var formula in currentFormula)
-                {
-                    var stepList = ProcessFormulaStep(formula);
-                    
-                    foreach(var t in stepList)
-                        tempFormulas.Add(t);
-                }
-
-                // Identify if we have created the formula or not
-                if (tempFormulas.Contains(originalFormula))
-                {
-                    break;
-                }
-
-                // It does not, continue on with new formulas
-                currentFormula = tempFormulas;
-                steps++;
-            }
-
-            return steps.ToString();
-        }
-
-        private string GetMD5Hash(string input)
-        {
-            using (var md5 = System.Security.Cryptography.MD5.Create())
-            {
-                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
-                byte[] hashBytes = md5.ComputeHash(inputBytes);
-
-                // Convert the byte array to hexadecimal string
-                StringBuilder sb = new StringBuilder();
-                for (int i = 0; i < hashBytes.Length; i++)
-                {
-                    sb.Append(hashBytes[i].ToString("X2"));
-                }
-                return sb.ToString();
-            }
+            // Huge thank you to /u/askalski
+            return (tokensTotal - RnTotal - ArTotal - (2*YTotal) - 1).ToString();
         }
     }
 }
