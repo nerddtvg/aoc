4cf347c4cea6d8c896d4644454d1aba6a2abdfbd
Drew <vsts@internal.cloudapp.net>
2021-09-20 20:07:23 -0500

    2016 Day 3 Part 1

--- AdventOfCode/Solutions/Year2016/Day03/Solution.cs
+++ AdventOfCode/Solutions/Year2016/Day03/Solution.cs
@@ -10,15 +10,29 @@ namespace AdventOfCode.Solutions.Year2016
 
     class Day03 : ASolution
     {
+        private List<(int x, int y, int z)> triangles = new List<(int x, int y, int z)>();
 
         public Day03() : base(03, 2016, "")
         {
+            // Process the input with regex
+            var regex = new Regex(@"([0-9]+)\s+([0-9]+)\s+([0-9]+)");
+            foreach (var line in Input.SplitByNewline(true))
+            {
+                var match = regex.Match(line);
 
+                if (match.Success)
+                {
+                    triangles.Add((Int32.Parse(match.Groups[1].Value), Int32.Parse(match.Groups[2].Value), Int32.Parse(match.Groups[3].Value)));
+                }
+            }
         }
+
+        private bool IsValidTriangle((int x, int y, int z) tri) =>
+            (tri.x + tri.y) > tri.z && (tri.x + tri.z) > tri.y && (tri.y + tri.z) > tri.x;
 
         protected override string SolvePartOne()
         {
-            return null;
+            return triangles.Count(t => IsValidTriangle(t)).ToString();
         }
 
         protected override string SolvePartTwo()
