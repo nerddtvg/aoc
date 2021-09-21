541f69941ebd727e5f7589a502ae085604161f69
Drew <vsts@internal.cloudapp.net>
2021-09-20 19:01:30 -0500

    2016 Day 1 Part 1

--- AdventOfCode/Solutions/Year2016/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2016/Day01/Solution.cs
@@ -7,18 +7,97 @@ using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2016
 {
+    enum Direction
+    {
+        North   = 0,
+        East    = 1,
+        South   = 2,
+        West    = 3
+    }
 
     class Day01 : ASolution
     {
+        public (int x, int y) pos { get; set; } = (0, 0);
+        public Direction dir { get; set; } = Direction.North;
 
         public Day01() : base(01, 2016, "")
         {
+            DebugInput = "R2, L3";
+            ResetPos();
+            ProcessInput();
+            Console.WriteLine($"Sample '{Input}': {Utilities.ManhattanDistance((0, 0), pos)}");
+            
+            DebugInput = "R2, R2, R2";
+            ResetPos();
+            ProcessInput();
+            Console.WriteLine($"Sample '{Input}': {Utilities.ManhattanDistance((0, 0), pos)}");
+            
+            DebugInput = "R5, L5, R5, R3";
+            ResetPos();
+            ProcessInput();
+            Console.WriteLine($"Sample '{Input}': {Utilities.ManhattanDistance((0, 0), pos)}");
+            
+            DebugInput = "R2, R2, R2, R2";
+            ResetPos();
+            ProcessInput();
+            Console.WriteLine($"Sample '{Input}': {Utilities.ManhattanDistance((0, 0), pos)}");
 
+            // Real work
+            DebugInput = null;
+            ResetPos();
+            ProcessInput();
+        }
+
+        private void ResetPos()
+        {
+            this.dir = Direction.North;
+            this.pos = (0, 0);
+        }
+
+        private void ProcessInput()
+        {
+            // Our directions!
+            var regex = new Regex("([LR])([0-9]+)+");
+            var matches = regex.Matches(Input);
+
+            // For each of these, figure out our position starting from 0,0 facing north (0)
+            foreach(Match match in matches)
+            {
+                // Change direction (keep it from going negative for modulus math)
+                int newDir = ((int)dir + (match.Groups[1].Value == "L" ? 3 : 1));
+                dir = (Direction)(newDir % 4);
+
+                (int x, int y) move = (0, 0);
+
+                var steps = Int32.Parse(match.Groups[2].Value);
+
+                switch(dir)
+                {
+                    case Direction.North:
+                        move.y = steps;
+                        break;
+                        
+                    case Direction.East:
+                        move.x = steps;
+                        break;
+                        
+                    case Direction.South:
+                        move.y = -1 * steps;
+                        break;
+                        
+                    case Direction.West:
+                        move.x = -1 * steps;
+                        break;
+                }
+
+                // Move!
+                pos = Utilities.Add(pos, move);
+            }
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            return Utilities.ManhattanDistance((0, 0), pos).ToString();
         }
 
         protected override string SolvePartTwo()
