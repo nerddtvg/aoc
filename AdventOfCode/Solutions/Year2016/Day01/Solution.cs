7602604fe13b07339978c20eef3d3d740f85f34e
Drew <vsts@internal.cloudapp.net>
2021-09-20 19:42:53 -0500

    2016 Day 1 Part 2

--- AdventOfCode/Solutions/Year2016/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2016/Day01/Solution.cs
@@ -20,8 +20,13 @@ namespace AdventOfCode.Solutions.Year2016
         public (int x, int y) pos { get; set; } = (0, 0);
         public Direction dir { get; set; } = Direction.North;
 
+        private List<(int x, int y)> stack = new List<(int x, int y)>();
+
+        private (int x, int y) firstDup = (0, 0);
+
         public Day01() : base(01, 2016, "")
         {
+            /*
             DebugInput = "R2, L3";
             ResetPos();
             ProcessInput();
@@ -41,6 +46,12 @@ namespace AdventOfCode.Solutions.Year2016
             ResetPos();
             ProcessInput();
             Console.WriteLine($"Sample '{Input}': {Utilities.ManhattanDistance((0, 0), pos)}");
+            */
+            
+            DebugInput = "R8, R4, R4, R8";
+            ResetPos();
+            ProcessInput();
+            Console.WriteLine($"Part 2 Sample '{Input}': {Utilities.ManhattanDistance((0, 0), this.firstDup)}");
 
             // Real work
             DebugInput = null;
@@ -52,6 +63,10 @@ namespace AdventOfCode.Solutions.Year2016
         {
             this.dir = Direction.North;
             this.pos = (0, 0);
+            this.firstDup = (0, 0);
+
+            // A dictionary works better for the key handling/searching
+            this.stack = new List<(int x, int y)>();
         }
 
         private void ProcessInput()
@@ -59,6 +74,8 @@ namespace AdventOfCode.Solutions.Year2016
             // Our directions!
             var regex = new Regex("([LR])([0-9]+)+");
             var matches = regex.Matches(Input);
+
+            bool foundFirst = false;
 
             // For each of these, figure out our position starting from 0,0 facing north (0)
             foreach(Match match in matches)
@@ -91,18 +108,37 @@ namespace AdventOfCode.Solutions.Year2016
                 }
 
                 // Move!
-                pos = Utilities.Add(pos, move);
+                var tempPos = this.pos.Add(move);
+
+                // Check if we found this twice
+                if (!foundFirst)
+                {
+                    foreach(var pt in this.pos.GetPointsBetweenInclusive(tempPos).Skip(1))
+                    {
+                        if (this.stack.Contains(pt))
+                        {
+                            foundFirst = true;
+                            this.firstDup = pt;
+                            break;
+                        }
+
+                        // Add it to the stack
+                        this.stack.Add(pt);
+                    }
+                }
+
+                this.pos = tempPos;
             }
         }
 
         protected override string SolvePartOne()
         {
-            return Utilities.ManhattanDistance((0, 0), pos).ToString();
+            return Utilities.ManhattanDistance((0, 0), this.pos).ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            return Utilities.ManhattanDistance((0, 0), this.firstDup).ToString();
         }
     }
 }
