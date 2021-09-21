edbc689e6ec18277704f52e642adecab1c240057
Drew <vsts@internal.cloudapp.net>
2021-09-21 11:30:41 -0500

    2016 Day 6 Part 1

--- AdventOfCode/Solutions/Year2016/Day06/Solution.cs
+++ AdventOfCode/Solutions/Year2016/Day06/Solution.cs
@@ -10,15 +10,41 @@ namespace AdventOfCode.Solutions.Year2016
 
     class Day06 : ASolution
     {
+        private Dictionary<int, List<char>> characters { get; set; }
 
         public Day06() : base(06, 2016, "")
         {
+            LoadChars();
+        }
 
+        private void LoadChars()
+        {
+            this.characters = new Dictionary<int, List<char>>();
+
+            // Figure out the line length first to initialize the dictionary
+            var l0 = Input.SplitByNewline().First();
+
+            for (int i = 0; i < l0.Length; i++)
+                this.characters[i] = new List<char>();
+
+            // Now add each character to the list
+            foreach(var line in Input.SplitByNewline())
+            {
+                int count = 0;
+                foreach(var c in line)
+                {
+                    this.characters[count].Add(c);
+                    count++;
+                }
+            }
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            return this.characters.Keys.Select(k =>
+            {
+                return this.characters[k].GroupBy(c => c).OrderByDescending(c => c.Count()).First().Key;
+            }).JoinAsString();
         }
 
         protected override string SolvePartTwo()
