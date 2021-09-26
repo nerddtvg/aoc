f9fe989f60de3a678d3f9232598c9fb0194f0046
Drew <vsts@internal.cloudapp.net>
2021-09-26 13:59:24 -0500

    2016 Day 7 Part 2

--- AdventOfCode/Solutions/Year2016/Day07/Solution.cs
+++ AdventOfCode/Solutions/Year2016/Day07/Solution.cs
@@ -26,9 +26,10 @@ namespace AdventOfCode.Solutions.Year2016
             // Find all of the valid items
             var regex = new Regex(@"([a-z]+|\[[a-z]+\])");
 
+            var aba_str = new List<string>();
+
             // Brute force build this matcher
             var abba_str = string.Empty;
-            var aba_str = string.Empty;
             for (char a = 'a'; a <= 'z'; a++)
             {
                 for (char b = 'a'; b <= 'z'; b++)
@@ -37,13 +38,12 @@ namespace AdventOfCode.Solutions.Year2016
                         continue;
 
                     abba_str += $"{a}{b}{b}{a}|";
-                    aba_str += $"{a}{b}{a}|";
+                    aba_str.Add($"{a}{b}{a}");
                 }
             }
 
             // Trim off the trailing '|'
             var abba_regex = new Regex($"({abba_str.Substring(0, abba_str.Length - 1)})");
-            var aba_regex = new Regex($"({aba_str.Substring(0, aba_str.Length - 1)})");
 
             foreach (var line in Input.SplitByNewline())
             {
@@ -66,13 +66,17 @@ namespace AdventOfCode.Solutions.Year2016
                     if (supernets.Any(s => abba_regex.IsMatch(s.Value)) && !hypernets.Any(h => abba_regex.IsMatch(h.Value)))
                         this.matchesTLS.Add(line);
 
+                    // Get every ABA that has a match inside a supernet
+                    var aba_matches = aba_str
+                        .SelectMany(aba => supernets.Where(s => s.Value.Contains(aba)).Select(s => aba))
+                        .Distinct()
+                        .ToList();
+
                     // Part 2
-                    if (supernets.Any(s => aba_regex.IsMatch(s.Value)))
+                    if (aba_matches.Count > 0)
                     {
                         // For each of the psossible matches, we need to find if we have a BAB sequence
-                        var groups = supernets.SelectMany(s => aba_regex.Matches(s.Value)).SelectMany(s => s.Groups.Values).Select(g => g.Value).Distinct().ToList();
-
-                        foreach(var g in groups)
+                        foreach(var g in aba_matches)
                         {
                             if (g.Length != 3)
                                 continue;
