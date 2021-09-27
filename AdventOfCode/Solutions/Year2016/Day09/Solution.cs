7d5be045c0b11981224ed6ba4bd581fe2389b44e
Drew <vsts@internal.cloudapp.net>
2021-09-26 20:16:24 -0500

    2016 Day 9 Part 2 Incomplete

--- AdventOfCode/Solutions/Year2016/Day09/Solution.cs
+++ AdventOfCode/Solutions/Year2016/Day09/Solution.cs
@@ -10,139 +10,66 @@ namespace AdventOfCode.Solutions.Year2016
 
     class Day09 : ASolution
     {
-        private string decompressed = string.Empty;
-
         public Day09() : base(09, 2016, "")
         {
 
         }
 
-        private void DecompressInput(string input)
+        private uint GetLength(string input, int part = 1)
         {
-            this.decompressed = string.Empty;
+            // Shortcut: if there are no parenthesis, we don't do anything
+            if (!input.Contains('('))
+                return (uint)input.Length;
 
-            var insideData = false;
-            var insideMarker = false;
+            // This is mostly math
+            uint ret = 0;
+            char[] arr = input.ToCharArray();
 
-            // When inside a marker, are we counting the data length or repeating section
-            var countingRepeat = false;
-
-            // How long the data section is
-            var strDataLength = string.Empty;
-            int dataLength = 0;
-
-            // What character we're on inside a data section
-            int count = 0;
-
-            // How many times to repeat it
-            var strRepeatCount = string.Empty;
-            int repeatCount = 0;
-
-            // The data section pulled out
-            var tempData = string.Empty;
-
-            foreach(var c in input)
+            for (int i = 0; i < input.Length; i++)
             {
-                // For each character, we need to determine what to do
-                if (insideData)
+                // We need to figure out what to do
+                if (arr[i] == '(')
                 {
-                    // This character gets added to our data length
-                    tempData += c;
+                    // Start of a marker!
+                    // Get the length of this and return the length of GetLength(entire_marker)
+                    var nextX = input.IndexOf('x', i);
+                    var nextParen = input.IndexOf(')', nextX);
 
-                    // Count our character
-                    count++;
+                    int length = Int32.Parse(input.Substring(i + 1, nextX - (i + 1)));
+                    uint repeat = uint.Parse(input.Substring(nextX + 1, nextParen - (nextX + 1)));
 
-                    if (count == dataLength)
+                    // If this is part 1, just repeat this string the desired times
+                    if (part == 1)
                     {
-                        // We've finished this section, reset!
-                        for (int i = 0; i < repeatCount; i++)
-                        {
-                            this.decompressed += tempData;
-                        }
-
-                        // Reset the values
-                        tempData = string.Empty;
-                        insideData = false;
-                        insideMarker = false;
-                        dataLength = 0;
-                        repeatCount = 0;
-                    }
-                }
-                else if (insideMarker)
-                {
-                    // We're inside the marker so we are determining what to do
-                    if (c == ')')
-                    {
-                        // Make sure we're good
-                        if (!countingRepeat)
-                            throw new Exception("Invalid ')' inside a marker but not the repeating length");
-
-                        // Marker is done, parse the integers
-                        dataLength = Int32.Parse(strDataLength);
-                        repeatCount = Int32.Parse(strRepeatCount);
-
-                        insideData = true;
-                        insideMarker = false;
-                        
-                        strDataLength = string.Empty;
-                        strRepeatCount = string.Empty;
-
-                        countingRepeat = false;
-                        count = 0;
-                    }
-                    else if (c == 'x')
-                    {
-                        // Split between length and count
-                        countingRepeat = true;
+                        ret += repeat * (uint) length;
                     }
                     else
                     {
-                        // Just append this character
-                        if (countingRepeat)
-                            strRepeatCount += c;
-                        else
-                            strDataLength += c;
+                        // Part 2: Now we gather the string up and process it
+                        ret += repeat * GetLength(input.Substring(nextParen + 1, length), 2);
                     }
+
+                    // Don't add one here because the loop iterator will do that
+                    i = nextParen + length;
                 }
                 else
                 {
-                    // We either have a '(' to denote a marker or just a random character
-                    if (c == '(')
-                    {
-                        insideMarker = true;
-                        
-                        strDataLength = string.Empty;
-                        strRepeatCount = string.Empty;
-
-                        dataLength = 0;
-                        repeatCount = 0;
-                        count = 0;
-                    }
-                    else
-                    {
-                        // Add this non-important character to the string
-                        this.decompressed += c;
-                    }
+                    // Add one for this character
+                    ret++;
                 }
             }
 
-            // At the very end, make sure we were not in the middle of something
-            if (insideData)
-            {
-                // Found invalid data I guess?
-                throw new Exception($"Invalid data at the end: {tempData}");
-            }
+            return ret;
         }
 
         protected override string SolvePartOne()
         {
-            DecompressInput(Input);
-            return this.decompressed.Length.ToString();
+            return GetLength(Input).ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            return GetLength(Input, 2).ToString();
         }
     }
 }
