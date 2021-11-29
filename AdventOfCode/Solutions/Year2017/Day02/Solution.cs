de00dd313a791780a02113050a6e827dc7009acd
Drew <nerddtvg@users.noreply.github.com>
2021-11-29 10:31:59 -0600

    2017 Day 2

--- AdventOfCode/Solutions/Year2017/Day02/Solution.cs
+++ AdventOfCode/Solutions/Year2017/Day02/Solution.cs
@@ -13,19 +13,42 @@ namespace AdventOfCode.Solutions.Year2017
     class Day02 : ASolution
     {
 
-        public Day02() : base(02, 2017, "")
+        public Day02() : base(02, 2017, "Corruption Checksum")
         {
 
+        }
+
+        private int DiffRow(IEnumerable<int> values) =>
+            values.Max() - values.Min();
+
+        private IEnumerable<int> GetRow(string row) =>
+            row.Split('\t').Select(i => Int32.Parse(i)).ToArray();
+
+        private int GetDivisors(IEnumerable<int> values)
+        {
+            var vals = values.OrderByDescending(v => v).ToList();
+
+            for (int i = 0; i < vals.Count; i++)
+            {
+                for (int q = i + 1; q < vals.Count; q++)
+                {
+                    if (vals[i] % vals[q] == 0)
+                        return vals[i] / vals[q];
+                }
+            }
+
+            // Backup:
+            return 0;
         }
 
         protected override string? SolvePartOne()
         {
-            return null;
+            return Input.SplitByNewline().Sum(row => DiffRow(GetRow(row))).ToString();
         }
 
         protected override string? SolvePartTwo()
         {
-            return null;
+            return Input.SplitByNewline().Sum(row => GetDivisors(GetRow(row))).ToString();
         }
     }
 }
