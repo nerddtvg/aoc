de1db1a6e95d4bd869647bb9aced1f3cf952abd6
Drew <nerddtvg@users.noreply.github.com>
2021-11-29 09:55:20 -0600

    2017 Day 1 Part 1

--- AdventOfCode/Solutions/Year2017/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2017/Day01/Solution.cs
@@ -13,14 +13,26 @@ namespace AdventOfCode.Solutions.Year2017
     class Day01 : ASolution
     {
 
-        public Day01() : base(01, 2017, "")
+        public Day01() : base(01, 2017, "Inverse Captcha")
         {
-
+            
         }
 
         protected override string? SolvePartOne()
         {
-            return null;
+            var split = Input.ToCharArray();
+            var total = 0;
+            for (int i = 0; i < split.Length; i++)
+            {
+                var next = (i + 1) % split.Length;
+
+                if (split[i] == split[next])
+                {
+                    total += Int32.Parse(split[i].ToString());
+                }
+            }
+
+            return total.ToString();
         }
 
         protected override string? SolvePartTwo()
