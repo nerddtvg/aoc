02a293f7f988cfc5e25ccb474224efc2e155e0ab
Drew <nerddtvg@users.noreply.github.com>
2021-12-02 15:13:50 -0600

    2017 Day 6 Pat 1

--- AdventOfCode/Solutions/Year2017/Day06/Solution.cs
+++ AdventOfCode/Solutions/Year2017/Day06/Solution.cs
@@ -12,15 +12,75 @@ namespace AdventOfCode.Solutions.Year2017
 
     class Day06 : ASolution
     {
+        private SortedSet<string> history = new SortedSet<string>();
+        private LinkedList<int> memoryBanks = new LinkedList<int>();
 
         public Day06() : base(06, 2017, "")
         {
 
         }
 
+        private void Reset()
+        {
+            memoryBanks.Clear();
+            history.Clear();
+
+            foreach(var entry in Input.Split('\t', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
+            {
+                memoryBanks.AddLast(Int32.Parse(entry));
+            }
+        }
+
+        private string GetHash() => string.Join('-', memoryBanks);
+
+        /// <summary>
+        /// Balances the memory and returns a string hash of this configuration for comparison
+        /// </summary>
+        private string BalanceMemory()
+        {
+            var max = memoryBanks.Max();
+            var node = memoryBanks.FirstNode(v => v == max);
+
+            // Clear this node
+            node.Value = 0;
+
+            while(max > 0)
+            {
+                node = node.Next != null ? node.Next : memoryBanks.First;
+
+                // Avoid a warning
+                if (node == null)
+                    throw new InvalidOperationException();
+
+                // Increase / decrease
+                node.Value++;
+                max--;
+            }
+
+            // We're done with this round, generate a hash
+            return GetHash();
+        }
+
         protected override string? SolvePartOne()
         {
-            return null;
+            Reset();
+
+            var count = 0;
+
+            // Add initial state to history
+            this.history.Add(GetHash());
+
+            do
+            {
+                count++;
+
+                var hash = BalanceMemory();
+
+                if (this.history.Contains(hash))
+                    return count.ToString();
+
+                this.history.Add(hash);
+            } while (true);
         }
 
         protected override string? SolvePartTwo()
