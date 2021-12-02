a5e0306b8f03522e674fb315c9aac1d3ff6ac6ce
Drew <nerddtvg@users.noreply.github.com>
2021-12-02 08:32:51 -0600

    2021 Day 2 Generalized

--- AdventOfCode/Solutions/Year2021/Day02/Solution.cs
+++ AdventOfCode/Solutions/Year2021/Day02/Solution.cs
@@ -12,77 +12,73 @@ namespace AdventOfCode.Solutions.Year2021
 
     class Day02 : ASolution
     {
+        int part1 = 0, part2 = 0;
 
         public Day02() : base(02, 2021, "Dive!")
         {
+            // Generalized
+            int ypos = 0;
+            int xpos = 0;
+            
+            int ypos2 = 0;
+            int xpos2 = 0;
+            int aim2 = 0;
 
+            // Changed to handle both processes at the same time
+            foreach(var line in Input.SplitByNewline(true))
+            {
+                var str = line.Split(' ', 2);
+                int val = Int32.Parse(str[1]);
+
+                switch(str[0])
+                {
+                    case "forward":
+                        // Part 1
+                        xpos += val;
+                        
+                        // Part 2
+                        xpos2 += val;
+                        ypos2 += aim2 * val;
+                        break;
+                        
+                    case "up":
+                        // Part 1
+                        ypos -= val;
+                        
+                        // Part 2
+                        aim2 -= val;
+                        break;
+                        
+                    case "down":
+                        // Part 1
+                        ypos += val;
+                        
+                        // Part 2
+                        aim2 += val;
+                        break;
+                }
+            }
+
+            Console.WriteLine($"[Part 1] X Pos: {xpos}");
+            Console.WriteLine($"[Part 1] Y Pos: {ypos}");
+            
+            Console.WriteLine($"[Part 2] X Pos: {xpos2}");
+            Console.WriteLine($"[Part 2] Y Pos: {ypos2}");
+            Console.WriteLine($"[Part 2]   Aim: {aim2}");
+
+            // Save the answers
+            this.part1 = xpos * ypos;
+            this.part2 = xpos2 * ypos2;
         }
 
         protected override string? SolvePartOne()
         {
-            int ypos = 0;
-            int xpos = 0;
-
-            foreach(var line in Input.SplitByNewline(true))
-            {
-                var str = line.Split(' ', 2);
-                int val = Int32.Parse(str[1]);
-
-                switch(str[0])
-                {
-                    case "forward":
-                        xpos += val;
-                        break;
-                        
-                    case "up":
-                        ypos -= val;
-                        break;
-                        
-                    case "down":
-                        ypos += val;
-                        break;
-                }
-            }
-
-            Console.WriteLine($"X Pos: {xpos}");
-            Console.WriteLine($"Y Pos: {ypos}");
-
-            return (xpos*ypos).ToString();
+            return (this.part1).ToString();
         }
 
         protected override string? SolvePartTwo()
         {
-            int ypos = 0;
-            int xpos = 0;
-            int aim = 0;
-
-            foreach(var line in Input.SplitByNewline(true))
-            {
-                var str = line.Split(' ', 2);
-                int val = Int32.Parse(str[1]);
-
-                switch(str[0])
-                {
-                    case "forward":
-                        xpos += val;
-                        ypos += aim * val;
-                        break;
-                        
-                    case "up":
-                        aim -= val;
-                        break;
-                        
-                    case "down":
-                        aim += val;
-                        break;
-                }
-            }
-
-            Console.WriteLine($"X Pos: {xpos}");
-            Console.WriteLine($"Y Pos: {ypos}");
-            Console.WriteLine($"Aim:   {aim}");
-
-            return (xpos*ypos).ToString();
+            return (this.part2).ToString();
         }
     }
 }
