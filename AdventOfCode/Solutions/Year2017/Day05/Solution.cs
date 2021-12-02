5a676b2ec57e71d7d39e9625933e9e8c759d2327
Drew <nerddtvg@users.noreply.github.com>
2021-12-02 11:11:35 -0600

    2017 Day 5 Part 1

--- AdventOfCode/Solutions/Year2017/Day05/Solution.cs
+++ AdventOfCode/Solutions/Year2017/Day05/Solution.cs
@@ -12,15 +12,54 @@ namespace AdventOfCode.Solutions.Year2017
 
     class Day05 : ASolution
     {
+        private List<int> instructions = new List<int>();
+        int pos = 0;
 
-        public Day05() : base(05, 2017, "")
+        public Day05() : base(05, 2017, "A Maze of Twisty Trampolines, All Alike")
         {
+            ResetList();
+        }
 
+        private void ResetList()
+        {
+            this.instructions = Input.SplitByNewline(true).Select(i => Int32.Parse(i)).ToList();
+        }
+
+        private bool Run()
+        {
+            // Order of operations:
+            // Move to new position (if outside, exit with false)
+            // Increment old position by 1
+            int oldPos = this.pos;
+
+            // Being safe here
+            if (this.pos < 0 || instructions.Count < this.pos)
+                return false;
+
+            // Determine our destination
+            var newPos = this.pos + instructions[this.pos];
+
+            if (newPos > instructions.Count-1 || newPos < 0)
+                return false;
+
+            // Move
+            this.pos = newPos;
+
+            // New value at old position
+            instructions[oldPos]++;
+
+            return true;
         }
 
         protected override string? SolvePartOne()
         {
-            return null;
+            int count = 0;
+            do
+            {
+                count++;
+            } while (Run());
+
+            return count.ToString();
         }
 
         protected override string? SolvePartTwo()
