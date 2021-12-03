a5da95e4b34ca922ba116ac436a6d3809d08733e
Drew <nerddtvg@users.noreply.github.com>
2021-12-02 19:38:02 -0600

    2017 Day 7 Part 1

--- AdventOfCode/Solutions/Year2017/Day07/Solution.cs
+++ AdventOfCode/Solutions/Year2017/Day07/Solution.cs
@@ -12,15 +12,73 @@ namespace AdventOfCode.Solutions.Year2017
 
     class Day07 : ASolution
     {
+        private class Tower
+        {
+            public string id { get; set; } = string.Empty;
+            public int weight { get; set; } = 0;
+            public Tower? parent { get; set; } = null;
+            public List<Tower> children { get; set; } = new List<Tower>();
+        }
+
+        private List<Tower> towers = new List<Tower>();
 
         public Day07() : base(07, 2017, "")
         {
 
         }
 
+        private Tower AddTower(string id)
+        {
+            // First, find if this exists already
+            var tower = this.towers.FirstOrDefault(t => t.id == id);
+
+            if (tower == default)
+            {
+                tower = new Tower()
+                {
+                    id = id
+                };
+
+                this.towers.Add(tower);
+            }
+
+            return tower;
+        }
+
+        private void ReadInput()
+        {
+            this.towers.Clear();
+
+            foreach(var line in Input.SplitByNewline(true))
+            {
+                var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+                // First, find if this exists already
+                var tower = AddTower(parts[0]);
+
+                // Set the weight
+                tower.weight = Int32.Parse(parts[1].Replace("(", "").Replace(")", ""));
+
+                // If we have children
+                if (parts.Length > 2)
+                {
+                    // Part 2 is '->', we skip that
+                    for (int i = 3; i < parts.Length; i++)
+                    {
+                        var child = AddTower(parts[i].Replace(",", ""));
+
+                        // Set the parent
+                        child.parent = tower;
+                    }
+                }
+            }
+        }
+
         protected override string? SolvePartOne()
         {
-            return null;
+            ReadInput();
+
+            return this.towers.First(tower => tower.parent == null).id;
         }
 
         protected override string? SolvePartTwo()
