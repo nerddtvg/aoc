433847634711e9709a9fecb38f3c3ede4f708429
Drew <nerddtvg@users.noreply.github.com>
2021-12-07 08:19:50 -0600

    2021 Day 7 Part 1

--- AdventOfCode/Solutions/Year2021/Day07/Solution.cs
+++ AdventOfCode/Solutions/Year2021/Day07/Solution.cs
@@ -12,15 +12,35 @@ namespace AdventOfCode.Solutions.Year2021
 
     class Day07 : ASolution
     {
+        private List<int> crabs = new List<int>();
+        private int minFuel = Int32.MaxValue;
+        private int minPos = Int32.MaxValue;
 
         public Day07() : base(07, 2021, "")
         {
-
+            // Load the crabs!
+            this.crabs = Input.ToIntArray(",").ToList();
         }
 
         protected override string? SolvePartOne()
         {
-            return null;
+            var max = this.crabs.Max();
+
+            // Search from 0 to max
+            // Find out how much fuel is required for that
+            for (int i = 0; i <= max; i++)
+            {
+                var tFuel = this.crabs.Sum(crab => (int) Math.Abs(crab - i));
+
+                if (tFuel < this.minFuel)
+                {
+                    // Found one possible
+                    this.minFuel = tFuel;
+                    this.minPos = i;
+                }
+            }
+
+            return this.minFuel.ToString();
         }
 
         protected override string? SolvePartTwo()
