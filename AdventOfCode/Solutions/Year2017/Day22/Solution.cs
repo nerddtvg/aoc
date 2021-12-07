8ce1a2631609f00aa9456345c15b20fc90f433b6
Drew <nerddtvg@users.noreply.github.com>
2021-12-07 14:57:29 -0600

    2017 Day 22 Part 2

--- AdventOfCode/Solutions/Year2017/Day22/Solution.cs
+++ AdventOfCode/Solutions/Year2017/Day22/Solution.cs
@@ -20,14 +20,24 @@ namespace AdventOfCode.Solutions.Year2017
             Left
         };
 
+        private enum State
+        {
+            Clean,
+            Weakened,
+            Infected,
+            Flagged
+        };
+
         private Dictionary<(int x, int y), bool> grid = new Dictionary<(int x, int y), bool>();
+        private Dictionary<(int x, int y), State> grid2 = new Dictionary<(int x, int y), State>();
 
         int posX = 0;
         int posY = 0;
         Direction dir = Direction.Up;
 
         // Count infected
-        int infectedCount = 0;
+        int infectedCount1 = 0;
+        int infectedCount2 = 0;
 
         public Day22() : base(22, 2017, "Sporifica Virus")
         {
@@ -38,21 +48,42 @@ namespace AdventOfCode.Solutions.Year2017
 
         private bool GetPoint((int x, int y) pos) => this.grid.ContainsKey(pos) ? this.grid[pos] : false;
 
-        private Direction NewDir((int x, int y) pos)
+        private State GetPoint2((int x, int y) pos) => this.grid2.ContainsKey(pos) ? this.grid2[pos] : State.Clean;
+
+        private Direction NewDir((int x, int y) pos, int part)
         {
-            // If this is clean, turn left
-            // If this is infected, turn right
-            if (GetPoint(pos))
+            // Easy, helpful directions
+            var reverse = (Direction)((((int)this.dir) + 2) % 4);
+            var right = (Direction)((((int)this.dir) + 1) % 4);
+            var left = (Direction)((((int)this.dir) + 3) % 4);
+
+            if (part == 1)
             {
-                // Infected
-                return (Direction)((((int)this.dir) + 1) % 4);
+                // If this is clean, turn left
+                // If this is infected, turn right
+                if (GetPoint(pos))
+                {
+                    // Infected
+                    return right;
+                }
+
+                return left;
             }
 
-            // Darn modulo!
-            var val = (((int)this.dir) - 1);
-            if (val < 0) val += 4;
+            // Part 2 has different logic
+            var point = GetPoint2(pos);
 
-            return (Direction)val;
+            if (point == State.Clean)
+                return left;
+
+            // No change
+            if (point == State.Weakened)
+                return this.dir;
+
+            if (point == State.Infected)
+                return right;
+
+            return reverse;
         }
 
         private (int x, int y) GetNewPoint((int x, int y) pos)
@@ -79,7 +110,9 @@ namespace AdventOfCode.Solutions.Year2017
         private void Reset()
         {
             this.grid.Clear();
-            
+
+            this.dir = Direction.Up;
+
             var lines = Input.SplitByNewline(true).ToList();
 
             int y = 0;
@@ -90,6 +123,7 @@ namespace AdventOfCode.Solutions.Year2017
                 foreach(var c in line)
                 {
                     this.grid[(x, y)] = c == '#';
+                    this.grid2[(x, y)] = c == '#' ? State.Infected : State.Clean;
                     x++;
                 }
 
@@ -101,18 +135,27 @@ namespace AdventOfCode.Solutions.Year2017
             this.posY = (int)Math.Ceiling(this.grid.Max(pt => pt.Key.y) / 2.0);
         }
 
-        private void Run()
+        private void Run(int part = 1)
         {
             // Our position
             var pos = (this.posX, this.posY);
 
             // First turn
-            this.dir = NewDir(pos);
+            this.dir = NewDir(pos, part);
 
             // Clean or infect
-            this.grid[pos] = !GetPoint(pos);
-            if (this.grid[pos])
-                this.infectedCount++;
+            if (part == 1)
+            {
+                this.grid[pos] = !GetPoint(pos);
+                if (this.grid[pos])
+                    this.infectedCount1++;
+            }
+            else
+            {
+                this.grid2[pos] = (State)(((int)GetPoint2(pos) + 1) % 4);
+                if (this.grid2[pos] == State.Infected)
+                    this.infectedCount2++;
+            }
 
             // Move!
             (this.posX, this.posY) = GetNewPoint(pos);
@@ -125,12 +168,19 @@ namespace AdventOfCode.Solutions.Year2017
                 Run();
             }, 10000);
 
-            return this.infectedCount.ToString();
+            return this.infectedCount1.ToString();
         }
 
         protected override string? SolvePartTwo()
         {
-            return null;
+            Reset();
+
+            Utilities.Repeat(() =>
+            {
+                Run(2);
+            }, 10000000);
+
+            return this.infectedCount2.ToString();
         }
     }
 }
