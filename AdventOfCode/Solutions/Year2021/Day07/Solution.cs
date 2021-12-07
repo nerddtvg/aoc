6012c134e729aa26ef9152ed90741b656c4a193e
Drew <nerddtvg@users.noreply.github.com>
2021-12-07 08:26:49 -0600

    2021 Day 7 Part 2

--- AdventOfCode/Solutions/Year2021/Day07/Solution.cs
+++ AdventOfCode/Solutions/Year2021/Day07/Solution.cs
@@ -13,17 +13,19 @@ namespace AdventOfCode.Solutions.Year2021
     class Day07 : ASolution
     {
         private List<int> crabs = new List<int>();
-        private int minFuel = Int32.MaxValue;
-        private int minPos = Int32.MaxValue;
 
         public Day07() : base(07, 2021, "")
         {
+            // DebugInput = "16,1,2,0,4,2,7,1,2,14";
+
             // Load the crabs!
             this.crabs = Input.ToIntArray(",").ToList();
         }
 
         protected override string? SolvePartOne()
         {
+            int minPos = Int32.MaxValue, minFuel = Int32.MaxValue;
+
             var max = this.crabs.Max();
 
             // Search from 0 to max
@@ -32,20 +34,43 @@ namespace AdventOfCode.Solutions.Year2021
             {
                 var tFuel = this.crabs.Sum(crab => (int) Math.Abs(crab - i));
 
-                if (tFuel < this.minFuel)
+                if (tFuel < minFuel)
                 {
                     // Found one possible
-                    this.minFuel = tFuel;
-                    this.minPos = i;
+                    minFuel = tFuel;
+                    minPos = i;
                 }
             }
 
-            return this.minFuel.ToString();
+            return minFuel.ToString();
         }
 
         protected override string? SolvePartTwo()
         {
-            return null;
+            int minPos = Int32.MaxValue, minFuel = Int32.MaxValue;
+
+            var max = this.crabs.Max();
+
+            // Search from 0 to max
+            // Find out how much fuel is required for that
+            for (int i = 0; i <= max; i++)
+            {
+                // Part 2: Every step requires 1 additional fuel
+                // 1 => 1
+                // 2 => 1 + 2
+                // 3 => 1 + 2 + 3
+                // This is a summation of a series of n entries A: Sn = (n*(A1 + An))/2
+                var tFuel = this.crabs.Sum(crab => (((int) Math.Abs(crab - i)) * (1 + ((int) Math.Abs(crab - i)))) / 2);
+
+                if (tFuel < minFuel)
+                {
+                    // Found one possible
+                    minFuel = tFuel;
+                    minPos = i;
+                }
+            }
+
+            return minFuel.ToString();
         }
     }
 }
