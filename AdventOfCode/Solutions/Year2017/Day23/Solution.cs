05fc4aca972353357a68765507a68205b6ea1bd8
Drew <nerddtvg@users.noreply.github.com>
2021-12-07 15:22:30 -0600

    Working through the solution slovely

--- AdventOfCode/Solutions/Year2017/Day23/Solution.cs
+++ AdventOfCode/Solutions/Year2017/Day23/Solution.cs
@@ -33,7 +33,70 @@ namespace AdventOfCode.Solutions.Year2017
 
         protected override string? SolvePartTwo()
         {
-            return null;
+            var p = new Day18.SoundProgram(Input);
+
+            /* Refactored into psuedo batch:
+                b = 93
+                c = b
+                if (a != 0) goto A
+                goto B
+                A: mul b 100
+                b += 100000
+                c = b
+                c += 17000
+                B: f = 1
+                d = 2
+                E: e = 2
+                D: g = d
+                g *= e
+                g -= b
+                if (g != 0) goto C
+                f = 0
+                C: e += 1
+                g = e
+                g -= b
+                if (g != 0) goto D
+                d += 1
+                g = d
+                g -= b
+                if (g != 0) goto E
+                if (f != 0) goto F
+                h += 1
+                F: g = b
+                g -= c
+                if (g != 0) goto G
+                goto H (EXIT)
+                G: b += 17
+                goto B
+                H: (EXIT)
+            */
+
+            // Set debug
+            p.SetRegister('a', 1);
+
+            var ret = true;
+            var c = 0;
+            while(ret && c++ < 100)
+            {
+                ret = p.Run();
+
+                // This goes into a pretty crazy loop comparing the values of b and c over and over
+                // To try and short-circuit it, on step 2 we override those values
+                if (c == 2)
+                {
+                    p.SetRegister('b', 4);
+                    //p.SetRegister('c', 0);
+                }
+
+                for (char i = 'a'; i <= 'h'; i++)
+                {
+                    Console.WriteLine($"{i}: {p.GetRegister(i)}");
+                }
+
+                Console.WriteLine("");
+            }
+
+            return p.GetRegister('h').ToString();
         }
     }
 }
