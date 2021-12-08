8dd914d97fd1b9cf0f0bd05867d5d343ee95d46b
Drew <nerddtvg@users.noreply.github.com>
2021-12-08 08:27:59 -0600

    2021 Day 8 Part 1

--- AdventOfCode/Solutions/Year2021/Day08/Solution.cs
+++ AdventOfCode/Solutions/Year2021/Day08/Solution.cs
@@ -13,14 +13,30 @@ namespace AdventOfCode.Solutions.Year2021
     class Day08 : ASolution
     {
 
-        public Day08() : base(08, 2021, "")
+        public Day08() : base(08, 2021, "Seven Segment Search")
         {
 
         }
 
         protected override string? SolvePartOne()
         {
-            return null;
+            // Digits 1, 4, 7, 8 all use different length sequences
+            // These are the sequence lengths
+            var uniqueLengths = new int[] { 2, 4, 3, 7 };
+
+            return Input.SplitByNewline()
+                // For every line...
+                .Sum(line =>
+                    // Get the output side
+                    line.Split('|', 2, StringSplitOptions.TrimEntries)[1]
+                    // Get all output groups
+                    .Split(' ', StringSplitOptions.TrimEntries)
+                    // Group them by length
+                    .GroupBy(grp => grp.Length)
+                    // If they're in the unique list, count them
+                    .Where(grp => uniqueLengths.Contains(grp.Key))
+                    .Sum(grp => grp.Count())
+                ).ToString();
         }
 
         protected override string? SolvePartTwo()
