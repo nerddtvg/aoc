ac3ca6ca41739d0b3e31f6e6037a938bf828cb72
Drew <nerddtvg@users.noreply.github.com>
2021-12-09 10:07:16 -0600

    2017 Day 25 Part 1

--- AdventOfCode/Solutions/Year2017/Day25/Solution.cs
+++ AdventOfCode/Solutions/Year2017/Day25/Solution.cs
@@ -12,15 +12,182 @@ namespace AdventOfCode.Solutions.Year2017
 
     class Day25 : ASolution
     {
+        public class TuringMachine
+        {
+            public enum State
+            {
+                A,
+                B,
+                C,
+                D,
+                E,
+                F
+            }
 
-        public Day25() : base(25, 2017, "")
+            // Starts in State A
+            public State state = State.A;
+
+            // Linked List may get too big, so let's use a dictionary and only set what we need
+            public Dictionary<int, int> tape = new Dictionary<int, int>() { { 0, 0 } };
+
+            // Cursor starts at zero
+            public int cursor = 0;
+
+            public void Run(bool debug = false)
+            {
+                if (debug == true)
+                {
+                    if (state == State.A)
+                    {
+                        if (GetValue() == 0)
+                        {
+                            this.tape[this.cursor] = 1;
+                            this.cursor++;
+                            this.state = State.B;
+                        }
+                        else
+                        {
+                            this.tape[this.cursor] = 0;
+                            this.cursor--;
+                            this.state = State.B;
+                        }
+                    }
+                    else if (state == State.B)
+                    {
+                        if (GetValue() == 0)
+                        {
+                            this.tape[this.cursor] = 1;
+                            this.cursor--;
+                            this.state = State.A;
+                        }
+                        else
+                        {
+                            this.tape[this.cursor] = 1;
+                            this.cursor++;
+                            this.state = State.A;
+                        }
+                    }
+                    return;
+                }
+
+                // Instructions from the input
+                if (state == State.A)
+                {
+                    if (GetValue() == 0)
+                    {
+                        this.tape[this.cursor] = 1;
+                        this.cursor++;
+                        this.state = State.B;
+                    }
+                    else
+                    {
+                        this.tape[this.cursor] = 0;
+                        this.cursor--;
+                        this.state = State.C;
+                    }
+                }
+                else if (state == State.B)
+                {
+                    if (GetValue() == 0)
+                    {
+                        this.tape[this.cursor] = 1;
+                        this.cursor--;
+                        this.state = State.A;
+                    }
+                    else
+                    {
+                        this.tape[this.cursor] = 1;
+                        this.cursor++;
+                        this.state = State.D;
+                    }
+                }
+                else if (state == State.C)
+                {
+                    if (GetValue() == 0)
+                    {
+                        this.tape[this.cursor] = 0;
+                        this.cursor--;
+                        this.state = State.B;
+                    }
+                    else
+                    {
+                        this.tape[this.cursor] = 0;
+                        this.cursor--;
+                        this.state = State.E;
+                    }
+                }
+                else if (state == State.D)
+                {
+                    if (GetValue() == 0)
+                    {
+                        this.tape[this.cursor] = 1;
+                        this.cursor++;
+                        this.state = State.A;
+                    }
+                    else
+                    {
+                        this.tape[this.cursor] = 0;
+                        this.cursor++;
+                        this.state = State.B;
+                    }
+                }
+                else if (state == State.E)
+                {
+                    if (GetValue() == 0)
+                    {
+                        this.tape[this.cursor] = 1;
+                        this.cursor--;
+                        this.state = State.F;
+                    }
+                    else
+                    {
+                        this.tape[this.cursor] = 1;
+                        this.cursor--;
+                        this.state = State.C;
+                    }
+                }
+                else if (state == State.F)
+                {
+                    if (GetValue() == 0)
+                    {
+                        this.tape[this.cursor] = 1;
+                        this.cursor++;
+                        this.state = State.D;
+                    }
+                    else
+                    {
+                        this.tape[this.cursor] = 1;
+                        this.cursor++;
+                        this.state = State.A;
+                    }
+                }
+            }
+
+            public int GetChecksum() => this.tape.Count(kvp => kvp.Value == 1);
+
+            public int GetValue()
+            {
+                if (tape.ContainsKey(this.cursor))
+                    return tape[this.cursor];
+
+                return 0;
+            }
+        }
+
+        public Day25() : base(25, 2017, "The Halting Problem")
         {
 
         }
 
         protected override string? SolvePartOne()
         {
-            return null;
+            var p = new TuringMachine();
+            
+            // Utilities.Repeat(() => p.Run(debug: true), 6);
+
+            Utilities.Repeat(() => p.Run(), 12667664);
+
+            return p.GetChecksum().ToString();
         }
 
         protected override string? SolvePartTwo()
