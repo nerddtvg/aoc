7bbead8b44967ad34edcf8ecb35c176011aec65a
Drew <nerddtvg@users.noreply.github.com>
2021-12-09 09:40:15 -0600

    2021 Day

--- AdventOfCode/Solutions/Year2021/Day09/Solution.cs
+++ AdventOfCode/Solutions/Year2021/Day09/Solution.cs
@@ -17,6 +17,8 @@ namespace AdventOfCode.Solutions.Year2021
 
         public Day09() : base(09, 2021, "")
         {
+            // DebugInput = "2199943210\n3987894921\n9856789892\n8767896789\n9899965678";
+
             grid = Input.SplitByNewline().Select(line => line.ToIntArray().ToArray()).ToArray();
 
             this.lowestPoints = Enumerable.Range(0, this.grid.Length).SelectMany(y =>
@@ -47,9 +49,36 @@ namespace AdventOfCode.Solutions.Year2021
             return this.lowestPoints.Sum(pt => GetValue(pt.x, pt.y) + 1).ToString();
         }
 
+        private void FindBasin((int x, int y) pt, ref List<(int x, int y)> found)
+        {
+            if (found.Contains(pt))
+                return;
+
+            // If this point is a 9 or out of bounds, skip it
+            if (GetValue(pt.x, pt.y) >= 9)
+                return;
+
+            // Add this point
+            found.Add(pt);
+
+            // Look around...
+            FindBasin((pt.x - 1, pt.y), ref found);
+            FindBasin((pt.x + 1, pt.y), ref found);
+            FindBasin((pt.x, pt.y - 1), ref found);
+            FindBasin((pt.x, pt.y + 1), ref found);
+        }
+
         protected override string? SolvePartTwo()
         {
-            return null;
+            // Basins
+            var basins = this.lowestPoints.Select(pt =>
+            {
+                var lst = new List<(int x, int y)>();
+                FindBasin(pt, ref lst);
+                return lst;
+            }).ToList();
+
+            return basins.Select(lst => lst.Count).OrderByDescending(lst => lst).Take(3).Aggregate((x, y) => x * y).ToString();
         }
     }
 }
