c6578405845cc4d598967194029bd860f06f1efc
Drew <nerddtvg@users.noreply.github.com>
2021-12-10 07:00:37 -0600

    2021 Day 10 Part 1

--- AdventOfCode/Solutions/Year2021/Day10/Solution.cs
+++ AdventOfCode/Solutions/Year2021/Day10/Solution.cs
@@ -12,15 +12,78 @@ namespace AdventOfCode.Solutions.Year2021
 
     class Day10 : ASolution
     {
+        private Dictionary<char, uint> values = new Dictionary<char, uint>()
+        {
+            { ')', 3 },
+            { ']', 57 },
+            { '}', 1197 },
+            { '>', 25137 }
+        };
+
+        private Dictionary<char, char> matches = new Dictionary<char, char>()
+        {
+            { '(', ')' },
+            { '[', ']' },
+            { '{', '}' },
+            { '<', '>' }
+        };
 
         public Day10() : base(10, 2021, "")
         {
+//             DebugInput = @"[({(<(())[]>[[{[]{<()<>>
+// [(()[<>])]({[<{<<[]>>(
+// {([(<{}[<>[]}>{[]{[(<()>
+// (((({<>}<{<{<>}{[]{[]{}
+// [[<[([]))<([[{}[[()]]]
+// [{[{({}]{}}([{[{{{}}([]
+// {<[[]]>}<{[{[{[]{()[[[]
+// [<(<(<(<{}))><([]([]()
+// <{([([[(<>()){}]>(<<{{
+// <{([{{}}[<[[[<>{}]]]>[]]";
 
+            // DebugInput = "[[<[([]))<([[{}[[()]]]";
+        }
+
+        private uint CorruptedScore(string line, int part = 1)
+        {
+            var openings = new Stack<char>();
+
+            uint score = 0;
+
+            foreach(var ch in line)
+            {
+                switch(ch)
+                {
+                    case '{':
+                    case '(':
+                    case '[':
+                    case '<':
+                        openings.Push(ch);
+                        break;
+
+                    case '}':
+                    case ')':
+                    case ']':
+                    case '>':
+                        if (this.matches[openings.Pop()] != ch)
+                        {
+                            // Mismatched
+                            score += this.values[ch];
+
+                            // Part 1 ends at the first one
+                            if (part == 1)
+                                return this.values[ch];
+                        }
+                        break;
+                }
+            }
+
+            return score;
         }
 
         protected override string? SolvePartOne()
         {
-            return null;
+            return Input.SplitByNewline().Sum(line => CorruptedScore(line)).ToString();
         }
 
         protected override string? SolvePartTwo()
