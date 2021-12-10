9eb9e7dcfb5c444e0b7e385274179364df8413e4
Drew <nerddtvg@users.noreply.github.com>
2021-12-10 10:32:40 -0600

    2016 Day 19 Part 1

--- AdventOfCode/Solutions/Year2016/Day19/Solution.cs
+++ AdventOfCode/Solutions/Year2016/Day19/Solution.cs
@@ -10,15 +10,66 @@ namespace AdventOfCode.Solutions.Year2016
 
     class Day19 : ASolution
     {
+        private Dictionary<uint, uint> elves = new Dictionary<uint, uint>();
+        private uint pos = 0;
+        private uint next = 0;
+        private uint count = 0;
 
-        public Day19() : base(19, 2016, "")
+        public Day19() : base(19, 2016, "An Elephant Named Joseph")
         {
+            // Initiate our list
+            this.count = UInt32.Parse(Input);
+            for (uint i = 0; i < count; i++)
+            {
+                elves.Add(i, 1);
+            }
+        }
 
+        private uint FindNext(uint start)
+        {
+            for (uint i = 0; i < count; i++)
+            {
+                var index = (start + i) % count;
+
+                if (this.elves[index] > 0)
+                    return index;
+            }
+
+            throw new Exception("None found!");
+        }
+
+        private void RunRound()
+        {
+            if (this.elves[this.pos] == 0)
+            {
+                // Search for the next present starting from the last stolen spot + 1
+                this.pos = FindNext(this.next);
+            }
+
+            // We're done here
+            if (this.elves[this.pos] == count)
+                return;
+
+            // Who are we stealing from?
+            this.next = FindNext(this.pos + 1);
+
+            this.elves[this.pos] += this.elves[this.next];
+            this.elves[this.next] = 0;
+
+            // Move one more up
+            this.pos = (this.next + 1) % count;
+            this.next += 2;
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            do
+            {
+                RunRound();
+            } while (this.elves[this.pos] != count);
+
+            // We're off by one in the index
+            return (this.pos + 1).ToString();
         }
 
         protected override string SolvePartTwo()
