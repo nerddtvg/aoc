6af8889bb7bda6235f30070c1b7b5fc3d5c5f73b
Drew <nerddtvg@users.noreply.github.com>
2021-12-11 16:08:55 -0600

    2016 Day 11 Initial logic

--- AdventOfCode/Solutions/Year2016/Day11/Solution.cs
+++ AdventOfCode/Solutions/Year2016/Day11/Solution.cs
@@ -10,20 +10,180 @@ using System.Linq;
 // This will be based on someone else's solution from the mega thread
 // https://old.reddit.com/r/adventofcode/comments/5hoia9/2016_day_11_solutions/
 
+// Attempting: https://old.reddit.com/r/adventofcode/comments/5hoia9/2016_day_11_solutions/db1zbu0/
+// This is an A* approach which we've already done in a few other days
+
 namespace AdventOfCode.Solutions.Year2016
 {
 
     class Day11 : ASolution
     {
+        public const int promethium = 1;
+        public const int cobalt = 2;
+        public const int curium = 3;
+        public const int ruthenium = 4;
+        public const int plutonium = 5;
 
-        public Day11() : base(11, 2016, "")
+        // Hard coding the inputs because it will be easier than reading them
+        public int[][] initial = new int[][]
+        {
+            new int[] { promethium, -promethium },
+            new int[] { cobalt, curium, ruthenium, plutonium },
+            new int[] { -cobalt, -curium, -ruthenium, -plutonium },
+            new int[] { }
+        };
+
+        public Day11() : base(11, 2016, "Radioisotope Thermoelectric Generators")
         {
 
         }
 
+        public bool FloorIsCorrect(int[] floor)
+        {
+            // Maybe the floor is empty or if it isn't, make sure everything cancels out or there is no extra generator
+            if (floor.Length == 0 || floor.Count(val => val < 0) == floor.Length)
+                return true;
+
+            // Remove every match
+            var unMatched = floor.Where(val => !floor.Contains(-1 * val)).ToArray();
+
+            // Nothing unmatched, or we have only chips OR only generators left
+            var chipsLeft = unMatched.Count(val => val < 0);
+            return unMatched.Length == 0 || (chipsLeft == 0 ^ chipsLeft == unMatched.Length);
+        }
+
+        public bool IsFinished(FloorState state)
+        {
+            var maxIdx = state.floors.Length - 1;
+            return state.floors.Where((val, idx) => idx != maxIdx).Sum(val => val.Length) == 0;
+        }
+
+        // Generate a hash of the floors so we can use this as a key
+        public string FloorHash(int[][] floors)
+        {
+            var hash = new List<string>();
+
+            for (int i = 0; i < floors.Length; i++)
+            {
+                if (floors[i].Length == 0)
+                {
+                    hash.Add("0");
+                }
+                else
+                {
+                    hash.Add(string.Join("-", floors[i].OrderBy(val => val).Select(val => val.ToString())));
+                }
+            }
+
+            return string.Join(":", hash);
+        }
+
+        public class FloorState : IEquatable<FloorState>
+        {
+            public int elevator = 0;
+            public int[][] floors = new int[][] { };
+
+            public override int GetHashCode()
+            {
+                return elevator + floors.Select((floor, idx) => ((idx + 1) * 1000) + floor.Sum()).Sum();
+            }
+
+            public override bool Equals(object obj)
+            {
+                return obj is FloorState && Equals((FloorState) obj);
+            }
+
+            public bool Equals(FloorState obj)
+            {
+                // Make sure we truly match
+                return elevator == obj.elevator
+                    && floors.Length == obj.floors.Length
+                    && floors.Select((floor, idx) => floor.OrderBy(val => val).SequenceEqual(obj.floors[idx].OrderBy(val => val))).All(ret => ret == true);
+            }
+        }
+
+        // Based on: https://en.wikipedia.org/wiki/A*_search_algorithm
+        public int AStar(int[][] initialFloors)
+        {
+            // This is the list of elevator positions (int) and floors (int[][]) we need to search
+            var openSet = new HashSet<FloorState>() { new FloorState() { elevator = 0, floors = initialFloors } };
+
+            // A Dictionary of the node that we cameFrom Value to reach the floor Key
+            var cameFrom = new Dictionary<FloorState, FloorState>();
+
+            // gScore is the known shortest path from start to Key, other values are assumed infinity
+            var gScore = new Dictionary<FloorState, int>() { { new FloorState() { elevator = 0, floors = initialFloors }, 0 } };
+
+            do
+            {
+                // Get the next node to work on
+                var min = gScore.Where(kvp => openSet.Contains(kvp.Key)).Min(kvp => kvp.Value);
+                var currentState = openSet.FirstOrDefault(pt => gScore[pt] == min);
+                    
+                // Did we find the shortest path?
+                if (IsFinished(currentState))
+                {
+                    return gScore[currentState];
+                }
+
+                // Work on this node
+                openSet.Remove(currentState);
+
+                // We know the gScore to get to a neighbor is gScore[currentNode] + 1
+                var tgScore = gScore[currentState] + 1;
+
+                // Get all possible moves
+                // We can either move one or two items from the floor we're on
+                // We can also go up or down or both
+                var dirs = new List<int>();
+                if (currentState.elevator > 0)
+                    dirs.Add(-1);
+                
+                if (currentState.elevator < initial.Length-1)
+                    dirs.Add(1);
+
+                // Get a list of moving two possible items, combine it with a list of just one item
+                var moves = (currentState.floors[currentState.elevator].Length > 2 ? currentState.floors[currentState.elevator].GetAllCombos(r: 2).Select(lst => lst.ToList()) : new List<List<int>>())
+                    .Union(currentState.floors[currentState.elevator].Select(val => new List<int>() { val }))
+                    .ToList();
+
+                foreach(var move in moves)
+                {
+                    foreach(var dir in dirs)
+                    {
+                        var newFloors = currentState.floors.Select(floor => (int[])floor.Clone()).ToArray();
+
+                        // Remove the item(s) we're moving
+                        newFloors[currentState.elevator] = newFloors[currentState.elevator].Where(val => !move.Contains(val)).ToArray();
+
+                        // Add the items we're moving
+                        newFloors[currentState.elevator + dir] = newFloors[currentState.elevator + dir].Union(move).ToArray();
+
+                        // Is this a valid combination?
+                        if (!FloorIsCorrect(newFloors[currentState.elevator]) || !FloorIsCorrect(newFloors[currentState.elevator + dir]))
+                            continue;
+
+                        // Setup our new floor state
+                        var newState = new FloorState() { elevator = currentState.elevator + dir, floors = newFloors };
+
+                        if (!gScore.ContainsKey(newState) || tgScore < gScore[newState])
+                        {
+                            cameFrom[newState] = currentState;
+                            gScore[newState] = tgScore;
+
+                            // Add to our search list
+                            openSet.Add(newState);
+                        }
+                    }
+                }
+            } while (openSet.Count > 0);
+
+            return 0;
+        }
+
         protected override string SolvePartOne()
         {
-            return null;
+            return AStar(this.initial).ToString();
         }
 
         protected override string SolvePartTwo()
