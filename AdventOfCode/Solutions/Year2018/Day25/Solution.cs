8c0e5eb7b5bdf153ad6ab051afae39f52e9e10d8
Drew <nerddtvg@users.noreply.github.com>
2021-12-12 11:50:10 -0600

    Replaced some older templates

--- AdventOfCode/Solutions/Year2018/Day25/Solution.cs
+++ AdventOfCode/Solutions/Year2018/Day25/Solution.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 using System.Linq;
+
+#nullable enable
 
 namespace AdventOfCode.Solutions.Year2018
 {
@@ -15,14 +18,16 @@ namespace AdventOfCode.Solutions.Year2018
 
         }
 
-        protected override string SolvePartOne()
+        protected override string? SolvePartOne()
         {
             return null;
         }
 
-        protected override string SolvePartTwo()
+        protected override string? SolvePartTwo()
         {
             return null;
         }
     }
 }
+
+#nullable restore
