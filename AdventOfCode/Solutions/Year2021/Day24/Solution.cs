79b072e402c51348fda37977e9639ceb20cf547a
Drew <nerddtvg@users.noreply.github.com>
2022-01-05 13:32:25 -0600

    2021 Day 24 Implemented someone's program

--- AdventOfCode/Solutions/Year2021/Day24/Solution.cs
+++ AdventOfCode/Solutions/Year2021/Day24/Solution.cs
@@ -7,311 +7,64 @@ using System.Linq;
 
 #nullable enable
 
+// /u/Ok_System_5724 had a really cool solution: https://old.reddit.com/r/adventofcode/comments/rnejv5/2021_day_24_solutions/hqgl5hj/
+// While I simplified the code into C#, the math was escaping me while trying to read it.
+// It all comes down to just three lines in each program block
+
+// I'm going to reimplement what they have provided and interpret it myself as a learning experience
+
 namespace AdventOfCode.Solutions.Year2021
 {
 
     class Day24 : ASolution
     {
+        (int a, int b, int c)[] programs;
+
+        long[] valid = new long[] { };
 
         public Day24() : base(24, 2021, "Arithmetic Logic Unit")
         {
-
+            programs = Input
+                // Each program is 18 lines long
+                .SplitByNewline().Chunk(18)
+                // Select lines 4, 5, and 15
+                // Line 4: div z 1 or div z 26 (always one or the other)
+                // Line 5: add x -8 (sample)
+                // Line 15: add y 12 (sample)
+                .SelectMany(c => new[] { c[4], c[5], c[15] })
+                // Get the integer values of each of these
+                .Select(c => Convert.ToInt32(c.Substring(6))).Chunk(3)
+                .Select(c => (a: c[0], b: c[1], c: c[2]))
+                .ToArray();
         }
 
         protected override string? SolvePartOne()
         {
-            // Start with the largest possible 14-digit number and find the first one that z is returned 0
-            for (long i = 99999999999999; i >= 11111111111111; i--)
-            {
-                if (i.ToString().Contains('0')) continue;
+            // Only use the digits 1 through 9
+            var nums = Enumerable.Range(1, 9);
 
-                if (ALU(i) == 0)
-                    return i.ToString();
+            IEnumerable<long> check(int[] digits, int i, int z)
+            {
+                if (digits.Length == programs.Length) return z == 0 
+                    ? new[] { long.Parse(String.Join("", digits)) } : new long[0];
+
+                IEnumerable<long> sub(IEnumerable<int> range, Func<int, int> z) =>
+                    range.SelectMany(w => check(digits.Append(w).ToArray(), i + 1, z(w)));
+
+                return programs[i].b < 0
+                    ? sub(nums.Where(n => z % 26 + programs[i].b == n), (w) => z / 26)
+                    : sub(nums, (w) => z * 26 + w + programs[i].c);
             }
 
-            return null;
+            // Get all possible answers
+            valid = check(new int[0], 0, 0).ToArray();
+
+            return valid.Max().ToString();
         }
 
         protected override string? SolvePartTwo()
         {
-            return null;
-        }
-
-        private int ALU(long input)
-        {
-            // Split our input into digits in a queue to pop them off
-            var inQueue = new Queue<int>(input.ToString().ToIntArray());
-
-            int x = 0, y = 0, z = 0;
-            int digit = 0;
-
-            digit = inQueue.Dequeue();
-            x *= 0;
-            x += z;
-            x %= 26;
-            z /= 1;
-            x += 14;
-            if (x == digit) x = 1; else x = 0;
-            if (x == 0) x = 1; else x = 0;
-            y *= 0;
-            y += 25;
-            y *= x;
-            y += 1;
-            z *= y;
-            y *= 0;
-            y += digit;
-            y += 12;
-            y *= x;
-            z += y;
-            
-            digit = inQueue.Dequeue();
-            x *= 0;
-            x += z;
-            x %= 26;
-            z /= 1;
-            x += 10;
-            if (x == digit) x = 1; else x = 0;
-            if (x == 0) x = 1; else x = 0;
-            y *= 0;
-            y += 25;
-            y *= x;
-            y += 1;
-            z *= y;
-            y *= 0;
-            y += digit;
-            y += 9;
-            y *= x;
-            z += y;
-
-            digit = inQueue.Dequeue();
-            x *= 0;
-            x += z;
-            x %= 26;
-            z /= 1;
-            x += 13;
-            if (x == digit) x = 1; else x = 0;
-            if (x == 0) x = 1; else x = 0;
-            y *= 0;
-            y += 25;
-            y *= x;
-            y += 1;
-            z *= y;
-            y *= 0;
-            y += digit;
-            y += 8;
-            y *= x;
-            z += y;
-
-            digit = inQueue.Dequeue();
-            x *= 0;
-            x += z;
-            x %= 26;
-            z /= 26;
-            x += -8;
-            if (x == digit) x = 1; else x = 0;
-            if (x == 0) x = 1; else x = 0;
-            y *= 0;
-            y += 25;
-            y *= x;
-            y += 1;
-            z *= y;
-            y *= 0;
-            y += digit;
-            y += 3;
-            y *= x;
-            z += y;
-
-            digit = inQueue.Dequeue();
-            x *= 0;
-            x += z;
-            x %= 26;
-            z /= 1;
-            x += 11;
-            if (x == digit) x = 1; else x = 0;
-            if (x == 0) x = 1; else x = 0;
-            y *= 0;
-            y += 25;
-            y *= x;
-            y += 1;
-            z *= y;
-            y *= 0;
-            y += digit;
-            y += 0;
-            y *= x;
-            z += y;
-
-            digit = inQueue.Dequeue();
-            x *= 0;
-            x += z;
-            x %= 26;
-            z /= 1;
-            x += 11;
-            if (x == digit) x = 1; else x = 0;
-            if (x == 0) x = 1; else x = 0;
-            y *= 0;
-            y += 25;
-            y *= x;
-            y += 1;
-            z *= y;
-            y *= 0;
-            y += digit;
-            y += 11;
-            y *= x;
-            z += y;
-            
-            digit = inQueue.Dequeue();
-            x *= 0;
-            x += z;
-            x %= 26;
-            z /= 1;
-            x += 14;
-            if (x == digit) x = 1; else x = 0;
-            if (x == 0) x = 1; else x = 0;
-            y *= 0;
-            y += 25;
-            y *= x;
-            y += 1;
-            z *= y;
-            y *= 0;
-            y += digit;
-            y += 10;
-            y *= x;
-            z += y;
-
-            digit = inQueue.Dequeue();
-            x *= 0;
-            x += z;
-            x %= 26;
-            z /= 26;
-            x += -11;
-            if (x == digit) x = 1; else x = 0;
-            if (x == 0) x = 1; else x = 0;
-            y *= 0;
-            y += 25;
-            y *= x;
-            y += 1;
-            z *= y;
-            y *= 0;
-            y += digit;
-            y += 13;
-            y *= x;
-            z += y;
-
-            digit = inQueue.Dequeue();
-            x *= 0;
-            x += z;
-            x %= 26;
-            z /= 1;
-            x += 14;
-            if (x == digit) x = 1; else x = 0;
-            if (x == 0) x = 1; else x = 0;
-            y *= 0;
-            y += 25;
-            y *= x;
-            y += 1;
-            z *= y;
-            y *= 0;
-            y += digit;
-            y += 3;
-            y *= x;
-            z += y;
-
-            digit = inQueue.Dequeue();
-            x *= 0;
-            x += z;
-            x %= 26;
-            z /= 26;
-            x += -1;
-            if (x == digit) x = 1; else x = 0;
-            if (x == 0) x = 1; else x = 0;
-            y *= 0;
-            y += 25;
-            y *= x;
-            y += 1;
-            z *= y;
-            y *= 0;
-            y += digit;
-            y += 10;
-            y *= x;
-            z += y;
-
-            digit = inQueue.Dequeue();
-            x *= 0;
-            x += z;
-            x %= 26;
-            z /= 26;
-            x += -8;
-            if (x == digit) x = 1; else x = 0;
-            if (x == 0) x = 1; else x = 0;
-            y *= 0;
-            y += 25;
-            y *= x;
-            y += 1;
-            z *= y;
-            y *= 0;
-            y += digit;
-            y += 10;
-            y *= x;
-            z += y;
-
-            digit = inQueue.Dequeue();
-            x *= 0;
-            x += z;
-            x %= 26;
-            z /= 26;
-            x += -5;
-            if (x == digit) x = 1; else x = 0;
-            if (x == 0) x = 1; else x = 0;
-            y *= 0;
-            y += 25;
-            y *= x;
-            y += 1;
-            z *= y;
-            y *= 0;
-            y += digit;
-            y += 14;
-            y *= x;
-            z += y;
-
-            digit = inQueue.Dequeue();
-            x *= 0;
-            x += z;
-            x %= 26;
-            z /= 26;
-            x += -16;
-            if (x == digit) x = 1; else x = 0;
-            if (x == 0) x = 1; else x = 0;
-            y *= 0;
-            y += 25;
-            y *= x;
-            y += 1;
-            z *= y;
-            y *= 0;
-            y += digit;
-            y += 6;
-            y *= x;
-            z += y;
-
-            digit = inQueue.Dequeue();
-            x *= 0;
-            x += z;
-            x %= 26;
-            z /= 26;
-            x += -6;
-            if (x == digit) x = 1; else x = 0;
-            if (x == 0) x = 1; else x = 0;
-            y *= 0;
-            y += 25;
-            y *= x;
-            y += 1;
-            z *= y;
-            y *= 0;
-            y += digit;
-            y += 5;
-            y *= x;
-            z += y;
-
-            return z;
+            return valid.Min().ToString();
         }
     }
 }
