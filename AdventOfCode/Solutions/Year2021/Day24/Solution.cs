031b8a31bf923a7d8ecdd2d45daab10fb4ab62fb
Drew <nerddtvg@users.noreply.github.com>
2022-01-05 12:52:48 -0600

    2021 Day 24 Part 1 incomplete

--- AdventOfCode/Solutions/Year2021/Day24/Solution.cs
+++ AdventOfCode/Solutions/Year2021/Day24/Solution.cs
@@ -13,19 +13,258 @@ namespace AdventOfCode.Solutions.Year2021
     class Day24 : ASolution
     {
 
-        public Day24() : base(24, 2021, "")
+        public Day24() : base(24, 2021, "Arithmetic Logic Unit")
         {
 
         }
 
         protected override string? SolvePartOne()
         {
+            // Start with the largest possible 14-digit number and find the first one that z is returned 0
+            for (long i = 99999999999999; i >= 11111111111111; i--)
+            {
+                if (i.ToString().Contains('0')) continue;
+
+                if (ALU(i) == 0)
+                    return i.ToString();
+            }
+
             return null;
         }
 
         protected override string? SolvePartTwo()
         {
             return null;
+        }
+
+        private int ALU(long input)
+        {
+            // Split our input into digits in a queue to pop them off
+            var inQueue = new Queue<int>(input.ToString().ToIntArray());
+
+            int x = 0, y = 0, z = 0;
+            int digit = 0;
+
+            digit = inQueue.Dequeue();
+            x = z;
+            x %= 26;
+            x += 14;
+            if (x == digit) x = 1; else x = 0;
+            y = 0;
+            y += 25;
+            y *= x;
+            y += 1;
+            z *= y;
+            y = digit;
+            y += 12;
+            y *= x;
+            z += y;
+
+            digit = inQueue.Dequeue();
+            x = z;
+            x %= 26;
+            x += 10;
+            if (x == digit) x = 1; else x = 0;
+            y = 0;
+            y += 25;
+            y *= x;
+            y += 1;
+            z *= y;
+            y = digit;
+            y += 9;
+            y *= x;
+            z += y;
+
+            digit = inQueue.Dequeue();
+            x = z;
+            x %= 26;
+            x += 13;
+            if (x == digit) x = 1; else x = 0;
+            y = 0;
+            y += 25;
+            y *= x;
+            y += 1;
+            z *= y;
+            y = digit;
+            y += 8;
+            y *= x;
+            z += y;
+
+            digit = inQueue.Dequeue();
+            x = z;
+            x %= 26;
+            z /= 26;
+            x -= 8;
+            if (x == digit) x = 1; else x = 0;
+            y = 0;
+            y += 25;
+            y *= x;
+            y += 1;
+            z *= y;
+            y = digit;
+            y += 3;
+            y *= x;
+            z += y;
+
+            digit = inQueue.Dequeue();
+            x = z;
+            x %= 26;
+            x += 11;
+            if (x == digit) x = 1; else x = 0;
+            y = 0;
+            y += 25;
+            y *= x;
+            y += 1;
+            z *= y;
+            y = digit;
+            y *= x;
+            z += y;
+
+            // Up to this point z should be 0
+            // The next one will cause z to be 12 if the digit is 1, otherwise 0
+
+            digit = inQueue.Dequeue();
+            x = z;
+            x %= 26;
+            x += 1;
+            if (x == digit) x = 1; else x = 0;
+            y = 0;
+            y += 25;
+            y *= x;
+            y += 1;
+            z *= y;
+            y = digit;
+            y += 11;
+            y *= x;
+            z += y;
+
+            digit = inQueue.Dequeue();
+            x = z;
+            x %= 26;
+            x += 14;
+            if (x == digit) x = 1; else x = 0;
+            y = 0;
+            y += 25;
+            y *= x;
+            y += 1;
+            z *= y;
+            y = digit;
+            y += 10;
+            y *= x;
+            z += y;
+
+            digit = inQueue.Dequeue();
+            x = z;
+            x %= 26;
+            z /= 26;
+            x -= 11;
+            if (x == digit) x = 1; else x = 0;
+            y = 0;
+            y += 25;
+            y *= x;
+            y += 1;
+            z *= y;
+            y = digit;
+            y += 13;
+            y *= x;
+            z += y;
+
+            digit = inQueue.Dequeue();
+            x = z;
+            x %= 26;
+            x += 14;
+            if (x == digit) x = 1; else x = 0;
+            y = 0;
+            y += 25;
+            y *= x;
+            y += 1;
+            z *= y;
+            y = digit;
+            y += 3;
+            y *= x;
+            z += y;
+
+            digit = inQueue.Dequeue();
+            x = z;
+            x %= 26;
+            z /= 26;
+            x -= 1;
+            if (x == digit) x = 1; else x = 0;
+            y = 0;
+            y += 25;
+            y *= x;
+            y += 1;
+            z *= y;
+            y = digit;
+            y += 10;
+            y *= x;
+            z += y;
+
+            digit = inQueue.Dequeue();
+            x = z;
+            x %= 26;
+            z /= 26;
+            x -= 8;
+            if (x == digit) x = 1; else x = 0;
+            y = 0;
+            y += 25;
+            y *= x;
+            y += 1;
+            z *= y;
+            y = digit;
+            y += 10;
+            y *= x;
+            z += y;
+
+            digit = inQueue.Dequeue();
+            x = z;
+            x %= 26;
+            z /= 26;
+            x -= 5;
+            if (x == digit) x = 1; else x = 0;
+            y = 0;
+            y += 25;
+            y *= x;
+            y += 1;
+            z *= y;
+            y = digit;
+            y += 14;
+            y *= x;
+            z += y;
+
+            digit = inQueue.Dequeue();
+            x = z;
+            x %= 26;
+            z /= 26;
+            x -= 16;
+            if (x == digit) x = 1; else x = 0;
+            y = 0;
+            y += 25;
+            y *= x;
+            y += 1;
+            z *= y;
+            y = digit;
+            y += 6;
+            y *= x;
+            z += y;
+
+            digit = inQueue.Dequeue();
+            x = z;
+            x %= 26;
+            z /= 26;
+            x += -6;
+            if (x == digit) x = 1; else x = 0;
+            y = 0;
+            y += 25;
+            y *= x;
+            y += 1;
+            z *= y;
+            y = digit;
+            y += 5;
+            y *= x;
+            z += y;
+
+            return z;
         }
     }
 }
