7a21e2516b99109525383e5731a3b7d9dfa51d19
Drew <nerddtvg@users.noreply.github.com>
2022-09-07 10:30:13 -0500

    Backfill 2018 Day 1

--- AdventOfCode/Solutions/Year2018/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2018/Day01/Solution.cs
@@ -9,20 +9,46 @@ namespace AdventOfCode.Solutions.Year2018
 
     class Day01 : ASolution
     {
+        int? twice;
+        int frequency = 0;
+        HashSet<int> frequencies = new();
 
-        public Day01() : base(01, 2018, "")
+        public Day01() : base(01, 2018, "Chronal Calibration")
         {
-
+            // DebugInput = "+1\n-1";
+            // DebugInput = "+3\n+3\n+4\n-2\n-4";
         }
 
         protected override string SolvePartOne()
         {
-            return string.Empty;
+            frequencies.Add(0);
+            RunInput();
+
+            return frequency.ToString();
+        }
+
+        private void RunInput()
+        {
+            foreach(var line in Input.SplitByNewline())
+            {
+                var dir = line[0] == '+' ? 1 : -1;
+                frequency += dir * Int32.Parse(line.Substring(1));
+
+                if (frequencies.Contains(frequency) && twice == default)
+                    twice = frequency;
+
+                frequencies.Add(frequency);
+            }
         }
 
         protected override string SolvePartTwo()
         {
-            return string.Empty;
+            while (twice == default)
+            {
+                RunInput();
+            }
+
+            return twice.ToString() ?? string.Empty;
         }
     }
 }
