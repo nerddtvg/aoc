b885a89aa8be72d3fb5c8e0c26242e298c5fedbd
Drew <nerddtvg@users.noreply.github.com>
2022-09-07 09:00:50 -0500

    Added title

--- AdventOfCode/Solutions/Year2021/Day18/Solution.cs
+++ AdventOfCode/Solutions/Year2021/Day18/Solution.cs
@@ -13,7 +13,7 @@ namespace AdventOfCode.Solutions.Year2021
     class Day18 : ASolution
     {
 
-        public Day18() : base(18, 2021, "")
+        public Day18() : base(18, 2021, "Snailfish")
         {
 
         }
