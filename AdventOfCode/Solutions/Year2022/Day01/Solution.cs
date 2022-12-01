b7aa029d70d16e0d1f508a2f85c23f62692c9798
Drew <nerddtvg@users.noreply.github.com>
2022-11-30 23:16:54 -0600

    2022 Day 1

--- AdventOfCode/Solutions/Year2022/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2022/Day01/Solution.cs
@@ -12,19 +12,24 @@ namespace AdventOfCode.Solutions.Year2022
     class Day01 : ASolution
     {
 
-        public Day01() : base(01, 2022, "")
+        public Day01() : base(01, 2022, "Calorie Counting")
         {
 
         }
 
         protected override string? SolvePartOne()
         {
-            return string.Empty;
+            return Input.SplitByBlankLine().Max(lines => lines.Select(line => Int32.Parse(line)).Sum()).ToString();
         }
 
         protected override string? SolvePartTwo()
         {
-            return string.Empty;
+            return Input.SplitByBlankLine()
+                .Select(lines => lines.Select(line => Int32.Parse(line)).Sum())
+                .OrderByDescending(x => x)
+                .Take(3)
+                .Sum()
+                .ToString();
         }
     }
 }
