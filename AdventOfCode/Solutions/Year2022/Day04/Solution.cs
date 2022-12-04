d33cb51223664b81470b6d5fdea404c3fc6ea794
Drew <nerddtvg@users.noreply.github.com>
2022-12-03 23:13:39 -0600

    2022 Day 4

--- AdventOfCode/Solutions/Year2022/Day04/Solution.cs
+++ AdventOfCode/Solutions/Year2022/Day04/Solution.cs
@@ -19,12 +19,25 @@ namespace AdventOfCode.Solutions.Year2022
 
         protected override string? SolvePartOne()
         {
-            return string.Empty;
+            var pairs = Input.SplitByNewline()
+                .Select(line => line.Split(',').ToArray())
+                .Select(pair => new int[] { Int32.Parse(pair[0].Split('-')[0]), Int32.Parse(pair[0].Split('-')[1]), Int32.Parse(pair[1].Split('-')[0]), Int32.Parse(pair[1].Split('-')[1]) })
+                .ToArray();
+
+            return pairs.Count(pair => (pair[0] >= pair[2] && pair[1] <= pair[3]) || (pair[2] >= pair[0] && pair[3] <= pair[1])).ToString();
         }
 
         protected override string? SolvePartTwo()
         {
-            return string.Empty;
+            var pairs = Input.SplitByNewline()
+                .Select(line => line.Split(',').ToArray())
+                .Select(pair => new int[] { Int32.Parse(pair[0].Split('-')[0]), Int32.Parse(pair[0].Split('-')[1]), Int32.Parse(pair[1].Split('-')[0]), Int32.Parse(pair[1].Split('-')[1]) })
+                .ToArray();
+
+            // Any overlap at all
+            // start1, end1 => start1 <= end2 && end1 >= end2
+            // start2, end2 => start2 <= end1 && end2 >= end1
+            return pairs.Count(pair => (pair[0] <= pair[3] && pair[1] >= pair[3]) || (pair[2] <= pair[1] && pair[3] >= pair[1])).ToString();
         }
     }
 }
