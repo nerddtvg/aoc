c0706f6a82cc66c4ea0b89daea18f769e33e9a5d
Drew <nerddtvg@users.noreply.github.com>
2022-12-09 23:26:28 -0600

    Initial work on an easier Point type

---
+++ AdventOfCode/Solutions/Types/Point.cs
@@ -0,0 +1,322 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Numerics;
+
+namespace AdventOfCode.Solutions;
+
+/// <summary>
+/// A simple (x, y) coordinate class with corresponding operators.
+/// </summary>
+public readonly struct Point<T> where T : INumber<T>
+{
+    /// <summary>
+    /// The array of coordinates for this point.
+    /// </summary>
+    private readonly T[] coordinates;
+
+    /// <summary>
+    /// How many dimensions does this <see cref="Point" /> consist of. (Ex: (x, y) is 2-dimensions)
+    /// </summary>
+    public int Dimensions { get => coordinates.Length; }
+
+    /// <summary>
+    /// Initialize a new point with the given coordinate values.
+    /// </summary>
+    public Point(params T[] coordinates)
+    {
+        if (coordinates.Length < 2)
+            throw new ArgumentOutOfRangeException(nameof(coordinates), "At least two coordinate values must be specified.");
+        this.coordinates = coordinates;
+    }
+
+    public static Point<T> operator +(Point<T>a) => a;
+    public static Point<T>operator -(Point<T>a) => new Point<T>(a.coordinates.Select(c => -c).ToArray());
+
+    /// <summary>
+    /// Compare the lengths of two <see cref="Point" /> to ensure they have the same number of dimensions.
+    /// </summary>
+    private static void EnsureSameDimensions(Point<T>a, Point<T>b)
+    {
+        if (a.coordinates.Length != b.coordinates.Length)
+            throw new Exception("The given points do not have the same dimensions.");
+    }
+
+    /// <summary>
+    /// Adds points <paramref name="a" /> and <paramref name="b" />
+    /// </summary>
+    public static Point<T>operator +(Point<T>a, Point<T>b)
+    {
+        EnsureSameDimensions(a, b);
+
+        return new Point<T>(
+            Enumerable.Range(0, a.coordinates.Length)
+            .Select(i => a.coordinates[i] + b.coordinates[i])
+            .ToArray()
+        );
+    }
+
+    /// <summary>
+    /// Subtract point <paramref name="b" /> from <paramref name="a" />
+    /// </summary>
+    public static Point<T>operator -(Point<T>a, Point<T>b)
+    {
+        EnsureSameDimensions(a, b);
+
+        return new Point<T>(
+            Enumerable.Range(0, a.coordinates.Length)
+            .Select(i => a.coordinates[i] - b.coordinates[i])
+            .ToArray()
+        );
+    }
+
+    /// <summary>
+    /// Not implemented.
+    /// </summary>
+    public static Point<T>operator *(Point<T>a, Point<T>b)
+        => throw new NotImplementedException();
+
+    /// <summary>
+    /// Not implemented.
+    /// </summary>
+    public static Point<T>operator /(Point<T>a, Point<T>b)
+        => throw new NotImplementedException();
+
+    /// <summary>
+    /// Manhattan Distance calculation
+    /// </summary>
+    public static ulong operator %(Point<T> a, Point<T> b)
+    {
+        EnsureSameDimensions(a, b);
+
+        return Enumerable.Range(0, a.Dimensions)
+            .Sum(i =>
+            {
+                var t = a.coordinates[i] - b.coordinates[i];
+                if (t < T.Zero)
+                    return ulong.CreateChecked(-t);
+                return ulong.CreateChecked(t);
+            }) ?? throw new OverflowException();
+    }
+
+    /// <summary>
+    /// Not implemented.
+    /// </summary>
+    public static Point<T> operator &(Point<T> a, Point<T> b)
+        => throw new NotImplementedException();
+
+    /// <summary>
+    /// Not implemented.
+    /// </summary>
+    public static Point<T> operator |(Point<T> a, Point<T> b)
+        => throw new NotImplementedException();
+
+    /// <summary>
+    /// Not implemented.
+    /// </summary>
+    public static Point<T> operator ^(Point<T> a, Point<T> b)
+        => throw new NotImplementedException();
+
+    /// <summary>
+    /// Not implemented.
+    /// </summary>
+    public static Point<T> operator <<(Point<T> a, Point<T> b)
+        => throw new NotImplementedException();
+
+    /// <summary>
+    /// Not implemented.
+    /// </summary>
+    public static Point<T> operator >>(Point<T> a, Point<T> b)
+        => throw new NotImplementedException();
+
+    /// <summary>
+    /// Not implemented.
+    /// </summary>
+    public static Point<T> operator >>>(Point<T> a, Point<T> b)
+        => throw new NotImplementedException();
+
+    /// <summary>
+    /// Increment all dimensions by 1.
+    /// </summary>
+    public static Point<T>operator ++(Point<T>a)
+    {
+        return new Point<T>(
+            a.coordinates
+            .Select(c => c + T.One)
+            .ToArray()
+        );
+    }
+
+    /// <summary>
+    /// Decrement all dimensions by 1.
+    /// </summary>
+    public static Point<T>operator --(Point<T>a)
+    {
+        return new Point<T>(
+            a.coordinates
+            .Select(c => c - T.One)
+            .ToArray()
+        );
+    }
+
+    /// <summary>
+    /// Ensure all dimensions are equal.
+    /// </summary>
+    public static bool operator ==(Point<T>a, Point<T>b)
+    {
+        if (a.coordinates.Length != b.coordinates.Length)
+            return false;
+
+        return Enumerable.Range(0, a.coordinates.Length)
+            .All(i => a.coordinates[i] == b.coordinates[i]);
+    }
+
+    /// <summary>
+    /// Ensure at least one dimension is not equal or they are different sizes.
+    /// </summary>
+    public static bool operator !=(Point<T>a, Point<T>b)
+    {
+        if (a.coordinates.Length != b.coordinates.Length)
+            return true;
+
+        return !Enumerable.Range(0, a.coordinates.Length)
+            .All(i => a.coordinates[i] == b.coordinates[i]);
+    }
+
+    /// <summary>
+    /// Overloading Object.Equals as required.
+    /// </summary>
+    public override bool Equals([NotNullWhen(true)] object? obj)
+    {
+        if (obj is Point<T>b)
+        {
+            return this == b;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Overloading GetHashCode as required.
+    /// </summary>
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    /// <summary>
+    /// Compare if <paramref name="a" /> is less than <paramref name="b" />
+    /// </summary>
+    public static bool operator <(Point<T>a, Point<T>b)
+    {
+        EnsureSameDimensions(a, b);
+
+        return Enumerable.Range(0, a.coordinates.Length)
+            .All(i => a.coordinates[i] < b.coordinates[i]);
+    }
+
+    /// <summary>
+    /// Compare if <paramref name="a" /> is greater than <paramref name="b" />
+    /// </summary>
+    public static bool operator >(Point<T>a, Point<T>b)
+    {
+        EnsureSameDimensions(a, b);
+
+        return Enumerable.Range(0, a.coordinates.Length)
+            .All(i => a.coordinates[i] > b.coordinates[i]);
+    }
+
+    /// <summary>
+    /// Compare if <paramref name="a" /> is less than or equal <paramref name="b" />
+    /// </summary>
+    public static bool operator <=(Point<T>a, Point<T>b)
+    {
+        EnsureSameDimensions(a, b);
+
+        return Enumerable.Range(0, a.coordinates.Length)
+            .All(i => a.coordinates[i] <= b.coordinates[i]);
+    }
+
+    /// <summary>
+    /// Compare if <paramref name="a" /> is greater than or equal <paramref name="b" />
+    /// </summary>
+    public static bool operator >=(Point<T>a, Point<T>b)
+    {
+        EnsureSameDimensions(a, b);
+
+        return Enumerable.Range(0, a.coordinates.Length)
+            .All(i => a.coordinates[i] >= b.coordinates[i]);
+    }
+
+    /// <summary>
+    /// Create a new <see cref="Point" /> with the existing coordinates and a new dimension.
+    /// </summary>
+    public Point<T>AddDimension(T value)
+        => new Point<T>(coordinates.Append(value).ToArray());
+
+    /// <summary>
+    /// Direct coordinate values
+    /// </summary>
+    public T this[int i]
+    {
+        get
+        {
+            if (i >= coordinates.Length)
+                throw new IndexOutOfRangeException();
+
+            return coordinates[i];
+        }
+        set
+        {
+            if (i >= coordinates.Length)
+                throw new IndexOutOfRangeException();
+
+            coordinates[i] = value;
+        }
+    }
+
+    /// <summary>
+    /// Direct coordinate values based on characters. X, Y, Z are 0, 1, and 2 respectfully. A, B, C, ... to w. are 0 through 22 respectfully.
+    /// </summary>
+    public T this[char c]
+    {
+        get
+        {
+            return this[GetIndex(c)];
+        }
+        set
+            {this[GetIndex(c)] = value;
+        }
+    }
+
+    private int GetIndex(char c)
+    {
+        // A-Z
+        if (65 <= c && c <= 90)
+        {
+            c -= (char)65;
+
+            // X-Z => 0-3
+            if (23 <= c)
+                c -= (char)23;
+
+            return (int)c;
+        }
+
+        // A-Z
+        if (97 <= c && c <= 122)
+        {
+            c -= (char)97;
+
+            // X-Z => 0-3
+            if (23 <= c)
+                c -= (char)23;
+
+            return (int)c;
+        }
+
+        throw new ArgumentException("Specified character index must be A-Z or a-z.", nameof(c));
+    }
+}
