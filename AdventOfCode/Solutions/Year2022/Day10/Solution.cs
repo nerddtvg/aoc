24c9fe6a75f20cedbb6290c4cda5ac9ca638f956
Drew <nerddtvg@users.noreply.github.com>
2022-12-11 12:41:20 -0600

    2022 Day 10 Part 1

--- AdventOfCode/Solutions/Year2022/Day10/Solution.cs
+++ AdventOfCode/Solutions/Year2022/Day10/Solution.cs
@@ -11,15 +11,57 @@ namespace AdventOfCode.Solutions.Year2022
 
     class Day10 : ASolution
     {
+        private int register = 0;
 
-        public Day10() : base(10, 2022, "")
+        private int cycle = 0;
+
+        private int signalStrength = 0;
+
+        public Day10() : base(10, 2022, "Cathode-Ray Tube")
         {
+            
+        }
 
+        private void ResetComputer()
+        {
+            register = 1;
+            cycle = 1;
+            signalStrength = 0;
+        }
+
+        private void CheckSignalStrength()
+        {
+            // If this is a signal strength location, add it
+            if (cycle <= 220 && (cycle - 20) % 40 == 0)
+            {
+                signalStrength += cycle * register;
+            }
+        }
+
+        private void ProcessInstruction(string line)
+        {
+            var instructions = line.Split(" ", 2, StringSplitOptions.TrimEntries);
+
+            CheckSignalStrength();
+            cycle++;
+
+            if (instructions[0] == "addx")
+            {
+                // Two-cycle step
+                CheckSignalStrength();
+
+                cycle++;
+                register += Int32.Parse(instructions[1]);
+            }
         }
 
         protected override string? SolvePartOne()
         {
-            return string.Empty;
+            ResetComputer();
+            foreach(var line in Input.SplitByNewline())
+                ProcessInstruction(line);
+
+            return signalStrength.ToString();
         }
 
         protected override string? SolvePartTwo()
