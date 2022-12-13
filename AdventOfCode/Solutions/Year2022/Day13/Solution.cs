8abfb0c8e622b564e0594a4dd9f3fe7acc0188a4
Drew <nerddtvg@users.noreply.github.com>
2022-12-13 10:42:36 -0600

    2022 Day 13 Part 1

--- AdventOfCode/Solutions/Year2022/Day13/Solution.cs
+++ AdventOfCode/Solutions/Year2022/Day13/Solution.cs
@@ -4,7 +4,8 @@ using System.Text;
 using System.Text.RegularExpressions;
 
 using System.Linq;
-
+using System.Diagnostics;
+using System.Text.Json.Nodes;
 
 namespace AdventOfCode.Solutions.Year2022
 {
@@ -12,14 +13,195 @@ namespace AdventOfCode.Solutions.Year2022
     class Day13 : ASolution
     {
 
-        public Day13() : base(13, 2022, "")
+        public Day13() : base(13, 2022, "Distress Signal")
         {
+            var examples = new Dictionary<string, bool>()
+            {
+                {
+                    @"[1,1,3,1,1]
+                    [1,1,5,1,1]",
+                    true
+                },
 
+                {
+                    @"[[1],[2,3,4]]
+                    [[1],4]",
+                    true
+                },
+
+                {
+                    @"[9]
+                    [[8,7,6]]",
+                    false
+                },
+
+                {
+                    @"[[4,4],4,4]
+                    [[4,4],4,4,4]",
+                    true
+                },
+
+                {
+                    @"[7,7,7,7]
+                    [7,7,7]",
+                    false
+                },
+
+                {
+                    @"[]
+                    [3]",
+                    true
+                },
+
+                {
+                    @"[[[]]]
+                    [[]]",
+                    false
+                },
+
+                {
+                    @"[1,[2,[3,[4,[5,6,7]]]],8,9]
+                    [1,[2,[3,[4,[5,6,0]]]],8,9]",
+                    false
+                }
+            };
+
+            foreach(var kvp in examples)
+            {
+                var lines = kvp.Key.SplitByNewline(true);
+                var a = ParseLine(lines[0]);
+                var b = ParseLine(lines[1]);
+                var result = CompareOrder(a, b);
+                Debug.Assert(Debug.Equals(result, kvp.Value), $"Input:\n{lines[0]}\n{lines[1]}\n\nExpected: {kvp.Value}\nActual: {result}");
+            }
+        }
+
+        private JsonNode ParseLine(string line)
+        {
+            return JsonArray.Parse(line) ?? throw new Exception();
+        }
+
+        private void ConvertToArray(JsonNode? parent, int index)
+        {
+            if (parent == default)
+                throw new Exception();
+
+            // Change this node from Number to Array with a single number inside
+            if (parent[index] is JsonValue value)
+            {
+                // Convert to an array
+                var array = JsonArray.Parse($"[{value.ToJsonString()}]");
+
+                // Assign via parent (to keep the DOM correct)
+                parent[index] = array;
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+
+        private int GetIndex(JsonNode child)
+        {
+            if (child.Parent is JsonArray parent)
+                for (int i = 0; i < parent.Count; i++)
+                    if (parent[i] == child)
+                        return i;
+
+            throw new Exception();
+        }
+
+        /// <summary>
+        /// Determine if "left" <paramref name="a" /> is in the right order with "right" <paramref name="b" />
+        /// </summary>
+        /// <returns><c>true</c> if ordered correctlly; otherwise, <c>false</c></returns>
+        private bool? CompareOrder(JsonNode a, JsonNode b)
+        {
+            if (a is JsonValue valueA && b is JsonValue valueB)
+            {
+                // Compare two integers, A should be less than or equal to B
+                var vA = valueA.GetValue<int>();
+                var vB = valueB.GetValue<int>();
+
+                // If we are equal, we move on to the next integer
+                if (vA == vB)
+                    return null;
+
+                return valueA.GetValue<int>() < valueB.GetValue<int>();
+            }
+
+            // If only one is not an Array, then we need to convert it
+            if (a is JsonValue)
+            {
+                var parent = a.Parent;
+                var index = GetIndex(a);
+                ConvertToArray(parent, index);
+                a = parent?[index] ?? throw new Exception();
+            }
+
+            if (b is JsonValue)
+            {
+                var parent = b.Parent;
+                var index = GetIndex(b);
+                ConvertToArray(parent, index);
+                b = parent?[index] ?? throw new Exception();
+            }
+
+            // Now compare two array values
+            // If both values are lists, compare each item
+            // If left runs out first => right orer
+            // If right runs out first => wrong over
+            // If no decision, move on
+            if (a is JsonArray arrayA && b is JsonArray arrayB)
+            {
+                var maxIndex = Math.Min(arrayA.Count, arrayB.Count);
+
+                for (int i = 0; i < maxIndex; i++)
+                {
+                    // Check if these are in the right order
+                    // If we do not get a value back, then we move on
+                    var compare = CompareOrder(arrayA[i] ?? throw new Exception(), arrayB[i] ?? throw new Exception());
+
+                    if (compare.HasValue)
+                        return compare.Value;
+                }
+
+                // We've compared all of our options up to this point, now we're either out of
+                // items or one array is 
+                if (arrayA.Count < arrayB.Count)
+                    return true;
+                if (arrayB.Count < arrayA.Count)
+                    return false;
+
+                // No decision, return null
+                return null;
+            }
+
+            return false;
         }
 
         protected override string? SolvePartOne()
         {
-            return string.Empty;
+            int index = 1;
+            int returnValue = 0;
+
+            foreach(var group in Input.SplitByBlankLine())
+            {
+                // Parse the values
+                var a = ParseLine(group[0]);
+                var b = ParseLine(group[1]);
+
+                // Compare them
+                var result = CompareOrder(a, b);
+
+                // If in order, count this index
+                if (result ?? false)
+                    returnValue += index;
+
+                index++;
+            }
+
+            return returnValue.ToString();
         }
 
         protected override string? SolvePartTwo()
