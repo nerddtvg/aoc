764a81890fbdbf786bf0d13cc8842c9897fd69a8
Drew <nerddtvg@users.noreply.github.com>
2022-12-13 21:10:33 -0600

    2019 Day 25 Trial and Error

--- AdventOfCode/Solutions/Year2019/Day25/Solution.cs
+++ AdventOfCode/Solutions/Year2019/Day25/Solution.cs
@@ -9,14 +9,161 @@ namespace AdventOfCode.Solutions.Year2019
 
     class Day25 : ASolution
     {
+        Intcode computer;
 
-        public Day25() : base(25, 2019, "")
+        public Day25() : base(25, 2019, "Cryostasis")
         {
-
+            computer = new(Input);
         }
 
         protected override string SolvePartOne()
         {
+            var inputs = new string[]
+            {
+                // Initial room has doors east, south, west
+                "inv",
+                // Room 2 has door south and west (room 1)
+                "east",
+                // Room 3 has doors south and north (room 2)
+                "south",
+                // "take giant electromagnet",
+                // Taking the electromagnnet prevents movement
+                // Room 4 has doors north (room 3)
+                "south",
+                "take hologram",
+                // Back to room 3
+                "north",
+                // Back to room 2
+                "north",
+                // Back to room 1
+                "west",
+                // Room 1 to Room 5 has doors north (room 1), east, and west
+                "south",
+                "take mouse",
+                // Room 6 has doors west (room 5)
+                "east",
+                "take shell",
+                // Back to room 5
+                "west",
+                // Room 6 has doors east (room 5)
+                "west",
+                "take whirled peas",
+                // Back to room 5
+                "east",
+                // Back to room 1
+                "north",
+                // Room 1 to Room 7 has doors north, east (room 1), and west
+                "west",
+                // Room 7 to Room 8 has doors east (room 7), south, and west
+                "west",
+                "take antenna",
+                // Room 8 to Room 9 has doors east (room 8) and south
+                "west",
+                // "take photons", // Eaten by a Grue
+                // Room 9 to Room 10 has doors north (Room 9) and south
+                "south",
+                // "take escape pod",
+                // Room 10 to Room 11 has doors north (Room 10) and south
+                "south",
+                // "south" // "Droids on this ship are lighter than the detected value"
+                // Back to room 10
+                "north",
+                // Back to room 9
+                "north",
+                // Back to room 8
+                "east",
+                // Room 8 to Room 12 has doors north (Room 8)
+                "south",
+                "take spool of cat6",
+                // Back to room 8
+                "north",
+                // Back to room 7
+                "east",
+                // Room 7 to Room 13 has doors north, south (Room 7), and west
+                "north",
+                // "take molten lava",
+                // Room 13 to Room 14 has rooms south (Room 13) and west
+                "north",
+                // Room 14 to Room 15 has rooms east (Room 14)
+                "west",
+                "take semiconductor",
+                // Back to room 14
+                "east",
+                // Back to room 13
+                "south",
+                // Room 13 to Room 16 has doors north, east (Room 13), and south
+                "west",
+                // Room 16 to Room 17 has doors south (Room 16)
+                "north",
+                // Back to Room 16
+                "south",
+                // Room 16 to Room 18 has doors north (Room 16)
+                "south",
+                "take hypercube",
+                // Back to Room 16
+                "north",
+                // Back to Room 13
+                "east",
+                // Back to Room 7
+                "south",
+                // Back to Room 1
+                "east",
+                // Room 1 to 7 to 8 to 9 to 10
+                "west", "west", "west", "south", "south",
+                // Move into droid space
+                "south",
+                // Ejected for being too heavy
+                "drop hypercube",
+                "drop shell",
+                "drop whirled peas",
+                "drop spool of cat6",
+                "drop mouse",
+                "drop antenna",
+                "drop hologram",
+                // Too light now
+                "south",
+                "take antenna",
+                "take mouse",
+                "take shell",
+                // Too heavy with the shell
+                "south",
+                "drop shell",
+                // "take whirled peas",
+                // "take hypercube",
+                // "take spool of cat6",
+                // "take hologram",
+                "drop semiconductor",
+                "take whirled peas",
+                // "take hologram",
+                // "take shell",
+                "take hypercube",
+                "take semiconductor",
+                "south",
+                "inv",
+            };
+
+            foreach (var instruction in inputs)
+            {
+                instruction
+                    .ToCharArray()
+                    .Select(c => (long)c)
+                    .ToList()
+                    .ForEach(c => computer.SetInput(c));
+
+                // Append a new line
+                computer.SetInput(10);
+            }
+
+            computer.Run();
+
+            // Print the output
+            foreach(var c in computer.output_register)
+            {
+                Console.Write((char)c);
+            }
+
+            Console.WriteLine();
+
             return string.Empty;
         }
 
