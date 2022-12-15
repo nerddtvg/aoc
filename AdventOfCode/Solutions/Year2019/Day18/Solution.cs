7ebe700f2fd74d0029b54381373441e5a5d4f35f
Drew <nerddtvg@users.noreply.github.com>
2022-12-14 22:10:31 -0600

    Recursion depth issues with BFS

--- AdventOfCode/Solutions/Year2019/Day18/Solution.cs
+++ AdventOfCode/Solutions/Year2019/Day18/Solution.cs
@@ -110,13 +110,17 @@ namespace AdventOfCode.Solutions.Year2019
                 }
             };
 
-            foreach(var exKvp in part1Example)
-            {
-                // ResetGrid(exKvp.Key);
-                // var paths = GetPath(start, default, map.Select(kvp => kvp.Value).ToArray(), Array.Empty<DoorLockPos>()).ToList();
-                // var min = paths.Min(path => path.Count());
-                // Debug.Assert(Debug.Equals(min, exKvp.Value), $"Expected: {exKvp.Value}\nActual: {min}");
-            }
+            // foreach(var exKvp in part1Example)
+            // {
+            //     ResetGrid(exKvp.Key);
+
+            //     var paths = GetShortestPath(start, Array.Empty<char>(), new (int x, int y)[] { start })
+            //         .Select(p => p.ToList())
+            //         .OrderBy(p => p.Count)
+            //         .FirstOrDefault();
+
+            //     Debug.Assert(Debug.Equals(minDistance, exKvp.Value), $"Expected: {exKvp.Value}\nActual: {minDistance}");
+            // }
         }
 
         private void ResetGrid(string input)
@@ -124,6 +128,8 @@ namespace AdventOfCode.Solutions.Year2019
             map = input.SplitByNewline(true)
                 .Select(line => line.ToCharArray())
                 .ToArray();
+
+            minDistance = Int32.MaxValue;
 
             // Find the start
             start = (-1, -1);
@@ -143,12 +149,12 @@ namespace AdventOfCode.Solutions.Year2019
             if (pos.y < 0 || pos.y >= map.Length)
                 return null;
 
-            return map[pos.x][pos.y];
+            return map[pos.y][pos.x];
         }
 
         private IEnumerable<(int x, int y)> GetMoves((int x, int y) pos, char[] keys)
         {
-            foreach(var move in new (int x, int y)[] { (-1, -1), (-1, 1), (1, -1), (1, 1) })
+            foreach(var move in new (int x, int y)[] { (-1, 0), (1, 0), (0, -1), (0, 1) })
             {
                 var newPos = pos.Add(move);
                 var newChar = GetGrid(newPos);
@@ -195,27 +201,31 @@ namespace AdventOfCode.Solutions.Year2019
             foreach(var move in moves)
             {
                 // Duplicate keys and paths
-                var newKeys = keys.Clone();
-                var newPath = path.Clone();
+                var newKeys = (char[])keys.Clone();
+                var newPath = ((int x, int y)[])path.Clone();
+
+                // Adding the move
+                newPath = newPath.Append(move).ToArray();
 
                 // If this move is a key and we don't have it, pick it up and start fresh!
                 var newChar = GetGrid(move);
                 if (97 <= newChar && newChar <= 122 && !keys.Any(c => c == newChar.Value))
                 {
-                    keys = keys.Union(new char[] { newChar.Value }).ToArray();
-                    path = Array.Empty<(int x, int y)>();
-
-                    minDistance = Math.Min(minDistance, depth);
+                    newKeys = newKeys.Union(new char[] { newChar.Value }).ToArray();
+                    newPath = Array.Empty<(int x, int y)>();
 
                     // If this is all of the keys, return our result instead
-                    if (keys.Length == keyCount)
+                    if (newKeys.Length == keyCount)
                     {
-                        yield return keys;
+                        // Found a new length
+                        minDistance = Math.Min(minDistance, depth);
+
+                        yield return newKeys;
                         continue;
                     }
                 }
 
-                foreach(var result in GetShortestPath(move, keys, path, depth))
+                foreach(var result in GetShortestPath(move, newKeys, newPath, depth))
                 {
                     yield return result;
                 }
