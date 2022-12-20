eff7c550db1be5a3ebd8be686b57f78ef57a0987
Drew <nerddtvg@users.noreply.github.com>
2022-12-19 19:53:54 -0600

    2022 Day 17 Scaffold

--- AdventOfCode/Solutions/Year2022/Day17/Solution.cs
+++ AdventOfCode/Solutions/Year2022/Day17/Solution.cs
@@ -11,10 +11,29 @@ namespace AdventOfCode.Solutions.Year2022
 
     class Day17 : ASolution
     {
+        /// <summary>
+        /// Holds the rocks in order of them falling
+        /// </summary>
+        public Queue<RockType> rocks = new();
 
-        public Day17() : base(17, 2022, "")
+        /// <summary>
+        /// Holds our input for air jet directions
+        /// </summary>
+        public Queue<char> airJet = new();
+
+        public Day17() : base(17, 2022, "Pyroclastic Flow")
         {
+            DebugInput = @">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
 
+            // Load the queues
+            rocks.Enqueue(RockType.Horiz);
+            rocks.Enqueue(RockType.Plus);
+            rocks.Enqueue(RockType.Bracket);
+            rocks.Enqueue(RockType.Vertical);
+            rocks.Enqueue(RockType.Box);
+
+            // And our input
+            Input.ToCharArray().ToList().ForEach(c => airJet.Enqueue(c));
         }
 
         protected override string? SolvePartOne()
@@ -25,6 +44,15 @@ namespace AdventOfCode.Solutions.Year2022
         protected override string? SolvePartTwo()
         {
             return string.Empty;
+        }
+
+        public enum RockType
+        {
+            Horiz,
+            Plus,
+            Bracket,
+            Vertical,
+            Box
         }
     }
 }
