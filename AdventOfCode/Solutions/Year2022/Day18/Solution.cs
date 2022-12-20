0abede31b7e03d25845721b21160469e1acd0eba
Drew <nerddtvg@users.noreply.github.com>
2022-12-20 10:28:41 -0600

    2022 Day 18 Part 1

--- AdventOfCode/Solutions/Year2022/Day18/Solution.cs
+++ AdventOfCode/Solutions/Year2022/Day18/Solution.cs
@@ -11,20 +11,66 @@ namespace AdventOfCode.Solutions.Year2022
 
     class Day18 : ASolution
     {
+        private DropletEdge[] points;
 
-        public Day18() : base(18, 2022, "")
+        public Day18() : base(18, 2022, "Boiling Boulders")
         {
+            // DebugInput = @"2,2,2
+            //     1,2,2
+            //     3,2,2
+            //     2,1,2
+            //     2,3,2
+            //     2,2,1
+            //     2,2,3
+            //     2,2,4
+            //     2,2,6
+            //     1,2,5
+            //     3,2,5
+            //     2,1,5
+            //     2,3,5";
 
+            points = Input.SplitByNewline(true)
+                .Select(line => new DropletEdge(new Point<int>(line.Split(",").Select(xyz => Int32.Parse(xyz)).ToArray())))
+                .ToArray();
         }
 
         protected override string? SolvePartOne()
         {
-            return string.Empty;
+            // Coordinates:
+            var coords = points.Select(p => p.coordinate).ToArray();
+
+            return points.Sum(point =>
+            {
+                // On this point, count how many of the neighbors don't exist in our list
+                return point.neighbors.Count(n => !coords.Contains(n));
+            }).ToString();
         }
 
         protected override string? SolvePartTwo()
         {
             return string.Empty;
+        }
+
+        public struct DropletEdge
+        {
+            public Point<int> coordinate;
+
+            public Point<int>[] neighbors;
+
+            public DropletEdge(Point<int> coordinate)
+            {
+                this.coordinate = coordinate;
+
+                this.neighbors = new Point<int>[]
+                {
+                    new(-1, 0, 0),
+                    new(1, 0, 0),
+                    new(0, -1, 0),
+                    new(0, 1, 0),
+                    new(0, 0, -1),
+                    new(0, 0, 1),
+                }.Select(shift => coordinate + shift).ToArray();
+            }
         }
     }
 }
