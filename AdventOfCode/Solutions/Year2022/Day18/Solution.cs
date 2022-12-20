339b283979dcbe0971ad67f9537244ff1c2a00f4
Drew <nerddtvg@users.noreply.github.com>
2022-12-20 15:06:05 -0600

    2022 Day 18 Part 2

--- AdventOfCode/Solutions/Year2022/Day18/Solution.cs
+++ AdventOfCode/Solutions/Year2022/Day18/Solution.cs
@@ -50,89 +50,71 @@ namespace AdventOfCode.Solutions.Year2022
         {
             // Coordinates:
             var coords = points.Select(p => p.coordinate).ToArray();
+
+            // No shortcuts about this one, just need to graph it out I guess
+            // Based on to assist with the graph search: https://old.reddit.com/r/adventofcode/comments/zoqhvy/2022_day_18_solutions/j0vh5gd/
+            var minX = coords.Min(c => c[0])-1;
+            var maxX = coords.Max(c => c[0])+1;
+            var minY = coords.Min(c => c[1])-1;
+            var maxY = coords.Max(c => c[1])+1;
+            var minZ = coords.Min(c => c[2])-1;
+            var maxZ = coords.Max(c => c[2])+1;
+
+            var queue = new Queue<Point<int>>();
             var exteriors = new HashSet<Point<int>>();
 
-            // Look from top, left, right, and bottom
-            // What are the first coordinates we get to in each direction?
-            // Those are exterior edges
-            // var minX = coords.Min(c => c[0])-1;
-            // var maxX = coords.Max(c => c[0])+1;
-            // var minY = coords.Min(c => c[1])-1;
-            // var maxY = coords.Max(c => c[1])+1;
-            // var minZ = coords.Min(c => c[2])-1;
-            // var maxZ = coords.Max(c => c[2])+1;
+            // Start from a known point on the outside
+            queue.Enqueue(new Point<int>(minX, minY, minZ));
 
-            // for (int z = minZ; z <= maxZ; z++)
-            // {
-            //     Console.WriteLine($"z = {z}");
-            //     for (int y = minY; y <= maxY; y++)
-            //     {
-            //         for (int x = minX; x <= maxX; x++)
-            //         {
-            //             if (coords.Contains(new Point<int>(x, y, z)))
-            //                 Console.Write('#');
-            //             else
-            //                 Console.Write('.');
-            //         }
-            //         Console.WriteLine();
-            //     }
-            //     Console.WriteLine();
-            // }
-
-            // ASSUMPTION: No exterior cube has an interior edge exposed
-
-            // Top coords:
-            coords
-                // Group by x,y
-                .GroupBy(coor => (x: coor[0], y: coor[1]))
-                // Find the lowest value z in each x,y column
-                .Select(grp => (grp.Key, grp.Select(coor => coor[2]).Min()))
-                // Convert to Point<int>
-                .Select(grp => new Point<int>(grp.Key.x, grp.Key.y, grp.Item2))
-                .ToList()
-                .ForEach(pt => exteriors.Add(pt));
-
-            // Bottom coords:
-            coords
-                // Group by x,y
-                .GroupBy(coor => (x: coor[0], y: coor[1]))
-                // Find the lowest value z in each x,y column
-                .Select(grp => (grp.Key, grp.Select(coor => coor[2]).Max()))
-                // Convert to Point<int>
-                .Select(grp => new Point<int>(grp.Key.x, grp.Key.y, grp.Item2))
-                .ToList()
-                .ForEach(pt => exteriors.Add(pt));
-
-            // Left coords:
-            coords
-                // Group by z,y
-                .GroupBy(coor => (z: coor[2], y: coor[1]))
-                // Find the lowest value z in each x,y column
-                .Select(grp => (grp.Key, grp.Select(coor => coor[0]).Min()))
-                // Convert to Point<int>
-                .Select(grp => new Point<int>(grp.Key.Item2, grp.Key.y, grp.Key.z))
-                .ToList()
-                .ForEach(pt => exteriors.Add(pt));
-
-            // Right coords:
-            coords
-                // Group by z,y
-                .GroupBy(coor => (z: coor[2], y: coor[1]))
-                // Find the lowest value z in each x,y column
-                .Select(grp => (grp.Key, grp.Select(coor => coor[0]).Max()))
-                // Convert to Point<int>
-                .Select(grp => new Point<int>(grp.Key.Item2, grp.Key.y, grp.Key.z))
-                .ToList()
-                .ForEach(pt => exteriors.Add(pt));
-
-            // Count only exteriors
-            return points
-                .Where(point => exteriors.Contains(point.coordinate))
-                .Sum(point =>
+            // For each point on the outside, count how many edges we bump up against
+            int count = 0;
+            while(queue.Count > 0)
+            {
+                var point = queue.Dequeue();
+                if (!exteriors.Contains(point))
                 {
-                    // On this point, count how many of the neighbors don't exist in our list
-                    return point.neighbors.Count(n => !coords.Contains(n));
-                }).ToString();
+                    // Haven't been here yet, check it out
+                    exteriors.Add(point);
+
+                    foreach(var neighbor in GetNeighbors(point).ToArray())
+                    {
+                        // If this is out of bounds, skip
+                        if (neighbor[0] < minX || maxX < neighbor[0] || neighbor[1] < minY || maxY < neighbor[1] || neighbor[2] < minZ || maxZ < neighbor[2])
+                            continue;
+
+                        // If we have been here (perhaps an earlier iteration, skip it)
+                        if (exteriors.Contains(neighbor))
+                            continue;
+
+                        // Check against our known droplet datapoints
+                        if (coords.Contains(neighbor))
+                        {
+                            // Found an edge to a neighbor on the outside of the droplet
+                            count++;
+                        }
+                        else
+                        {
+                            // Still outside, add to the queue
+                            queue.Enqueue(neighbor);
+                        }
+                    }
+                }
+            }
+
+            return count.ToString();
+        }
+
+        public static IEnumerable<Point<int>> GetNeighbors(Point<int> point)
+        {
+            return new Point<int>[]
+                {
+                    new(-1, 0, 0),
+                    new(1, 0, 0),
+                    new(0, -1, 0),
+                    new(0, 1, 0),
+                    new(0, 0, -1),
+                    new(0, 0, 1)
+                }.Select(shift => point + shift);
         }
 
         public struct DropletEdge
@@ -141,60 +123,11 @@ namespace AdventOfCode.Solutions.Year2022
 
             public Point<int>[] neighbors;
 
-            public Point<int>[] exteriorNeighborsX1;
-            public Point<int>[] exteriorNeighborsX2;
-            public Point<int>[] exteriorNeighborsY1;
-            public Point<int>[] exteriorNeighborsY2;
-            public Point<int>[] exteriorNeighborsZ1;
-            public Point<int>[] exteriorNeighborsZ2;
-
             public DropletEdge(Point<int> coordinate)
             {
                 this.coordinate = coordinate;
 
-                var shifts = new Point<int>[]
-                {
-                    new(-1, 0, 0),
-                    new(1, 0, 0),
-                    new(0, -1, 0),
-                    new(0, 1, 0),
-                    new(0, 0, -1),
-                    new(0, 0, 1)
-                };
-
-                this.neighbors = shifts.Select(shift => coordinate + shift).ToArray();
-
-                var exteriorRange = 5;
-
-                this.exteriorNeighborsX1 = Enumerable.Range(1, exteriorRange).SelectMany(extOffset => new Point<int>[]
-                {
-                    new(-1, 0, 0)
-                }.Select(s => s * extOffset)).Select(shift => coordinate + shift).ToArray();
-
-                this.exteriorNeighborsX2 = Enumerable.Range(1, exteriorRange).SelectMany(extOffset => new Point<int>[]
-                {
-                    new(1, 0, 0)
-                }.Select(s => s * extOffset)).Select(shift => coordinate + shift).ToArray();
-
-                this.exteriorNeighborsY1 = Enumerable.Range(1, exteriorRange).SelectMany(extOffset => new Point<int>[]
-                {
-                    new(0, -1, 0)
-                }.Select(s => s * extOffset)).Select(shift => coordinate + shift).ToArray();
-
-                this.exteriorNeighborsY2 = Enumerable.Range(1, exteriorRange).SelectMany(extOffset => new Point<int>[]
-                {
-                    new(0, 1, 0)
-                }.Select(s => s * extOffset)).Select(shift => coordinate + shift).ToArray();
-
-                this.exteriorNeighborsZ1 = Enumerable.Range(1, exteriorRange).SelectMany(extOffset => new Point<int>[]
-                {
-                    new(0, 0, -1)
-                }.Select(s => s * extOffset)).Select(shift => coordinate + shift).ToArray();
-
-                this.exteriorNeighborsZ2 = Enumerable.Range(1, exteriorRange).SelectMany(extOffset => new Point<int>[]
-                {
-                    new(0, 0, 1)
-                }.Select(s => s * extOffset)).Select(shift => coordinate + shift).ToArray();
+                this.neighbors = Day18.GetNeighbors(coordinate).ToArray();
             }
         }
     }
