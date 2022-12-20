dc7e49d178e090f0c383c01416e8a8a396b03d01
Drew <nerddtvg@users.noreply.github.com>
2022-12-20 11:09:12 -0600

    2022 Day 18 Part 2 Bad Logic

--- AdventOfCode/Solutions/Year2022/Day18/Solution.cs
+++ AdventOfCode/Solutions/Year2022/Day18/Solution.cs
@@ -15,19 +15,19 @@ namespace AdventOfCode.Solutions.Year2022
 
         public Day18() : base(18, 2022, "Boiling Boulders")
         {
-            // DebugInput = @"2,2,2
-            //     1,2,2
-            //     3,2,2
-            //     2,1,2
-            //     2,3,2
-            //     2,2,1
-            //     2,2,3
-            //     2,2,4
-            //     2,2,6
-            //     1,2,5
-            //     3,2,5
-            //     2,1,5
-            //     2,3,5";
+            DebugInput = @"2,2,2
+                1,2,2
+                3,2,2
+                2,1,2
+                2,3,2
+                2,2,1
+                2,2,3
+                2,2,4
+                2,2,6
+                1,2,5
+                3,2,5
+                2,1,5
+                2,3,5";
 
             points = Input.SplitByNewline(true)
                 .Select(line => new DropletEdge(new Point<int>(line.Split(",").Select(xyz => Int32.Parse(xyz)).ToArray())))
@@ -48,7 +48,39 @@ namespace AdventOfCode.Solutions.Year2022
 
         protected override string? SolvePartTwo()
         {
-            return string.Empty;
+            // Coordinates:
+            var coords = points.Select(p => p.coordinate).ToArray();
+
+            return points.Sum(point =>
+            {
+                if (point.coordinate[0] == 2 && point.coordinate[1] == 2 && point.coordinate[2] == 4)
+                    System.Diagnostics.Debugger.Break();
+                // If any of the exterior X, Y, or Z coords are true, then we have an exterior point
+                // Assumption: We should be able to go a large range in at least one direction to be an exterior point
+                if (
+                    (
+                        point.exteriorNeighborsX1.All(n => !coords.Contains(n))
+                        ^
+                        point.exteriorNeighborsX2.All(n => !coords.Contains(n))
+                    )
+                    ||
+                    (
+                        point.exteriorNeighborsY1.All(n => !coords.Contains(n))
+                        ^
+                        point.exteriorNeighborsY2.All(n => !coords.Contains(n))
+                    )
+                    ||
+                    (
+                        point.exteriorNeighborsZ1.All(n => !coords.Contains(n))
+                        ^
+                        point.exteriorNeighborsZ2.All(n => !coords.Contains(n))
+                    )
+                )
+                    // On this point, count how many of the neighbors don't exist in our list
+                    return point.neighbors.Count(n => !coords.Contains(n));
+                else
+                    return 0;
+            }).ToString();
         }
 
         public struct DropletEdge
@@ -57,19 +89,60 @@ namespace AdventOfCode.Solutions.Year2022
 
             public Point<int>[] neighbors;
 
+            public Point<int>[] exteriorNeighborsX1;
+            public Point<int>[] exteriorNeighborsX2;
+            public Point<int>[] exteriorNeighborsY1;
+            public Point<int>[] exteriorNeighborsY2;
+            public Point<int>[] exteriorNeighborsZ1;
+            public Point<int>[] exteriorNeighborsZ2;
+
             public DropletEdge(Point<int> coordinate)
             {
                 this.coordinate = coordinate;
 
-                this.neighbors = new Point<int>[]
+                var shifts = new Point<int>[]
                 {
                     new(-1, 0, 0),
                     new(1, 0, 0),
                     new(0, -1, 0),
                     new(0, 1, 0),
                     new(0, 0, -1),
-                    new(0, 0, 1),
-                }.Select(shift => coordinate + shift).ToArray();
+                    new(0, 0, 1)
+                };
+
+                this.neighbors = shifts.Select(shift => coordinate + shift).ToArray();
+
+                var exteriorRange = 20;
+
+                this.exteriorNeighborsX1 = Enumerable.Range(1, exteriorRange).SelectMany(extOffset => new Point<int>[]
+                {
+                    new(-1, 0, 0)
+                }.Select(s => s * extOffset)).Select(shift => coordinate + shift).ToArray();
+
+                this.exteriorNeighborsX2 = Enumerable.Range(1, exteriorRange).SelectMany(extOffset => new Point<int>[]
+                {
+                    new(1, 0, 0)
+                }.Select(s => s * extOffset)).Select(shift => coordinate + shift).ToArray();
+
+                this.exteriorNeighborsY1 = Enumerable.Range(1, exteriorRange).SelectMany(extOffset => new Point<int>[]
+                {
+                    new(0, -1, 0)
+                }.Select(s => s * extOffset)).Select(shift => coordinate + shift).ToArray();
+
+                this.exteriorNeighborsY2 = Enumerable.Range(1, exteriorRange).SelectMany(extOffset => new Point<int>[]
+                {
+                    new(0, 1, 0)
+                }.Select(s => s * extOffset)).Select(shift => coordinate + shift).ToArray();
+
+                this.exteriorNeighborsZ1 = Enumerable.Range(1, exteriorRange).SelectMany(extOffset => new Point<int>[]
+                {
+                    new(0, 0, -1)
+                }.Select(s => s * extOffset)).Select(shift => coordinate + shift).ToArray();
+
+                this.exteriorNeighborsZ2 = Enumerable.Range(1, exteriorRange).SelectMany(extOffset => new Point<int>[]
+                {
+                    new(0, 0, 1)
+                }.Select(s => s * extOffset)).Select(shift => coordinate + shift).ToArray();
             }
         }
     }
