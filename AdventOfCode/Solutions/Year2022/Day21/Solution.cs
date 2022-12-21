3f80b88af0630c797928cb89b937d3a234d63b0a
Drew <nerddtvg@users.noreply.github.com>
2022-12-21 10:25:04 -0600

    2022 Day 21 Part 1

--- AdventOfCode/Solutions/Year2022/Day21/Solution.cs
+++ AdventOfCode/Solutions/Year2022/Day21/Solution.cs
@@ -11,20 +11,106 @@ namespace AdventOfCode.Solutions.Year2022
 
     class Day21 : ASolution
     {
+        public Dictionary<string, Monkey> monkeys = new();
 
-        public Day21() : base(21, 2022, "")
+        public Day21() : base(21, 2022, "Monkey Math")
         {
+            // DebugInput = @"root: pppw + sjmn
+            //     dbpl: 5
+            //     cczh: sllz + lgvd
+            //     zczc: 2
+            //     ptdq: humn - dvpt
+            //     dvpt: 3
+            //     lfqf: 4
+            //     humn: 5
+            //     ljgn: 2
+            //     sjmn: drzm * dbpl
+            //     sllz: 4
+            //     pppw: cczh / lfqf
+            //     lgvd: ljgn * ptdq
+            //     drzm: hmdt - zczc
+            //     hmdt: 32";
 
+            ReadMonkeys();
+        }
+
+        private void ReadMonkeys()
+        {
+            monkeys.Clear();
+
+            var regex = new Regex(@"(?<monkey>[a-z]+): ((?<number>[0-9]+)|(?<left>[a-z]+) (?<op>[\+\-\*\/]) (?<right>[a-z]+))$");
+
+            foreach(var line in Input.SplitByNewline(true))
+            {
+                var match = regex.Match(line);
+
+                if (!match.Success)
+                    throw new Exception();
+
+                var monkey = new Monkey();
+
+                if (match.Groups.ContainsKey("number") && match.Groups["number"].Length > 0)
+                {
+                    monkey.value = Int32.Parse(match.Groups["number"].Value);
+                }
+                else
+                {
+                    monkey.op = match.Groups["op"].Value[0];
+                    monkey.left = match.Groups["left"].Value;
+                    monkey.right = match.Groups["right"].Value;
+                }
+
+                monkeys.Add(match.Groups["monkey"].Value, monkey);
+            }
         }
 
         protected override string? SolvePartOne()
         {
-            return string.Empty;
+            return SolveMonkey(monkeys["root"]).ToString();
         }
 
         protected override string? SolvePartTwo()
         {
             return string.Empty;
+        }
+
+        /// <summary>
+        /// int overflowed
+        /// </summary>
+        private long SolveMonkey(Monkey monkey)
+        {
+            if (monkey.value.HasValue)
+                return monkey.value.Value;
+
+            var left = SolveMonkey(monkeys[monkey.left ?? throw new Exception()]);
+            var right = SolveMonkey(monkeys[monkey.right ?? throw new Exception()]);
+
+            switch(monkey.op ?? throw new Exception())
+            {
+                case '+':
+                    return left + right;
+
+                case '-':
+                    return left - right;
+
+                case '*':
+                    return left * right;
+
+                case '/':
+                    return left / right;
+            }
+
+            throw new Exception();
+        }
+
+        public struct Monkey
+        {
+            public int? value;
+
+            public char? op;
+
+            public string? left;
+            public string? right;
         }
     }
 }
