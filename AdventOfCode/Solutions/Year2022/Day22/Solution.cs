4d98d8635ecfc3d60dc4e75f6109df6608a7adfd
Drew <nerddtvg@users.noreply.github.com>
2022-12-22 14:12:20 -0600

    2022 Day 22 Intial grid work

--- AdventOfCode/Solutions/Year2022/Day22/Solution.cs
+++ AdventOfCode/Solutions/Year2022/Day22/Solution.cs
@@ -11,10 +11,83 @@ namespace AdventOfCode.Solutions.Year2022
 
     class Day22 : ASolution
     {
+        private char[][] grid;
+        private (int x, int y) start;
 
-        public Day22() : base(22, 2022, "")
+        public Day22() : base(22, 2022, "Monkey Map")
         {
+            grid = ReadGrid(Input);
 
+            start = (FindRowStart(0), 0);
+        }
+
+        private char[][] ReadGrid(string input)
+        {
+            // Read the input into [y][x] char array
+            var arr = input.SplitByNewline()
+                .Select(line => line.ToCharArray())
+                .ToArray();
+
+            return arr;
+        }
+
+        /// <summary>
+        /// Search the grid by row, find the first non-empty character
+        /// </summary>
+        private int FindRowStart(int y)
+        {
+            for (int x = 0; x < grid[y].Length; x++)
+            {
+                if (grid[y][x] != ' ')
+                    return x;
+            }
+
+            throw new Exception();
+        }
+
+        /// <summary>
+        /// Search the grid by row, find the last non-empty character
+        /// </summary>
+        private int FindRowEnd(int y)
+        {
+            for (int x = grid[y].Length - 1; x >= 0; x--)
+            {
+                if (grid[y][x] != ' ')
+                    return x;
+            }
+
+            throw new Exception();
+        }
+
+        /// <summary>
+        /// Search the grid by column, find the first non-empty character
+        /// </summary>
+        private int FindColStart(int x)
+        {
+            for (int y = 0; y < grid.Length; y++)
+            {
+                if (grid[y][x] != ' ')
+                    return y;
+            }
+
+            throw new Exception();
+        }
+
+        /// <summary>
+        /// Search the grid by column, find the last non-empty character
+        /// </summary>
+        private int FindColEnd(int x)
+        {
+            // This may require searching different length arrays
+            for (int y = grid.Length - 1; y >= 0; y--)
+            {
+                if (x >= grid[y].Length) continue;
+
+                if (grid[y][x] != ' ')
+                    return x;
+            }
+
+            throw new Exception();
         }
 
         protected override string? SolvePartOne()
