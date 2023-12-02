abb21e3cc41e37094d8d444838138b5a8e03ce62
Drew <nerddtvg@users.noreply.github.com>
2023-12-01 19:04:20 -0600

    2023 Day 1 Part 1

--- AdventOfCode/Solutions/Year2023/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2023/Day01/Solution.cs
@@ -11,15 +11,21 @@ namespace AdventOfCode.Solutions.Year2023
 
     class Day01 : ASolution
     {
+        private List<int> calibrationNumbers = new();
 
-        public Day01() : base(01, 2023, "")
+        public Day01() : base(01, 2023, "Trebuchet?!")
         {
+            var reg = new Regex("[0-9]");
 
+            calibrationNumbers = Input.SplitByNewline()
+                .Select(line => reg.Matches(line))
+                .Select(allNumbers => int.Parse($"{allNumbers[0].Value}{allNumbers[^1].Value}"))
+                .ToList();
         }
 
         protected override string? SolvePartOne()
         {
-            return string.Empty;
+            return calibrationNumbers.Sum().ToString();
         }
 
         protected override string? SolvePartTwo()
