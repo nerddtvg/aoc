9101dbd7dd14a050b7b29d587ca4f31b6c8de965
Drew <nerddtvg@users.noreply.github.com>
2023-12-01 19:56:43 -0600

    2023 Day 1 Part 2

--- AdventOfCode/Solutions/Year2023/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2023/Day01/Solution.cs
@@ -11,27 +11,42 @@ namespace AdventOfCode.Solutions.Year2023
 
     class Day01 : ASolution
     {
-        private List<int> calibrationNumbers = new();
-
         public Day01() : base(01, 2023, "Trebuchet?!")
+        {
+
+        }
+
+        private int getSum(string input)
         {
             var reg = new Regex("[0-9]");
 
-            calibrationNumbers = Input.SplitByNewline()
+            return input.SplitByNewline()
                 .Select(line => reg.Matches(line))
                 .Select(allNumbers => int.Parse($"{allNumbers[0].Value}{allNumbers[^1].Value}"))
-                .ToList();
+                .Sum();
         }
 
         protected override string? SolvePartOne()
         {
-            return calibrationNumbers.Sum().ToString();
+            return getSum(Input).ToString();
         }
 
         protected override string? SolvePartTwo()
         {
-            return string.Empty;
+            // Words can be overlapping so replacing them with digits + first/last characters makes the original code work
+            var input = Input;
+
+            input = input.Replace("one", "o1e");
+            input = input.Replace("two", "t2o");
+            input = input.Replace("three", "t3e");
+            input = input.Replace("four", "f4r");
+            input = input.Replace("five", "f5e");
+            input = input.Replace("six", "s6x");
+            input = input.Replace("seven", "s7n");
+            input = input.Replace("eight", "e8t");
+            input = input.Replace("nine", "n9e");
+
+            return getSum(input).ToString();
         }
     }
 }
-
