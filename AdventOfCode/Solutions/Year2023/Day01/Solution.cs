21a760c1c3f8ad9b8cc40be5fef8ef21b74875ae
Drew <nerddtvg@users.noreply.github.com>
2023-12-04 15:16:23 -0600

    2021 Day 1 Part 2 Redo - Regex lookbehinds

--- AdventOfCode/Solutions/Year2023/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2023/Day01/Solution.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 
 using System.Linq;
+using Microsoft.VisualBasic;
 
 
 namespace AdventOfCode.Solutions.Year2023
@@ -13,19 +14,37 @@ namespace AdventOfCode.Solutions.Year2023
         [GeneratedRegex("[0-9]")]
         private static partial Regex regex();
 
+        [GeneratedRegex(@"((?<=o)ne|(?<=t)wo|(?<=t)hree|four|five|six|seven|(?<=e)ight|(?<=n)ine|[0-9])")]
+        private static partial Regex regexWithWords();
+
         public Day01() : base(01, 2023, "Trebuchet?!")
         {
 
         }
 
-        private int getSum(string input)
+        private int getSum(string input, int part = 1)
         {
             // For each of the lines in the input
             return input.SplitByNewline()
                 // Match all digits
-                .Select(line => regex().Matches(line))
+                .Select(line => (part == 1 ? regex() : regexWithWords()).Matches(line))
                 // Then find the first and last digit, pair them together and parse into an integer
-                .Select(allNumbers => int.Parse($"{allNumbers[0].Value}{allNumbers[^1].Value}"))
+                .Select(allNumbers => $"{allNumbers[0].Value}{allNumbers[^1].Value}")
+                // For part 2, we need to replace words
+                .Select(numberStr =>
+                    numberStr
+                        .Replace("wo", "2")
+                        .Replace("hree", "3")
+                        .Replace("four", "4")
+                        .Replace("five", "5")
+                        .Replace("six", "6")
+                        .Replace("seven", "7")
+                        .Replace("ight", "8")
+                        .Replace("ine", "9")
+                        // Ordered at bottom to not conflict with "ine"
+                        .Replace("ne", "1")
+                )
+                .Select(int.Parse)
                 // Get the total
                 .Sum();
         }
@@ -37,21 +56,11 @@ namespace AdventOfCode.Solutions.Year2023
 
         protected override string? SolvePartTwo()
         {
-            // Words can be overlapping so replacing them with digits + first/last characters makes the original code work
-            // oneight => o1e8t
-            var input = Input;
-
-            input = input.Replace("one", "o1e");
-            input = input.Replace("two", "t2o");
-            input = input.Replace("three", "t3e");
-            input = input.Replace("four", "f4r");
-            input = input.Replace("five", "f5e");
-            input = input.Replace("six", "s6x");
-            input = input.Replace("seven", "s7n");
-            input = input.Replace("eight", "e8t");
-            input = input.Replace("nine", "n9e");
-
-            return getSum(input).ToString();
+            // Words can be overlapping so we need to account for consumed characters
+            // The lookbehinds will consume the letters at the starts of words
+            // that start with e, o, r, x, n, t
+            // That impacts: one, two, three, eight, nine
+            return getSum(Input, 2).ToString();
         }
     }
 }
