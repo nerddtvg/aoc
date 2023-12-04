5180a8b814f883ccf969412dd4900cb31e7f1397
Drew <nerddtvg@users.noreply.github.com>
2023-12-03 20:16:41 -0600

    2023 Day 3 Part 2

--- AdventOfCode/Solutions/Year2023/Day03/Solution.cs
+++ AdventOfCode/Solutions/Year2023/Day03/Solution.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 
 using System.Linq;
+using System.Diagnostics;
 
 
 namespace AdventOfCode.Solutions.Year2023
@@ -37,7 +38,6 @@ namespace AdventOfCode.Solutions.Year2023
                     // Read each character
                     // If it is a number, append it to the curNumber and scan for validity
                     // If it is anything else, ignore it
-
                     if ('0' <= grid[y][x] && grid[y][x] <= '9')
                     {
                         // Shift the number left and append the next digit
@@ -116,7 +116,72 @@ namespace AdventOfCode.Solutions.Year2023
 
         protected override string? SolvePartTwo()
         {
-            return string.Empty;
+            // Part 2 needs to find all numbers adjacent to '*' symbols
+            // Any two numbers adjacent are gears
+            // Anything besides two numbers is ignored
+            // For this is may be easier to find all gears and "remember"
+            // what gear they are associated with
+            int curNumber = 0;
+            (int x, int y) foundGear = default;
+            List<((int x, int y) pos, int gear)> gears = new();
+
+            for (var y = 0; y < grid.Length; y++)
+            {
+                for (var x = 0; x < grid[0].Length; x++)
+                {
+                    if ('0' <= grid[y][x] && grid[y][x] <= '9')
+                    {
+                        // Shift the number left and append the next digit
+                        curNumber = (curNumber * 10) + (grid[y][x] - '0');
+
+                        if (x > 0)
+                        {
+                            if (y > 0 && grid[y - 1][x - 1] == '*')
+                                foundGear = (x - 1, y - 1);
+                            else if (grid[y][x - 1] == '*')
+                                foundGear = (x - 1, y);
+                            else if (y < grid.Length - 1 && grid[y + 1][x - 1] == '*')
+                                foundGear = (x - 1, y + 1);
+                        }
+
+                        if (y > 0 && grid[y - 1][x] == '*')
+                            foundGear = (x, y - 1);
+                        else if (y < grid.Length - 1 && grid[y + 1][x] == '*')
+                            foundGear = (x, y + 1);
+
+                        if (x < grid[0].Length - 1)
+                        {
+                            if (y > 0 && grid[y - 1][x + 1] == '*')
+                                foundGear = (x + 1, y - 1);
+                            else if (grid[y][x + 1] == '*')
+                                foundGear = (x + 1, y);
+                            else if (y < grid.Length - 1 && grid[y + 1][x + 1] == '*')
+                                foundGear = (x + 1, y + 1);
+                        }
+                    }
+                    else
+                    {
+                        if (curNumber > 0 && foundGear != default)
+                            gears.Add((foundGear, curNumber));
+
+                        curNumber = 0;
+                        foundGear = default;
+                    }
+                }
+
+                // Always reset at the end of the row
+                if (curNumber > 0 && foundGear != default)
+                    gears.Add((foundGear, curNumber));
+
+                curNumber = 0;
+                foundGear = default;
+            }
+
+            return gears
+                .GroupBy(gear => gear.pos)
+                .Where(group => group.Count() == 2)
+                .Sum(group => group.First().gear * group.Last().gear)
+                .ToString();
         }
     }
 }
