9b6175e23172e9f588fda3de2a5aea2d4e011066
Drew <nerddtvg@users.noreply.github.com>
2023-12-03 19:22:45 -0600

    2023 Day 2 Part 1

--- AdventOfCode/Solutions/Year2023/Day02/Solution.cs
+++ AdventOfCode/Solutions/Year2023/Day02/Solution.cs
@@ -12,14 +12,54 @@ namespace AdventOfCode.Solutions.Year2023
     class Day02 : ASolution
     {
 
-        public Day02() : base(02, 2023, "")
+        private List<Game> Games = new();
+        private class Game
         {
+            public int id;
+            public List<GameRound> rounds = new();
+        }
+        private class GameRound
+        {
+            public int r = 0;
+            public int b = 0;
+            public int g = 0;
+        }
 
+        public Day02() : base(02, 2023, "Cube Conundrum")
+        {
+            // Read the inputs
+            var gameRe = new Regex(@"^Game ([\d]+): (.+)$");
+            var roundRe = new Regex(@"(\d+) (blue|red|green)(|, (\d+) (blue|red|green)(|, (\d+) (blue|red|green)))(; |$)");
+
+            Games = Input.SplitByNewline()
+                .Select(line => gameRe.Match(line))
+                .Select(match => new Game
+                {
+                    id = int.Parse(match.Groups[1].Value),
+                    rounds = roundRe.Matches(match.Groups[2].Value)
+                        .Select(round => new GameRound
+                        {
+                            r = (round.Groups[2].Value == "red" ? int.Parse(round.Groups[1].Value) : 0) + (round.Groups[5].Value == "red" ? int.Parse(round.Groups[4].Value) : 0) + (round.Groups[8].Value == "red" ? int.Parse(round.Groups[7].Value) : 0),
+                            g = (round.Groups[2].Value == "green" ? int.Parse(round.Groups[1].Value) : 0) + (round.Groups[5].Value == "green" ? int.Parse(round.Groups[4].Value) : 0) + (round.Groups[8].Value == "green" ? int.Parse(round.Groups[7].Value) : 0),
+                            b = (round.Groups[2].Value == "blue" ? int.Parse(round.Groups[1].Value) : 0) + (round.Groups[5].Value == "blue" ? int.Parse(round.Groups[4].Value) : 0) + (round.Groups[8].Value == "blue" ? int.Parse(round.Groups[7].Value) : 0)
+                        })
+                        .ToList()
+                }
+                )
+                .ToList();
+        }
+
+        /// <summary>
+        /// Return the games that are possible with the provided maximum values
+        /// </summary>
+        private List<Game> FindPossibleGames(int maxR, int maxG, int maxB)
+        {
+            return Games.Where(game => game.rounds.All(round => round.r <= maxR && round.g <= maxG && round.b <= maxB)).ToList();
         }
 
         protected override string? SolvePartOne()
         {
-            return string.Empty;
+            return FindPossibleGames(12, 13, 14).Sum(game => game.id).ToString();
         }
 
         protected override string? SolvePartTwo()
