24fb0f56c47d12eb24224d4df5793a3af6fa6ba5
Drew <nerddtvg@users.noreply.github.com>
2023-12-06 14:42:41 -0600

    Rewrite logic problem

--- AdventOfCode/Solutions/Year2023/Day05/Solution.cs
+++ AdventOfCode/Solutions/Year2023/Day05/Solution.cs
@@ -5,6 +5,8 @@ using System.Text.RegularExpressions;
 
 using System.Linq;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection;
 
 
 namespace AdventOfCode.Solutions.Year2023
@@ -32,6 +34,40 @@ namespace AdventOfCode.Solutions.Year2023
 
         public Day05() : base(05, 2023, "If You Give A Seed A Fertilizer")
         {
+            DebugInput = @"seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4";
+
             // Load the desired seed indices
             var groups = Input.SplitByBlankLine();
             seeds = new Regex(@"\d+").Matches(groups[0][0]).Select(digit => uint.Parse(digit.Value)).ToList();
@@ -39,63 +75,165 @@ namespace AdventOfCode.Solutions.Year2023
             maps = new();
 
             // Load the maps
-            foreach(var order in Enum.GetValues<TypeOrder>().SkipLast(1))
+            for (TypeOrder order = 0; (int)order < TypeOrderCount - 1; order++)
             {
                 // Find the "{key}-to..." section and parse it
                 var section = groups.First(grp => grp[0].StartsWith($"{order}-"));
 
-                for(uint q=1; q<section.Length; q++)
-                {
-                    // Parse the digits again
-                    var digits = new Regex(@"\d+").Matches(section[q]).Select(digit => uint.Parse(digit.Value)).ToList();
+                // We need to reorder the section by sourceIdx
+                var regex = new Regex(@"^(\d+) (\d+) (\d+)$", RegexOptions.Multiline);
 
-                    maps.Add((order, order + 1, digits[1], digits[0], digits[2]));
+                var orderedDigits = regex
+                    .Matches(string.Join('\n', section))
+                    .Select(match => match.Groups.Values.Skip(1).Select(grp => uint.Parse(grp.Value)).ToArray())
+                    .OrderBy(row => row[1])
+                    .ToArray();
+
+                // First step goes in as-is
+                if (order == TypeOrder.seed)
+                {
+                    orderedDigits.ForEach(digits => maps.Add((order, order + 1, digits[1], digits[0], digits[2])));
+
+                    // Fill in gaps at the top and bottom
+                    // 0 => min(source)
+                    // max(source) + count => uint.MaxValue
+                    var min = orderedDigits.Min(digits => digits[1]);
+                    var max = orderedDigits.Max(digits => digits[1]);
+                    var maxCount = orderedDigits.First(digits => digits[1] == max)[2];
+                    max += maxCount;
+
+                    if (min > 0)
+                        maps.Add((TypeOrder.seed, TypeOrder.soil, 0, 0, min));
+
+                    if (max < uint.MaxValue)
+                        maps.Add((TypeOrder.seed, TypeOrder.seed, max, max, uint.MaxValue - max));
+
+                    continue;
                 }
+
+                // Now we have a full range list (0 => uint.MaxValue)
+                // It should be easier to map the destinations now
+
+                var tmpMaps = new List<TypeMap>();
+                TypeMap defaultMap = (TypeOrder.seed, TypeOrder.seed, uint.MaxValue, uint.MaxValue, 0);
+
+                // We need to work up each slice and figure out where each begins/ends
+                var stepDestMap = maps.First(map => map.destIdx == 0);
+
+                var orderedIdx = 0;
+
+                // Pad the end of orderedDigits to include the full range as well
+                var maxDigits = orderedDigits.Max(digits => digits[1]);
+                var maxDigitsCount = orderedDigits.First(digits => digits[1] == maxDigits)[2];
+
+                if (maxDigits + maxDigitsCount < uint.MaxValue)
+                {
+                    orderedDigits = orderedDigits.Append(new uint[] { maxDigits + maxDigitsCount, maxDigits + maxDigitsCount, uint.MaxValue - (maxDigits + maxDigitsCount) }).ToArray();
+                }
+
+                // We work our way up the slices and determine which is next by figuring out where
+                // the Idx + Counts start/end
+                do
+                {
+                    // Remove for future replace
+                    maps.Remove(stepDestMap);
+
+                    // Check if we break out
+                    if (stepDestMap.count == 0 && orderedDigits.Length <= orderedIdx)
+                        break;
+
+                    var stepDestIdx = stepDestMap.destIdx;
+                    var stepSourceIdx = orderedDigits[orderedIdx][1];
+                    var stepSourceCount = orderedDigits[orderedIdx][2];
+
+                    if (stepDestIdx < stepSourceIdx)
+                    {
+                        // Gap at the top where the maps has mappings that orderedDigits doesn't
+                        if (stepDestIdx + stepDestMap.count < stepSourceIdx)
+                        {
+                            // Directly copy this mapping changing the destination type
+                            stepDestMap = stepDestMap.Clone();
+                            stepDestMap.destName = order + 1;
+                            tmpMaps.Add(stepDestMap);
+
+                            // Move up to the next one
+                            stepDestMap = maps
+                                .Where(map => map.destIdx == (stepDestIdx + stepDestMap.count))
+                                .DefaultIfEmpty(defaultMap)
+                                .FirstOrDefault();
+
+                            continue;
+                        }
+
+                        // We have a gap but we have to update the break point
+                        tmpMaps.Add((TypeOrder.seed, order + 1, stepDestIdx, stepDestIdx, stepSourceIdx - stepDestIdx));
+
+                        // Change the old breakpoint
+                        stepDestMap.sourceIdx += stepSourceIdx - stepDestIdx;
+                        stepDestMap.destIdx += stepSourceIdx - stepDestIdx;
+                        stepDestMap.count -= stepSourceIdx - stepDestIdx;
+
+                        if (stepDestMap.count == 0)
+                            stepDestMap = maps.First(map => map.destIdx == stepSourceIdx);
+
+                        continue;
+                    }
+
+                    // At this point we should always have equal values
+                    Debug.Assert(stepDestIdx == stepSourceIdx, "Invalid condition");
+
+                    // We have equal starts, find the lowest "count" step
+                    var minStep = Math.Min(stepDestMap.count, stepSourceCount);
+
+                    // Add the new map
+                    tmpMaps.Add((TypeOrder.seed, order + 1, stepSourceIdx, orderedDigits[orderedIdx][0], minStep));
+
+                    if (stepSourceIdx + minStep == uint.MaxValue)
+                        break;
+
+                    // Update the old map
+                    stepDestMap.sourceIdx += minStep;
+                    stepDestMap.destIdx += minStep;
+                    stepDestMap.count -= minStep;
+
+                    // Update the ordered start and count
+                    orderedDigits[orderedIdx][0] += minStep;
+                    orderedDigits[orderedIdx][1] += minStep;
+                    orderedDigits[orderedIdx][2] -= minStep;
+
+                    // Find the next if needed
+                    if (stepDestMap.count == 0)
+                        stepDestMap = maps.First(map => map.destIdx == stepSourceIdx + minStep);
+
+                    // Increment if needed
+                    if (orderedDigits[orderedIdx][2] == 0)
+                        orderedIdx++;
+                } while (true);
+
+                // We're done, save the new map
+                maps = tmpMaps;
+
+                Console.WriteLine($"seed 0 => {order+1} {maps.First(map => map.sourceIdx == 0).destIdx}");
             }
         }
 
         private uint MapSeedLocation(uint seed)
         {
-            uint value = seed;
+            var map = maps.First(map => map.sourceIdx <= seed && seed <= map.sourceIdx + map.count);
 
-            // We need to find each of the next locations
-            for(TypeOrder order = (TypeOrder)1; (int)order < TypeOrderCount; order++)
-                value = FindNextValue(order, value);
-
-            return value;
-        }
-
-        /// <summary>
-        /// Find the value of <paramref name="orderToFind"/>
-        /// </summary>
-        private uint FindNextValue(TypeOrder orderToFind, uint sourceValue)
-        {
-            // Find the source map that matches
-            // the range of sourceIdx <= sourceValue < (sourceIdx + count)
-            // If it is undefined, then the value is sourceValue
-            // "Any source numbers that aren't mapped correspond to the same destination number. So, seed number 10 corresponds to soil number 10."
-            Func<TypeMap, bool> conditional = map => map.destName == orderToFind && map.sourceIdx <= sourceValue && sourceValue < (map.sourceIdx + map.count);
-
-            if (!maps.Any(conditional))
-                return sourceValue;
-
-            // Switched to Any to kick out bad values due to the excess debug loggin on exceptions
-            var item = maps.First(map => map.destName == orderToFind && map.sourceIdx <= sourceValue && sourceValue < (map.sourceIdx + map.count));
-
-            // Calculate the offset from the start -> count
-            var offset = sourceValue - item.sourceIdx;
-
-            // Return the destIdx with offset
-            return item.destIdx + offset;
+            return map.destIdx + (seed - map.sourceIdx);
         }
 
         protected override string? SolvePartOne()
         {
+            seeds.ForEach(seed => Console.WriteLine($"{seed}: {MapSeedLocation(seed)}"));
+
             return seeds.Min(MapSeedLocation).ToString();
         }
 
         protected override string? SolvePartTwo()
         {
+            return string.Empty;
             // The original seeds array is a set of pairs
             // a b c d ... => (a, b), (c, d), ...
             // a is the start, b is the count
