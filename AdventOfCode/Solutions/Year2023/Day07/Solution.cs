7a57505323bae5d7f79fc40e77986b7bdb48eb2f
Drew Garrett <drew.garrett@ocelotconsulting.com>
2023-12-07 12:50:50 -0600

    2023 Day 7 Simplified

--- AdventOfCode/Solutions/Year2023/Day07/Solution.cs
+++ AdventOfCode/Solutions/Year2023/Day07/Solution.cs
@@ -9,47 +9,7 @@ using System.Diagnostics;
 
 namespace AdventOfCode.Solutions.Year2023
 {
-    using Hand = (char[] cards, int bid, HandType handType, HandType handType2, int rank);
-
-    /// <summary>
-    /// Cards in order of rank
-    /// </summary>
-    public enum CardRank
-    {
-        Two,
-        Three,
-        Four,
-        Five,
-        Six,
-        Seven,
-        Eight,
-        Nine,
-        T,
-        J,
-        Q,
-        K,
-        A
-    }
-
-    /// <summary>
-    /// Cards in order of rank
-    /// </summary>
-    public enum CardRank2
-    {
-        J,
-        Two,
-        Three,
-        Four,
-        Five,
-        Six,
-        Seven,
-        Eight,
-        Nine,
-        T,
-        Q,
-        K,
-        A
-    }
+    using Hand = (char[] cards, int bid, HandType handType, HandType handType2, int rank, int rank2);
 
     /// <summary>
     /// Poker hands in order of rank (None == 0 for scoring)
@@ -68,45 +28,31 @@ namespace AdventOfCode.Solutions.Year2023
 
     class Day07 : ASolution
     {
-        public Dictionary<char, int> CardMap = new()
-        {
-            { '2', (int)CardRank.Two },
-            { '3', (int)CardRank.Three },
-            { '4', (int)CardRank.Four },
-            { '5', (int)CardRank.Five },
-            { '6', (int)CardRank.Six },
-            { '7', (int)CardRank.Seven },
-            { '8', (int)CardRank.Eight },
-            { '9', (int)CardRank.Nine },
-            { 'T', (int)CardRank.T },
-            { 'J', (int)CardRank.J },
-            { 'Q', (int)CardRank.Q },
-            { 'K', (int)CardRank.K },
-            { 'A', (int)CardRank.A }
-        };
-
-        public Dictionary<char, int> CardMap2 = new()
-        {
-            { '2', (int)CardRank2.Two },
-            { '3', (int)CardRank2.Three },
-            { '4', (int)CardRank2.Four },
-            { '5', (int)CardRank2.Five },
-            { '6', (int)CardRank2.Six },
-            { '7', (int)CardRank2.Seven },
-            { '8', (int)CardRank2.Eight },
-            { '9', (int)CardRank2.Nine },
-            { 'T', (int)CardRank2.T },
-            { 'J', (int)CardRank2.J },
-            { 'Q', (int)CardRank2.Q },
-            { 'K', (int)CardRank2.K },
-            { 'A', (int)CardRank2.A }
-        };
-
         public List<Hand> hands;
+
+        string CardRank = "23456789TJQKA";
+        string CardRank2 = "J23456789TQKA";
+
 
         public Day07() : base(07, 2023, "Camel Cards")
         {
             hands = Input.SplitByNewline().Select(line => parseHand(line)).ToList();
+
+            // Now rank them
+            hands = hands
+                .OrderByDescending(hand => hand.handType)
+                .ThenByDescending(hand => hand.cards, new CompareHands() { map = CardRank })
+                // Score them starting at highestRank and moving down
+                .Select((hand, idx) => { hand.rank = hands.Count - idx; return hand; })
+                .ToList();
+
+            // Now rank them again
+            hands = hands
+                .OrderByDescending(hand => hand.handType2)
+                .ThenByDescending(hand => hand.cards, new CompareHands() { map = CardRank2 })
+                // Score them starting at highestRank and moving down
+                .Select((hand, idx) => { hand.rank2 = hands.Count - idx; return hand; })
+                .ToList();
         }
 
         private Hand parseHand(string line, int part = 1)
@@ -116,7 +62,7 @@ namespace AdventOfCode.Solutions.Year2023
             var cards = split[0].ToCharArray();
             var bid = int.Parse(split[1]);
 
-            return (cards, bid, scoreHand(cards), scoreHand(cards, 2), 0);
+            return (cards, bid, scoreHand(cards), scoreHand(cards, 2), 0, 0);
         }
 
         private HandType scoreHand(char[] cards, int part = 1)
@@ -134,12 +80,15 @@ namespace AdventOfCode.Solutions.Year2023
                     .DefaultIfEmpty('J')
                     .GroupBy(c => c)
                     .OrderByDescending(grp => grp.Count())
-                    .ThenByDescending(grp => (int)CardMap2[grp.Key])
+                    .ThenByDescending(grp => CardRank2.IndexOf(grp.Key))
                     .First()
                     .Key;
+
+                // Replace any 'J' with the identified newCard
                 cards = cards.Select(c => c == 'J' ? newCard : c).ToArray();
             }
 
+            // Group the cards together
             var groups = cards.GroupBy(c => c);
             var groupsCount = groups.Count();
 
@@ -169,34 +118,18 @@ namespace AdventOfCode.Solutions.Year2023
 
         protected override string? SolvePartOne()
         {
-            // Now rank them
-            hands = hands
-                .OrderByDescending(hand => hand.handType)
-                .ThenByDescending(hand => hand.cards, new CompareHands() { map = CardMap })
-                // Score them starting at highestRank and moving down
-                .Select((hand, idx) => { hand.rank = hands.Count - idx; return hand; })
-                .ToList();
-
             return hands.Sum(hand => hand.bid * hand.rank).ToString();
         }
 
         protected override string? SolvePartTwo()
         {
-            // Now rank them
-            hands = hands
-                .OrderByDescending(hand => hand.handType2)
-                .ThenByDescending(hand => hand.cards, new CompareHands() { map = CardMap2 })
-                // Score them starting at highestRank and moving down
-                .Select((hand, idx) => { hand.rank = hands.Count - idx; return hand; })
-                .ToList();
-
-            return hands.Sum(hand => hand.bid * hand.rank).ToString();
+            return hands.Sum(hand => hand.bid * hand.rank2).ToString();
         }
     }
 
     public class CompareHands : IComparer<char[]>
     {
-        public Dictionary<char, int> map = new();
+        public string map = string.Empty;
         public int Compare(char[] a, char[] b)
         {
             //return positive if a should be higher, return negative if b should be higher
@@ -208,9 +141,9 @@ namespace AdventOfCode.Solutions.Year2023
 
             for(int i =0; i<5; i++)
             {
-                if (map[a[i]] > map[b[i]])
+                if (map.IndexOf(a[i]) > map.IndexOf(b[i]))
                     return 1;
-                else if (map[a[i]] < map[b[i]])
+                else if (map.IndexOf(a[i]) < map.IndexOf(b[i]))
                     return -1;
             }
 
