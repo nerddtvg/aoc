9ef46b0adf959480256399ec28a501e99ec19687
Drew Garrett <drew.garrett@ocelotconsulting.com>
2023-12-08 08:43:48 -0600

    2023 Day 8 Part 1

--- AdventOfCode/Solutions/Year2023/Day08/Solution.cs
+++ AdventOfCode/Solutions/Year2023/Day08/Solution.cs
@@ -8,18 +8,59 @@ using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2023
 {
-
     class Day08 : ASolution
     {
-
-        public Day08() : base(08, 2023, "")
+        private class Node
         {
+            public string L = string.Empty;
+            public string R = string.Empty;
+        }
 
+        private Dictionary<string, Node> map;
+        private string instructions;
+
+        const string startNode = "AAA";
+        const string endNode = "ZZZ";
+
+        public Day08() : base(08, 2023, "Haunted Wasteland")
+        {
+            var groups = Input.SplitByBlankLine();
+
+            instructions = groups[0].JoinAsString();
+
+            map = groups[1].Select(line =>
+            {
+                var matches = new Regex(@"^([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)").Match(line);
+
+                return new KeyValuePair<string, Node>(matches.Groups[1].Value, new Node() { L = matches.Groups[2].Value, R = matches.Groups[3].Value });
+            }).ToDictionary();
+        }
+
+        private int FindStepsToEnd()
+        {
+            var step = 0;
+
+            // Track our node and instruction count
+            var currentNode = startNode;
+            var currentIdx = 0;
+
+            do
+            {
+                if (instructions[currentIdx++] == 'L')
+                    currentNode = map[currentNode].L;
+                else
+                    currentNode = map[currentNode].R;
+
+                currentIdx %= instructions.Length;
+                step++;
+            } while (currentNode != endNode);
+
+            return step;
         }
 
         protected override string? SolvePartOne()
         {
-            return string.Empty;
+            return FindStepsToEnd().ToString();
         }
 
         protected override string? SolvePartTwo()
