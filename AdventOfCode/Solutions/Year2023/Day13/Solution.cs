a16d4d15d2a65c2f816e065f61f0b366099c72c8
Drew Garrett <drew.garrett@ocelotconsulting.com>
2023-12-13 10:08:07 -0600

    2023 Day 13 Part 2

--- AdventOfCode/Solutions/Year2023/Day13/Solution.cs
+++ AdventOfCode/Solutions/Year2023/Day13/Solution.cs
@@ -5,6 +5,7 @@ using System.Text.RegularExpressions;
 
 using System.Linq;
 using System.Reflection;
+using System.Diagnostics;
 
 
 namespace AdventOfCode.Solutions.Year2023
@@ -13,19 +14,34 @@ namespace AdventOfCode.Solutions.Year2023
     class Day13 : ASolution
     {
         private string[][] groups;
+        private List<int> results = new();
 
         public Day13() : base(13, 2023, "Point of Incidence")
         {
+            // DebugInput = @"#.##..##.
+            //                ..#.##.#.
+            //                ##......#
+            //                ##......#
+            //                ..#.##.#.
+            //                ..##..##.
+            //                #.#.##.#.
+
+            //                #...##..#
+            //                #....#..#
+            //                ..##..###
+            //                #####.##.
+            //                #####.##.
+            //                ..##..###
+            //                #....#..#";
+
             // For Part 2, make these 0/1 strings so we can convert to numbers quickly
             groups = Input.SplitByBlankLine(true)
                 .Select(group => group.Select(line => line.Replace('.', '0').Replace('#', '1')).ToArray())
                 .ToArray();
         }
 
-        private int GetMirrorPosition(uint[] rows, int part)
+        private int GetMirrorPosition(uint[] rows, int cache, int part)
         {
-            int ret = 0;
-
             // Work down each row provided
             // Look at the current row and next row
             // If they match:
@@ -34,6 +50,11 @@ namespace AdventOfCode.Solutions.Year2023
             // * If the end is reached and rows matched, return the original row index
             for (int i = 0, q = 1; i < rows.Length-1; i++, q++)
             {
+                // If we have a cache and we match, skip this for part 2
+                // part 2 demands different results
+                if (cache > 0 && i + 1 == cache)
+                    continue;
+
                 // If rows[i] and rows[q] match, then we need to work backwards
                 if (rows[i] == rows[q] || (part == 2 && Part2Match(rows[i], rows[q])))
                 {
@@ -42,15 +63,16 @@ namespace AdventOfCode.Solutions.Year2023
 
                     // Need to track if we have used the "smudge"
                     // For part 1, we keep this true to prevent issues later
-                    var usedPart2 = part == 1 || rows[i] != rows[q];
+                    var usedSmudge = part == 1 || rows[i] != rows[q];
 
                     for (int i2 = i - 1, q2 = q + 1; matched && 0 <= i2 && q2 < rows.Length; i2--, q2++)
                     {
                         if (rows[i2] != rows[q2])
                         {
-                            if (!usedPart2 && Part2Match(rows[i2], rows[q2]))
+                            // If we haven't used the "smudge" yet, we could here
+                            if (!usedSmudge && Part2Match(rows[i2], rows[q2]))
                             {
-                                usedPart2 = true;
+                                usedSmudge = true;
                                 continue;
                             }
 
@@ -60,11 +82,12 @@ namespace AdventOfCode.Solutions.Year2023
 
                     // Return the 1-indexed position
                     // Not starting from zero
-                    if (matched) return i + 1;
+                    // usedSmudge makes sure in part 2 we have used a "smudge"
+                    if (matched && usedSmudge) return i + 1;
                 }
             }
 
-            return ret;
+            return 0;
         }
 
         private bool Part2Match(uint a, uint b)
@@ -86,32 +109,39 @@ namespace AdventOfCode.Solutions.Year2023
             return group.Select(line => Convert.ToUInt32(line, 2)).ToArray();
         }
 
-        private int FindMirror(string[] group, int part)
+        private int FindMirror(string[] group, int cache=0)
         {
+            // If we have a cache we are in part 2, need to _NOT_
+            // find the same value
+            int part = cache == 0 ? 1 : 2;
+
             // Provided a group of rows, find the mirror position
             var cols = StringsToInts(RowsToColums(group));
 
-            var mirror = GetMirrorPosition(cols, part);
+            var mirror = GetMirrorPosition(cols, cache, part);
 
             if (mirror > 0)
                 return mirror;
 
-            mirror = GetMirrorPosition(StringsToInts(group), part);
+            mirror = GetMirrorPosition(StringsToInts(group), cache / 100, part);
 
-            if (mirror == 0)
-                throw new Exception();
+            Debug.Assert(mirror > 0);
 
             return 100 * mirror;
         }
 
         protected override string? SolvePartOne()
         {
-            return groups.Sum(group => FindMirror(group, 1)).ToString();
+            groups.ForEach(group => results.Add(FindMirror(group)));
+
+            return results.Sum().ToString();
         }
 
         protected override string? SolvePartTwo()
         {
-            return groups.Sum(group => FindMirror(group, 2)).ToString();
+            var newResults = groups.Select((group, idx) => FindMirror(group, results[idx])).ToList();
+
+            return newResults.Sum().ToString();
         }
     }
 }
