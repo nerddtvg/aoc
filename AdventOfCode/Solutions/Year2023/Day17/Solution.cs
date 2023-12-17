523d162a42407428804af2096923c310070b20ca
Drew Garrett <drew.garrett@ocelotconsulting.com>
2023-12-17 13:16:44 -0600

    Back to weighted BFS, wrong answer

--- AdventOfCode/Solutions/Year2023/Day17/Solution.cs
+++ AdventOfCode/Solutions/Year2023/Day17/Solution.cs
@@ -25,7 +25,6 @@ namespace AdventOfCode.Solutions.Year2023
         private int[][] grid;
         private int maxX;
         private int maxY;
-        private int heatLoss;
 
         private Dictionary<Direction, Point> deltas = new()
         {
@@ -57,44 +56,30 @@ namespace AdventOfCode.Solutions.Year2023
             maxX = grid[0].Length - 1;
         }
 
-        private void RunGrid(Point start, Point end)
+        private int RunGrid(Point start, Point end)
         {
-            heatLoss = int.MaxValue;
-
-            // BFS: Never backtrack, know if we have been somewhere
-            // But we need to account for the heatLoss quantity and
-            // if we have been to the same point but have less heatLoss
-            // then we should continue
-            var seen = new Dictionary<Point, int>();
+            // Using a priority queue based on heatLoss ensures
+            // our first end will be the lowest hestLoss possible
+            // Our seen only needs to know if this has been visited
+            // ever in the past
+            var seen = new HashSet<Point>();
 
             // Track our queue of work
-            var queue = new PriorityQueue<(Point pos, Direction dir, int straightCount, int heatLoss), int>();
+            // Priority is heatLoss so the lowest heatLoss is always tried next
+            var queue = new PriorityQueue<(Point pos, Direction dir, int straightCount), int>();
 
-            queue.Enqueue((start, Direction.Right, 0, 0), 0);
+            queue.Enqueue((start, Direction.Right, 0), 0);
+            queue.Enqueue((start, Direction.Down, 0), 0);
 
-            while (queue.Count > 0)
+            while (queue.TryDequeue(out var item, out var thisHeatLoss))
             {
-                (var thisPos, var thisDir, var thisStraight, var thisHeatLoss) = queue.Dequeue();
+                (var thisPos, var thisDir, var thisStraight) = item;
 
                 // If we are at the end, skip out
                 if (thisPos == end)
                 {
-                    heatLoss = Math.Min(thisHeatLoss, heatLoss);
-                    continue;
+                    return thisHeatLoss;
                 }
-
-                // If we're already over the minimum, get out of here
-                // Not using <= here because we already checked for the end
-                // position which means we are always going to increase
-                // on the next iteration in this loop
-                if (heatLoss < thisHeatLoss)
-                    continue;
-
-                // Making sure if we have been here before we have a reason to continue
-                if (seen.TryGetValue(thisPos, out int seenHeatLoss) && seenHeatLoss < thisHeatLoss)
-                    continue;
-
-                seen[thisPos] = thisHeatLoss;
 
                 // Get our next steps
                 // Add heat loss in the queue because the start
@@ -102,42 +87,34 @@ namespace AdventOfCode.Solutions.Year2023
                 var left = (Direction)(((int)thisDir + 3) % 4);
                 var right = (Direction)(((int)thisDir + 1) % 4);
 
-                Point posLeft = thisPos.Add(deltas[left]);
-                Point posStraight = thisPos.Add(deltas[thisDir]);
-                Point posRight = thisPos.Add(deltas[right]);
-
                 var newPoints = new (Point newPos, Direction newDir)[] {
-                    (posLeft, left),
-                    (posStraight, thisDir),
-                    (posRight, right)
+                    (thisPos.Add(deltas[left]), left),
+                    (thisPos.Add(deltas[thisDir]), thisDir),
+                    (thisPos.Add(deltas[right]), right)
                 };
 
                 foreach((var newPos, var newDir) in newPoints)
                 {
-                    if (IsInGrid(newPos) && (newDir != thisDir || thisStraight < 3))
+                    // Make sure it is in the grid
+                    // Make sure the move is valid
+                    // Make sure we add it to the HashSet (this is false if already visited)
+                    if (IsInGrid(newPos) && (newDir != thisDir || thisStraight < 3) && seen.Add(newPos))
                     {
                         var newHeatLoss = thisHeatLoss + grid[newPos.y][newPos.x];
 
-                        // Weight the priority so that we are always moving down and to the right if possible
-                        var priority = (int)newPos.ManhattanDistance(end) + (newDir == Direction.Up || newDir == Direction.Left ? 5 : 0);
-
-                        // Skip ahead just to not pollute the queue
-                        if (newHeatLoss > heatLoss)
-                            continue;
-
-                        queue.Enqueue((newPos, newDir, newDir == thisDir ? (thisStraight + 1) : 0, newHeatLoss), priority);
+                        queue.Enqueue((newPos, newDir, newDir == thisDir ? (thisStraight + 1) : 0), newHeatLoss);
                     }
                 }
             }
+
+            return int.MaxValue;
         }
 
         private bool IsInGrid(Point point) => 0 <= point.y && point.y <= maxY && 0 <= point.x && point.x <= maxX;
 
         protected override string? SolvePartOne()
         {
-            RunGrid((0, 0), (maxX, maxY));
-
-            return heatLoss.ToString();
+            return RunGrid((0, 0), (maxX, maxY)).ToString();
         }
 
         protected override string? SolvePartTwo()
