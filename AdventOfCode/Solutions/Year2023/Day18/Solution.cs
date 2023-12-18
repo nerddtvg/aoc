c865b24ff867332ba5cba81e165fa89d4d335b13
Drew Garrett <drew.garrett@ocelotconsulting.com>
2023-12-18 11:40:08 -0600

    2023 Day 18 Part 1

--- AdventOfCode/Solutions/Year2023/Day18/Solution.cs
+++ AdventOfCode/Solutions/Year2023/Day18/Solution.cs
@@ -4,6 +4,8 @@ using System.Text;
 using System.Text.RegularExpressions;
 
 using System.Linq;
+using System.Reflection;
+using System.Diagnostics;
 
 
 namespace AdventOfCode.Solutions.Year2023
@@ -69,9 +71,42 @@ namespace AdventOfCode.Solutions.Year2023
                 }, instruction.length);
             }
 
-            DrawPit();
+            FillPit();
 
-            return string.Empty;
+            return pit.Count.ToString();
+        }
+
+        private void FillPit()
+        {
+            // Fill method will be a basic search method
+            // Start with the first known "in pit" point
+            // This will be the corner inside the top points
+            Point topLeftCorner = pit.OrderBy(itm => itm.y).ThenBy(itm => itm.x).First();
+            topLeftCorner = (topLeftCorner.x + 1, topLeftCorner.y + 1);
+
+            // Make sure that we have a true corner
+            Debug.Assert(pit.Contains((topLeftCorner.x - 1, topLeftCorner.y)));
+            Debug.Assert(pit.Contains((topLeftCorner.x, topLeftCorner.y - 1)));
+            Debug.Assert(!pit.Contains((topLeftCorner.x, topLeftCorner.y)));
+
+            // For each point up, down, left, right
+            // If the point is in the pit: skip
+            // If not, add to the pit and keep going
+            var queue = new Queue<Point>();
+            queue.Enqueue(topLeftCorner);
+
+            while(queue.TryDequeue(out Point point))
+            {
+                if (pit.Contains(point))
+                    continue;
+
+                pit.Add(point);
+
+                queue.Enqueue(point.Add(deltas['U']));
+                queue.Enqueue(point.Add(deltas['R']));
+                queue.Enqueue(point.Add(deltas['D']));
+                queue.Enqueue(point.Add(deltas['L']));
+            }
         }
 
         private void DrawPit()
@@ -80,6 +115,8 @@ namespace AdventOfCode.Solutions.Year2023
             var minY = pit.Min(itm => itm.y);
             var maxX = pit.Max(itm => itm.x);
             var maxY = pit.Max(itm => itm.y);
+
+            Console.WriteLine("Pit:");
 
             for (int y = minY; y <= maxY; y++)
             {
