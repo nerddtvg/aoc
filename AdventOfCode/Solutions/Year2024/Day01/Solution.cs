24c22a95893060dae4c7616b4d84e01d64ca0071
Drew <nerddtvg@users.noreply.github.com>
2024-12-01 09:51:49 -0600

    2024 Day 1 Part 1

--- AdventOfCode/Solutions/Year2024/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2024/Day01/Solution.cs
@@ -12,13 +12,37 @@ namespace AdventOfCode.Solutions.Year2024
     class Day01 : ASolution
     {
 
-        public Day01() : base(01, 2024, "")
-        {
+        public int[] List1;
+        public int[] List2;
 
+        public Day01() : base(01, 2024, "Historian Hysteria")
+        {
+//             DebugInput = @"3   4
+// 4   3
+// 2   5
+// 1   3
+// 3   9
+// 3   3";
+
+            var inputs = Input
+                .SplitByNewline()
+                .Select(line => line.Split("   "))
+                .Select(item => new int[] { int.Parse(item[0]), int.Parse(item[1]) })
+                .ToArray();
+
+            List1 = inputs.Select(row => row[0]).ToArray();
+            List2 = inputs.Select(row => row[1]).ToArray();
         }
 
         protected override string? SolvePartOne()
         {
+            List1 = List1.Order().ToArray();
+            List2 = List2.Order().ToArray();
+
+            return Enumerable.Range(0, List1.Length)
+                .Sum(i => Math.Abs(List1[i] - List2[i]))
+                .ToString();
+
             return string.Empty;
         }
 
