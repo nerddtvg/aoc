4d0fada337b6bcf14916cb873e88122189b0cda3
Drew <nerddtvg@users.noreply.github.com>
2024-12-02 07:18:46 -0600

    2024 Day 2 Part 2

--- AdventOfCode/Solutions/Year2024/Day02/Solution.cs
+++ AdventOfCode/Solutions/Year2024/Day02/Solution.cs
@@ -38,6 +38,30 @@ namespace AdventOfCode.Solutions.Year2024
             return !diffs.Any(d => d == 0 || d < -3 || d > 3);
         }
 
+        public bool IsSafeP2(int[] report)
+        {
+            // Go through each report and:
+            // 1. If IsSafe as-is, return true
+            // 2. Remove a single instance, try again
+            // 3. If IsSafe, count it
+            // 4. Repeat until all variances are tried
+            // 5. Check that safeCount == 1
+
+            // There is probably a faster way to analyze this
+            // But at 0.0064284 seconds, I didn't care
+
+            if (IsSafe(report)) return true;
+
+            for (int i = 0; i < report.Length; i++)
+            {
+                // Take + Skip to avoid the item in this index
+                if (IsSafe(report.Take(i).Concat(report.Skip(i + 1)).ToArray()))
+                    return true;
+            }
+
+            return false;
+        }
+
         protected override string? SolvePartOne()
         {
             return reports.Count(IsSafe).ToString();
@@ -45,7 +69,7 @@ namespace AdventOfCode.Solutions.Year2024
 
         protected override string? SolvePartTwo()
         {
-            return string.Empty;
+            return reports.Count(IsSafeP2).ToString();
         }
     }
 }
