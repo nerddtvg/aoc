d878c3ce1108f4e6762432b8ce30d712ecf9040d
Drew <nerddtvg@users.noreply.github.com>
2024-12-03 19:46:56 -0600

    2024 Day 3 Part 1

--- AdventOfCode/Solutions/Year2024/Day03/Solution.cs
+++ AdventOfCode/Solutions/Year2024/Day03/Solution.cs
@@ -11,15 +11,19 @@ namespace AdventOfCode.Solutions.Year2024
 
     class Day03 : ASolution
     {
+        public Regex regex = new("mul\\(([0-9]{1,3}),([0-9]{1,3})\\)");
 
-        public Day03() : base(03, 2024, "")
+        public Day03() : base(03, 2024, "Mull It Over")
         {
-
+            // DebugInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
         }
 
         protected override string? SolvePartOne()
         {
-            return string.Empty;
+            // Time: 00:00:00.0034292
+            return regex.Matches(Input)
+                .Sum(match => ulong.Parse(match.Groups[1].Value) * ulong.Parse(match.Groups[2].Value))
+                .ToString();
         }
 
         protected override string? SolvePartTwo()
