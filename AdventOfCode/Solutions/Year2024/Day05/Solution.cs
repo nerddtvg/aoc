8a99c8fff8112e3360ded1a38dd4d3a61a982518
Drew <nerddtvg@users.noreply.github.com>
2024-12-06 21:48:37 -0600

    2024 Day 5 Part 2

--- AdventOfCode/Solutions/Year2024/Day05/Solution.cs
+++ AdventOfCode/Solutions/Year2024/Day05/Solution.cs
@@ -13,7 +13,8 @@ namespace AdventOfCode.Solutions.Year2024
     {
         // Tracks pages printed after Key page
         public Dictionary<int, HashSet<int>> after = new();
-        public string[] instructions;
+        public int[][] instructions;
+        public List<int[]> invalid = new();
 
         public Day05() : base(05, 2024, "Print Queue")
         {
@@ -58,42 +59,79 @@ namespace AdventOfCode.Solutions.Year2024
                 after[vals[0]].Add(vals[1]);
             });
 
-            instructions = inputs[1];
+            instructions = inputs[1].Select(line => line.ToIntArray(",")).ToArray();
         }
 
-        public bool IsValid(int[] pages)
+        public (bool valid, int i, int q) IsValid(int[] pages)
         {
-            return Enumerable.Range(0, pages.Length - 1).All(i =>
+            for (int i = 0; i < pages.Length; i++)
             {
-                return Enumerable
-                .Range(i + 1, pages.Length - i - 1)
-                .All(q =>
-                // Make sure either pages[i] doesn't exist OR it includes pages[q] for AFTER
-                (!after.ContainsKey(pages[i]) || after[pages[i]].Contains(pages[q]))
-                &&
-                // Make sure either pages[q] doesn't exist OR it does NOT include pages[i] for BEFORE
-                (!after.ContainsKey(pages[q]) || !after[pages[q]].Contains(pages[i]))
-                );
-            });
+                for (int q = i + 1; q < pages.Length; q++)
+                {
+                    if (!(
+                        // Make sure either pages[i] doesn't exist OR it includes pages[q] for AFTER
+                        (!after.ContainsKey(pages[i]) || after[pages[i]].Contains(pages[q]))
+                        &&
+                        // Make sure either pages[q] doesn't exist OR it does NOT include pages[i] for BEFORE
+                        (!after.ContainsKey(pages[q]) || !after[pages[q]].Contains(pages[i]))
+                    ))
+                        return (false, i, q);
+                }
+            }
+
+            return (true, 0, 0);
         }
 
         protected override string? SolvePartOne()
         {
             // Time: 00:00:00.0182829
-            return instructions.Select(line =>
+            return instructions.Select(pages =>
             {
-                var pages = line.ToIntArray(",");
+                if (pages.Length % 2 == 0) return 0;
 
-                if (pages.Length % 2 == 1 && IsValid(pages))
+                var (valid, i, q) = IsValid(pages);
+
+                if (valid)
                     return pages[pages.Length / 2];
+                else
+                {
+                    // Swap the i and q positions
+                    Swap(pages, (i, q));
+                    invalid.Add(pages);
+                }
 
                 return 0;
             }).Sum().ToString();
         }
 
+        private int[] Swap(int[] pages, (int i, int q) swap) {
+            (pages[swap.i], pages[swap.q]) = (pages[swap.q], pages[swap.i]);
+
+            return pages;
+        }
+
+        // We could check all permutations, but that is brute force and slow
+        // This will swap two invalid entries until it finds a valid record
+        private int[] FindValid(int[] pages) {
+            var (valid, i, q) = IsValid(pages);
+
+            if (valid)
+                return pages;
+
+            Swap(pages, (i, q));
+            return FindValid(pages);
+        }
+
         protected override string? SolvePartTwo()
         {
-            return string.Empty;
+            return invalid.Select(pages =>
+            {
+                if (pages.Length % 2 == 0) return 0;
+
+                var valid = FindValid(pages);
+
+                return pages[pages.Length / 2];
+            }).Sum().ToString();
         }
     }
 }
