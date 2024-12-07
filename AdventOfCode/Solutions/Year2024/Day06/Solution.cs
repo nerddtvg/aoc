27f47861bb2eedffb3fd90489837c07d7fa19f74
Drew <nerddtvg@users.noreply.github.com>
2024-12-06 22:46:22 -0600

    2024 Day 6 Part 2

--- AdventOfCode/Solutions/Year2024/Day06/Solution.cs
+++ AdventOfCode/Solutions/Year2024/Day06/Solution.cs
@@ -20,7 +20,8 @@ namespace AdventOfCode.Solutions.Year2024
         }
 
         private char[][] grid;
-        private HashSet<(int x, int y)> visited = new();
+        private Dictionary<(int x, int y), HashSet<Dir>> visited = new();
+        private (int x, int y) start;
         private (int x, int y) pos;
         private Dir dir = Dir.North;
         private Dictionary<Dir, (int x, int y)> move = new() {
@@ -51,26 +52,38 @@ namespace AdventOfCode.Solutions.Year2024
                 for (int x = 0; x < grid[y].Length && !found; x++)
                     if (grid[y][x] == '^')
                     {
-                        pos = (x, y);
+                        start = (x, y);
                         found = true;
                         break;
                     }
-
-            visited.Add(pos);
         }
 
-        private bool Move() {
+        private void Reset()
+        {
+            pos = start;
+            dir = Dir.North;
+
+            // Clear out the visited array each time
+            visited = new() { [start] = [dir] };
+        }
+
+        private bool Move((int x, int y)? fakeObstacle = null)
+        {
             (int x, int y) newPos = pos.Add(move[dir]);
 
-            if (newPos.x < 0 || newPos.y < 0 || grid[0].Length <= newPos.x || grid.Length <= newPos.y) {
+            if (newPos.x < 0 || newPos.y < 0 || grid[0].Length <= newPos.x || grid.Length <= newPos.y)
+            {
                 return false;
             }
-            
-            switch(grid[newPos.y][newPos.x]) {
+
+            switch (fakeObstacle?.x == newPos.x && fakeObstacle?.y == newPos.y ? '#' : grid[newPos.y][newPos.x])
+            {
                 case '.':
                 case '^':
                     pos = newPos;
-                    visited.Add(pos);
+                    // This tracks what we have visited
+                    if (!visited.ContainsKey(pos))
+                        visited[pos] = new();
                     break;
 
                 case '#':
@@ -84,15 +97,42 @@ namespace AdventOfCode.Solutions.Year2024
 
         protected override string? SolvePartOne()
         {
-            while(Move()) {
+            // Time: 00:00:00.0058982
+            Reset();
+            while (Move())
+            {
                 // Keep moving
             }
-            return visited.Count.ToString();
+            return visited.Keys.Count.ToString();
         }
 
         protected override string? SolvePartTwo()
         {
-            return string.Empty;
+            // Time: 00:00:05.8077128
+            // We will review all visited locations and attempt to place an object there
+            var visitedKeys = visited.Keys.Where(key => key.x != start.x || key.y != start.y).ToArray();
+
+            int validObstructions = 0;
+
+            foreach (var key in visitedKeys)
+            {
+                // if (key == (5, 1)) System.Diagnostics.Debugger.Break();
+                Reset();
+                while (Move(key))
+                {
+                    // Check if we have visited this location before
+                    // Key is valid because it is added in Move()
+                    if (visited[pos].Contains(dir)) {
+                        validObstructions++;
+                        break;
+                    }
+
+                    // Otherwise add this
+                    visited[pos].Add(dir);
+                }
+            }
+
+            return validObstructions.ToString();
         }
     }
 }
