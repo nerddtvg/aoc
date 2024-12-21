efbd199309711bc810d8b32c2ed3e4e319ddd6af
Drew <nerddtvg@users.noreply.github.com>
2024-12-20 22:20:24 -0600

    2024 Day 20 Part 2

--- AdventOfCode/Solutions/Year2024/Day19/Solution.cs
+++ AdventOfCode/Solutions/Year2024/Day19/Solution.cs
@@ -11,23 +11,24 @@ namespace AdventOfCode.Solutions.Year2024
 
     class Day19 : ASolution
     {
-        Dictionary<string, ulong> combinations = [];
         string[] desired = [];
+        readonly HashSet<string> allPatterns = [];
 
         Regex patterns = new("^$");
+        int maxLength;
 
         public Day19() : base(19, 2024, "Linen Layout")
         {
-            DebugInput = @"r, wr, b, g, bwu, rb, gb, br
+            // DebugInput = @"r, wr, b, g, bwu, rb, gb, br
 
-            brwrr
-            bggr
-            gbbr
-            rrbgbr
-            ubwu
-            bwurrg
-            brgr
-            bbrgwb";
+            // brwrr
+            // bggr
+            // gbbr
+            // rrbgbr
+            // ubwu
+            // bwurrg
+            // brgr
+            // bbrgwb";
         }
 
         protected override string? SolvePartOne()
@@ -50,57 +51,32 @@ namespace AdventOfCode.Solutions.Year2024
                 // Combine validPatterns with the other patterns
                 patterns = new($"^({string.Join('|', tempPatterns[1..])})+$");
 
+                allPatterns.Add(tempPatterns[0]);
+
                 // If we can match tempPatterns[0], then it is a duplicate
                 if (!patterns.IsMatch(tempPatterns[0]))
                 {
                     // Found a valid option
-                    validPatterns.Add(tempPatterns[0]);
-
                     // Only one way to make this
-                    combinations[tempPatterns[0]] = 1;
-                }
-                else
-                {
-                    // Work down the tempPatterns array to determine how many ways this can be made
-                    HashSet<string> variations = [];
-
-                    for (int i = 1; i < tempPatterns.Count; i++)
-                    {
-                        Regex shortPattern = new($"^(({string.Join(")|(", tempPatterns[i..])}))+$");
-
-                        var matches = shortPattern.Match(tempPatterns[0]);
-
-                        // No more matching, exit early
-                        if (!matches.Success)
-                            break;
-
-                        // Otherwise let's add this to the variations list
-                        var str = "";
-
-                        foreach (Group grp in matches.Groups.Values.Skip(1))
-                            if (!string.IsNullOrEmpty(grp.Value))
-                                str += $"|{grp.Value}";
-
-                        variations.Add(str);
-                    }
-
-                    // Add one for the duplicate
-                    combinations[tempPatterns[0]] = (ulong)variations.Count + 1;
+                    validPatterns.Add(tempPatterns[0]);
                 }
 
                 tempPatterns.RemoveAt(0);
             }
 
             // Add the last one on
-            combinations[tempPatterns[0]] = 1;
+            allPatterns.Add(tempPatterns[0]);
             validPatterns.Add(tempPatterns[0]);
+
+            // For Part 2, we need the maxLength
+            maxLength = allPatterns.Max(key => key.Length);
 
             // Regenerate
             // Include the duplicates in here (change from original Part 1 code)
             patterns = new($"^({string.Join('|', validPatterns)})+$");
 
-            // All the time is in the constructor
             // Time: 00:00:00.1188499
+            // Time with P2, moving procesing code locally: 00:00:00.2104896
             return desired.Count(patterns.IsMatch).ToString();
         }
 
@@ -111,39 +87,41 @@ namespace AdventOfCode.Solutions.Year2024
                 return 0;
             }
 
-            if (combinations.TryGetValue(str, out ulong val))
-            {
-                // Console.WriteLine($"*{str}: {val}");
-                return val;
-            }
-
-            ulong count = 0;
-
             // Need to go through the permutations
-            // Since we know some of the longer strings, start with those and see if we can shortcut a bit
-            foreach(var key in combinations.Keys.OrderByDescending(key => key.Length).ThenBy(key => key))
-            {
-                // On the first match of the longest one possible, find the math for the rest of the string
-                if (str.StartsWith(key))
-                {
-                    // We canot have str == key here because that would have been caught earlier
-                    count = combinations[key] * CountCombinations(str[key.Length..]);
+            // Took a hint from one of the subreddit solutions
+            // which was similar to my second attempt to subdivide the strings
+            var counts = new Dictionary<int, ulong>() { [0] = 1 };
 
-                    // Found a match!
-                    break;
+            // i goes from 1 to str.Length because it is our ending offset, not an index
+            for (int i = 1; i <= str.Length; i++)
+            {
+                if (!counts.ContainsKey(i))
+                    counts[i] = 0;
+
+                var start = Math.Max(0, i - maxLength);
+                var end = start + i;
+
+                // Go from i-maxLength to maxLength offset
+                for (int q = start; q < end && q < i; q++)
+                {
+                    if (!counts.ContainsKey(q))
+                        counts[q] = 0;
+
+                    // If this combination is a valid pattern, then count it
+                    // We add count[q] because we're multiplying each step
+                    if (allPatterns.Contains(str[q..i]))
+                    {
+                        counts[i] += counts[q];
+                    }
                 }
             }
 
-            Console.WriteLine($"{str}: {count}");
-
-            // Cache for later
-            // combinations[str] = count;
-
-            return count;
+            return counts[counts.Count - 1];
         }
 
         protected override string? SolvePartTwo()
         {
+            // Time: 00:00:00.1384304
             return desired
                 .Where(pattern => patterns.IsMatch(pattern))
                 .Sum(pattern => CountCombinations(pattern))
