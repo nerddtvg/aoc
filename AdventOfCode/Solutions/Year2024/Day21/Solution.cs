16329122d6de3500f6938958bb134387f15bfade
Drew <nerddtvg@users.noreply.github.com>
2024-12-22 16:20:45 -0600

    2024 Day 21 Progress

--- AdventOfCode/Solutions/Year2024/Day21/Solution.cs
+++ AdventOfCode/Solutions/Year2024/Day21/Solution.cs
@@ -11,15 +11,329 @@ namespace AdventOfCode.Solutions.Year2024
 
     class Day21 : ASolution
     {
-
-        public Day21() : base(21, 2024, "")
+        #region Button Paths
+        // Hardcoding directions into to make it easier
+        Dictionary<char, HashSet<char>> cantMoveDirectional = new()
         {
+            ['^'] = ['<'],
+            ['A'] = [],
+            ['<'] = ['^'],
+            ['v'] = [],
+            ['>'] = [],
+        };
+        Dictionary<char, HashSet<char>> cantMoveNumerical = new()
+        {
+            ['7'] = [],
+            ['8'] = [],
+            ['9'] = [],
+            ['4'] = [],
+            ['5'] = [],
+            ['6'] = [],
+            ['1'] = ['v'],
+            ['2'] = [],
+            ['3'] = [],
+            ['0'] = ['<'],
+            ['A'] = [],
+        };
+        Dictionary<char, Dictionary<char, string>> distDirectional = new()
+        {
+            ['^'] = new()
+            {
+                ['^'] = "A",
+                ['A'] = ">A",
+                ['<'] = "<A",
+                ['v'] = "vA",
+                ['>'] = "v>A",
+            },
+            ['A'] = new()
+            {
+                ['^'] = "<A",
+                ['A'] = "A",
+                ['<'] = "<v<A",
+                ['v'] = "<vA",
+                ['>'] = "vA",
+            },
+            ['<'] = new()
+            {
+                ['^'] = ">^A",
+                ['A'] = ">>^A",
+                ['<'] = "A",
+                ['v'] = ">A",
+                ['>'] = ">>A",
+            },
+            ['v'] = new()
+            {
+                ['^'] = "^A",
+                ['A'] = ">^A",
+                ['<'] = "<A",
+                ['v'] = "A",
+                ['>'] = ">A",
+            },
+            ['>'] = new()
+            {
+                ['^'] = "<^A",
+                ['A'] = "^A",
+                ['<'] = "<<A",
+                ['v'] = "<A",
+                ['>'] = "A",
+            }
+        };
 
+        Dictionary<char, Dictionary<char, string>> distNumeric = new()
+        {
+            ['7'] = new()
+            {
+                ['7'] = "A",
+                ['8'] = ">A",
+                ['9'] = ">>A",
+
+                ['4'] = "vA",
+                ['5'] = "v>A",
+                ['6'] = "v>>A",
+
+                ['1'] = "vvA",
+                ['2'] = "vv>A",
+                ['3'] = "vv>>A",
+
+                ['0'] = ">vvvA",
+                ['A'] = ">>vvvA"
+            },
+            ['8'] = new()
+            {
+                ['7'] = "<A",
+                ['8'] = "A",
+                ['9'] = ">A",
+
+                ['4'] = "v<A",
+                ['5'] = "vA",
+                ['6'] = "v>A",
+
+                ['1'] = "vv<A",
+                ['2'] = "vvA",
+                ['3'] = "vv>A",
+
+                ['0'] = "vvvA",
+                ['A'] = "vvv>A"
+            },
+            ['9'] = new()
+            {
+                ['7'] = "<<A",
+                ['8'] = "<A",
+                ['9'] = "A",
+
+                ['4'] = "v<<A",
+                ['5'] = "v<A",
+                ['6'] = "vA",
+
+                ['1'] = "vv<<A",
+                ['2'] = "vv<A",
+                ['3'] = "vvA",
+
+                ['0'] = "vvv<A",
+                ['A'] = "vvvA"
+            },
+
+            ['4'] = new()
+            {
+                ['7'] = "^A",
+                ['8'] = "^>A",
+                ['9'] = "^>>A",
+
+                ['4'] = "AA",
+                ['5'] = ">A",
+                ['6'] = ">>A",
+
+                ['1'] = "vA",
+                ['2'] = "v>A",
+                ['3'] = "v>>A",
+
+                ['0'] = ">vvA",
+                ['A'] = ">>vvA"
+            },
+            ['5'] = new()
+            {
+                ['7'] = "^<A",
+                ['8'] = "^A",
+                ['9'] = "^>A",
+
+                ['4'] = "<A",
+                ['5'] = "A",
+                ['6'] = ">A",
+
+                ['1'] = "v<A",
+                ['2'] = "vA",
+                ['3'] = "v>A",
+
+                ['0'] = "vvA",
+                ['A'] = "vv>A"
+            },
+            ['6'] = new()
+            {
+                ['7'] = "^<<A",
+                ['8'] = "^<A",
+                ['9'] = "^A",
+
+                ['4'] = "<<A",
+                ['5'] = "<A",
+                ['6'] = "A",
+
+                ['1'] = "v<<A",
+                ['2'] = "v<A",
+                ['3'] = "vA",
+
+                ['0'] = "vv<A",
+                ['A'] = "vvA"
+            },
+
+            ['1'] = new()
+            {
+                ['7'] = "^^A",
+                ['8'] = "^^>A",
+                ['9'] = "^^>>A",
+
+                ['4'] = "^A",
+                ['5'] = "^>A",
+                ['6'] = "^>>A",
+
+                ['1'] = "A",
+                ['2'] = ">A",
+                ['3'] = ">>A",
+
+                ['0'] = ">vA",
+                ['A'] = ">>vA"
+            },
+            ['2'] = new()
+            {
+                ['7'] = "^^<A",
+                ['8'] = "^^A",
+                ['9'] = "^^>A",
+
+                ['4'] = "^<A",
+                ['5'] = "^A",
+                ['6'] = "^>A",
+
+                ['1'] = "<A",
+                ['2'] = "A",
+                ['3'] = ">A",
+
+                ['0'] = "vA",
+                ['A'] = "v>A"
+            },
+            ['3'] = new()
+            {
+                ['7'] = "^^<<A",
+                ['8'] = "^^<A",
+                ['9'] = "^^A",
+
+                ['4'] = "^<<A",
+                ['5'] = "^<A",
+                ['6'] = "^A",
+
+                ['1'] = "<<A",
+                ['2'] = "<A",
+                ['3'] = "A",
+
+                ['0'] = "v<A",
+                ['A'] = "vA"
+            },
+
+            ['0'] = new()
+            {
+                ['7'] = "^^^<A",
+                ['8'] = "^^^A",
+                ['9'] = "^^^>A",
+
+                ['4'] = "^^<A",
+                ['5'] = "^^A",
+                ['6'] = "^^>A",
+
+                ['1'] = "^<A",
+                ['2'] = "^A",
+                ['3'] = "^>A",
+
+                ['0'] = "A",
+                ['A'] = ">A"
+            },
+            ['A'] = new()
+            {
+                ['7'] = "^^^<<A",
+                ['8'] = "^^^<A",
+                ['9'] = "^^^A",
+
+                ['4'] = "^^<<A",
+                ['5'] = "^^<A",
+                ['6'] = "^^A",
+
+                ['1'] = "^<<A",
+                ['2'] = "^<A",
+                ['3'] = "^A",
+
+                ['0'] = "<A",
+                ['A'] = "A"
+            }
+        };
+        #endregion
+
+        char[] bots = [];
+
+        public Day21() : base(21, 2024, "Keypad Conundrum")
+        {
+            bots = ['A', 'A', 'A'];
+
+            DebugInput = @"029A
+            980A
+            179A
+            456A
+            379A";
+        }
+
+        string GetDigitPattern(string code)
+        {
+            char[] bots = ['A', 'A', 'A'];
+            var pattern = string.Empty;
+
+            // bot[0] needs to push the values of the code
+            // bot[1] directs bot[0]
+            // bot[2] directs bot[1]
+            // user directs bot[3]
+            // We need bots[0] to move from bots[0] value to each char in code
+            // The directions are: distNumeric[bots[0]][c]
+            foreach (var c in code)
+            {
+                // Need to move from bots[0] to c
+                foreach (var d in distNumeric[bots[0]][c])
+                {
+                    // Need to move from bots[1] to d
+                    foreach (var e in distDirectional[bots[1]][d])
+                    {
+                        // Need to move from bots[2] to e
+                        pattern += distDirectional[bots[2]][e];
+
+                        bots[2] = e;
+                    }
+
+                    bots[1] = d;
+                }
+
+                bots[0] = c;
+            }
+
+            return pattern;
         }
 
         protected override string? SolvePartOne()
         {
-            return string.Empty;
+            int total = 0;
+
+            foreach (var code in Input.SplitByNewline(shouldTrim: true))
+            {
+                var numericCode = int.Parse(code.Split("A")[0]);
+                var pattern = GetDigitPattern(code);
+
+                total += numericCode * pattern.Length;
+            }
+
+            return total.ToString();
         }
 
         protected override string? SolvePartTwo()
