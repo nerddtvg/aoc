6ef192e23ef719df882e2d4c8d22c8e5af81d54e
Drew <nerddtvg@users.noreply.github.com>
2024-12-23 10:03:35 -0600

    2024 Day 23 Part 1

--- AdventOfCode/Solutions/Year2024/Day23/Solution.cs
+++ AdventOfCode/Solutions/Year2024/Day23/Solution.cs
@@ -11,15 +11,83 @@ namespace AdventOfCode.Solutions.Year2024
 
     class Day23 : ASolution
     {
+        readonly List<HashSet<string>> connected = [];
 
-        public Day23() : base(23, 2024, "")
+        public Day23() : base(23, 2024, "LAN Party")
         {
-
+            // DebugInput = @"kh-tc
+            // qp-kh
+            // de-cg
+            // ka-co
+            // yn-aq
+            // qp-ub
+            // cg-tb
+            // vc-aq
+            // tb-ka
+            // wh-tc
+            // yn-cg
+            // kh-ub
+            // ta-co
+            // de-co
+            // tc-td
+            // tb-wq
+            // wh-td
+            // ta-ka
+            // td-qp
+            // aq-cg
+            // wq-ub
+            // ub-vc
+            // de-ta
+            // wq-aq
+            // wq-vc
+            // wh-yn
+            // ka-de
+            // kh-ta
+            // co-tc
+            // wh-qp
+            // tb-vc
+            // td-yn";
         }
 
         protected override string? SolvePartOne()
         {
-            return string.Empty;
+            Dictionary<string, List<string>> connections = [];
+
+            foreach(var line in Input.SplitByNewline(shouldTrim: true))
+            {
+                var split = line.Split('-');
+
+                if (connections.TryGetValue(split[0], out var c))
+                    c.Add(split[1]);
+                else
+                    connections[split[0]] = [split[1]];
+
+                if (connections.TryGetValue(split[1], out var c2))
+                    c2.Add(split[0]);
+                else
+                    connections[split[1]] = [split[0]];
+            }
+
+            // For each computer that starts with t, look for two connected computers
+            var sets = new HashSet<string>();
+            foreach(var tComputerKey in connections.Keys.Where(k => k[0] == 't'))
+            {
+                var tComputer = connections[tComputerKey];
+
+                for(int tA=0; tA<tComputer.Count - 1; tA++)
+                {
+                    for (int tB = tA+1; tB < tComputer.Count; tB++)
+                    {
+                        // If tA appears in tB's list, we have a cluster
+                        if (connections[tComputer[tA]].Contains(tComputer[tB]))
+                        {
+                            sets.Add(string.Join(",", new string[] { tComputerKey, tComputer[tA], tComputer[tB] }.OrderBy(v => v)));
+                        }
+                    }
+                }
+            }
+
+            return sets.Count.ToString();
         }
 
         protected override string? SolvePartTwo()
