0b05381f090f447de0d428ac1f36578ea4d4e014
Drew <nerddtvg@users.noreply.github.com>
2024-12-26 19:22:50 -0600

    2024 Day 24 Part 2

--- AdventOfCode/Solutions/Year2024/Day24/Solution.cs
+++ AdventOfCode/Solutions/Year2024/Day24/Solution.cs
@@ -131,6 +131,7 @@ namespace AdventOfCode.Solutions.Year2024
                 LoopOperation(operation);
             }
 
+            // Time: 00:00:00.2240480
             return Convert.ToInt64(values.Keys.Where(k => k[0] == 'z').OrderByDescending(k => k).Select(k => values[k] ? '1' : '0').JoinAsString(), 2).ToString();
         }
 
@@ -142,50 +143,99 @@ namespace AdventOfCode.Solutions.Year2024
             // Let's search if any don't match
             HashSet<string> swapped = [];
 
-            foreach((char op, string val1, string val2, string dest) in operations)
+            // Binary adder: https://www.electronics-lab.com/article/binary-adder/
+
+            foreach ((char op, string val1, string val2, string dest) in operations)
             {
-                switch(op)
+                // Graphviz helper output
+                // Console.WriteLine($"{op}_{dest} [label=\"{(op == 'A' ? "AND" : op == 'X' ? "XOR" : "OR")}\"]");
+                // Console.WriteLine($"{val1} -> {op}_{dest}");
+                // Console.WriteLine($"{val2} -> {op}_{dest}");
+                // Console.WriteLine($"{op}_{dest} -> {dest}");
+
+                switch (op)
                 {
                     case 'A':
                         // Feeds an output, invalid
                         if (dest[0] == 'z')
                             swapped.Add(dest);
+                        // Feeds AND with x00/y00 as inputs, valid
+                        else if (operations.Where(top => top.val1 == dest || top.val2 == dest).All(top => top.op == 'X' || (top.op == 'A' && (val1 == "x00" || val1 == "y00"))))
+                            break;
                         // Feeds non-OR, invalid
                         else if (operations.Any(top => top.op != 'O' && (top.val1 == dest || top.val2 == dest)))
                             swapped.Add(dest);
                         break;
 
                     case 'X':
-                    case 'O':
-                        // X Feeds an output, valid
-                        // O Feeds an output, invalid
-                        if (dest[0] == 'z')
                         {
-                            // Only invalid if not the last one
-                            if (op == 'O' && dest != "z45")
+                            // XOR: x/y => XOR => var => XOR (or x00/y00 => z00)
+                            // XOR: a/b => XOR => output
+                            if (dest == "z00" && ((val1 == "x00" || val2 == "y00") ^ (val1 == "y00" || val2 == "x00")))
+                                break;
+
+                            // If XOR feeds into OR at all, it's invalid
+                            if (operations.Any(top => top.op == 'O' && (top.val1 == dest || top.val2 == dest)))
+                            {
+                                swapped.Add(dest);
+                                break;
+                            }
+
+                            var sameFeeds = operations.Where(top => (top.val1 == val1 || top.val2 == val2) ^ (top.val1 == val2 || top.val2 == val1)).ToArray();
+
+                            if ((val1[0] == 'x' && val2[0] == 'y') ^ (val1[0] == 'y' && val2[0] == 'x'))
+                            {
+                                // sameFeeds should be 2, one is AND
+                                if (sameFeeds.Count(feed => feed.op == 'X') != 1 || sameFeeds.Count(feed => feed.op == 'A') != 1)
+                                    swapped.Add(dest);
+
+                                // If it is going to output, it's invalid
+                                if (dest[0] == 'z')
+                                    swapped.Add(dest);
+                            }
+                            else
+                            {
+                                // This is an output XOR
+                                if (dest[0] != 'z')
+                                    swapped.Add(dest);
+                            }
+
+                            // If a/b are not x/y inputs, then expect an output
+
+                            break;
+                        }
+                    case 'O':
+                        {
+                            // O Feeds an output, invalid
+                            if (dest[0] == 'z')
+                            {
+                                // Only invalid if not the last one
+                                if (op == 'O' && dest != "z45")
+                                    swapped.Add(dest);
+
+                                break;
+                            }
+
+                            // XOR should feed an output if the same two inputs are used in AND operations
+                            var feeds = operations.Where(top => top.val1 == dest || top.val2 == dest).ToArray();
+
+                            // Feeds OR, invalid
+                            if (feeds.Any(feed => feed.op == 'O'))
+                            {
+                                swapped.Add(dest);
+                                break;
+                            }
+
+                            // Must feed a single pair of XOR and AND
+                            if (feeds.Count(feed => feed.op == 'X') != 1 || feeds.Count(feed => feed.op == 'A') != 1)
                                 swapped.Add(dest);
 
                             break;
                         }
-
-                        var feeds = operations.Where(top => top.val1 == dest || top.val2 == dest).ToArray();
-
-                        // Feeds OR, invalid
-                        if (feeds.Any(feed => feed.op == 'O'))
-                        {
-                            swapped.Add(dest);
-                            break;
-                        }
-
-                        // Must feed a single pair of XOR and AND
-                        if (feeds.Count(feed => feed.op == 'X') != 1 || feeds.Count(feed => feed.op == 'A') != 1)
-                            swapped.Add(dest);
-
-                        break;
                 }
             }
 
-            // Part 2: jgb,mjh,rkf,rrs,rvc,z09,z20,z24
+            // Time: 00:00:00.1228360
             return string.Join(",", swapped.OrderBy(k => k));
         }
     }
