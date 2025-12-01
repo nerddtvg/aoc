220a8aeaecba0817a6793afdc1925b959e09b874
Drew <nerddtvg@users.noreply.github.com>
2025-12-01 08:03:51 -0600

    2025 Day 01: Part 1

--- AdventOfCode/Solutions/Year2025/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2025/Day01/Solution.cs
@@ -12,14 +12,42 @@ namespace AdventOfCode.Solutions.Year2025
     class Day01 : ASolution
     {
 
-        public Day01() : base(01, 2025, "")
+        public Day01() : base(01, 2025, "Secret Entrance")
         {
-
+            // DebugInput = @"
+            //     L68
+            //     L30
+            //     R48
+            //     L5
+            //     R60
+            //     L55
+            //     L1
+            //     L99
+            //     R14
+            //     L82";
         }
 
         protected override string? SolvePartOne()
         {
-            return string.Empty;
+            int position = 50;
+            int password = 0;
+
+            Input.SplitByNewline(true).ForEach(line =>
+            {
+                position += (line[0] == 'L' ? -1 : 1) * int.Parse(line[1..]);
+
+                // Change negative to positive
+                // Because % is not modulo in C#, we must operate with positive numbers only
+                while (position < 0)
+                    position += 100;
+
+                position %= 100;
+
+                if (position == 0)
+                    password++;
+            });
+
+            return password.ToString();
         }
 
         protected override string? SolvePartTwo()
