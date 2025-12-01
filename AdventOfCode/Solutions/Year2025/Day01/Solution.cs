51c580db0f2b267f79cee0b4c83abc4e4eb0cdb0
Drew <nerddtvg@users.noreply.github.com>
2025-12-01 13:02:20 -0600

    2025 Day 01: Part 2

--- AdventOfCode/Solutions/Year2025/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2025/Day01/Solution.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 
 using System.Linq;
+using System.Numerics;
 
 
 namespace AdventOfCode.Solutions.Year2025
@@ -11,6 +12,7 @@ namespace AdventOfCode.Solutions.Year2025
 
     class Day01 : ASolution
     {
+        const int dialMax = 100;
 
         public Day01() : base(01, 2025, "Secret Entrance")
         {
@@ -25,6 +27,16 @@ namespace AdventOfCode.Solutions.Year2025
             //     L99
             //     R14
             //     L82";
+
+            // DebugInput = "L1000";
+
+            // DebugInput = "L1000\nL50";
+
+            // DebugInput = "R1000\nR50";
+
+            // DebugInput = "L50\nR101";
+
+            // DebugInput = "R50\nL101";
         }
 
         protected override string? SolvePartOne()
@@ -36,12 +48,8 @@ namespace AdventOfCode.Solutions.Year2025
             {
                 position += (line[0] == 'L' ? -1 : 1) * int.Parse(line[1..]);
 
-                // Change negative to positive
                 // Because % is not modulo in C#, we must operate with positive numbers only
-                while (position < 0)
-                    position += 100;
-
-                position %= 100;
+                position = (position % dialMax + dialMax) % dialMax;
 
                 if (position == 0)
                     password++;
@@ -52,8 +60,27 @@ namespace AdventOfCode.Solutions.Year2025
 
         protected override string? SolvePartTwo()
         {
-            return string.Empty;
+            int position = 50;
+            int password = 0;
+
+            Input.SplitByNewline(true).ForEach(line =>
+            {
+                var oldPosition = position;
+                position += (line[0] == 'L' ? -1 : 1) * int.Parse(line[1..]);
+
+                // Part 2: Count the number of times we pass 0 / 100
+                if (position >= dialMax)
+                    password += Math.Abs(position / dialMax);
+                else if (position < 0)
+                    // For negative, we count if we passed 0 on this turn but only if we started above zero
+                    password += Math.Abs(position / dialMax) + (oldPosition > 0 ? 1 : 0);
+                else if (position == 0)
+                    password++;
+
+                position = (position % dialMax + dialMax) % dialMax;
+            });
+
+            return password.ToString();
         }
     }
 }
-
