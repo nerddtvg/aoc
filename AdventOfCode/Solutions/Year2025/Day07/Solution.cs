34e5d9ef5f315d7f56d568ad2864dea949901591
Drew <nerddtvg@users.noreply.github.com>
2025-12-07 12:24:31 -0600

    2025 Day 07: Part 1

--- AdventOfCode/Solutions/Year2025/Day07/Solution.cs
+++ AdventOfCode/Solutions/Year2025/Day07/Solution.cs
@@ -11,15 +11,74 @@ namespace AdventOfCode.Solutions.Year2025
 
     class Day07 : ASolution
     {
+        private string[] grid;
 
-        public Day07() : base(07, 2025, "")
+        public Day07() : base(07, 2025, "Laboratories")
         {
+            // DebugInput = @"
+            //     .......S.......
+            //     ...............
+            //     .......^.......
+            //     ...............
+            //     ......^.^......
+            //     ...............
+            //     .....^.^.^.....
+            //     ...............
+            //     ....^.^...^....
+            //     ...............
+            //     ...^.^...^.^...
+            //     ...............
+            //     ..^...^.....^..
+            //     ...............
+            //     .^.^.^.^.^...^.
+            //     ...............
+            //     ";
 
+            grid = Input.SplitByNewline(true);
         }
 
         protected override string? SolvePartOne()
         {
-            return string.Empty;
+            var active = new HashSet<int>([grid[0].IndexOf('S')]);
+            var splitCount = 0;
+
+            foreach (var line in grid.Skip(1))
+            {
+                var newActive = new HashSet<int>();
+
+                // We track the 'active' beams traveling down
+                // We remove any that hit splitters
+                // We add any that need to be added
+                // Ensure we don't double count
+                line.ForEach((c, idx) =>
+                {
+                    // If this index is active
+                    if (active.Contains(idx))
+                    {
+                        // Did we get a splitter?
+                        if (c == '^')
+                        {
+                            // Count this splitter
+                            splitCount++;
+
+                            // If this splitter position was 'active' above, stop and start new
+                            // Overflows don't matter since we are not using these in a loop
+                            newActive.Add(idx - 1);
+                            newActive.Add(idx + 1);
+                        }
+                        else
+                        {
+                            // This continues as-is
+                            newActive.Add(idx);
+                        }
+                    }
+                });
+
+                active = newActive;
+            }
+
+            // Time  : 00:00:00.0032827
+            return splitCount.ToString();
         }
 
         protected override string? SolvePartTwo()
