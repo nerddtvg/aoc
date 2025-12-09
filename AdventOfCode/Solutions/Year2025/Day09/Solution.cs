bcbad1282f26c8b6895e4ba48c69a3b419e7d9f8
Drew <nerddtvg@users.noreply.github.com>
2025-12-09 07:17:09 -0600

    2025 Day 09: Part 1

--- AdventOfCode/Solutions/Year2025/Day09/Solution.cs
+++ AdventOfCode/Solutions/Year2025/Day09/Solution.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 
 using System.Linq;
+using System.Numerics;
 
 
 namespace AdventOfCode.Solutions.Year2025
@@ -11,15 +12,37 @@ namespace AdventOfCode.Solutions.Year2025
 
     class Day09 : ASolution
     {
+        private readonly HashSet<(int a, int b)> points;
 
-        public Day09() : base(09, 2025, "")
+        public Day09() : base(09, 2025, "Movie Theater")
         {
+            // DebugInput = @"7,1
+            //     11,1
+            //     11,7
+            //     9,7
+            //     9,5
+            //     2,5
+            //     2,3
+            //     7,3";
 
+            points = [.. Input.SplitByNewline(true, true)
+                .Select(line =>
+                {
+                    var t = line.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(int.Parse).ToArray();
+                    return (t[0], t[1]);
+                })];
         }
 
         protected override string? SolvePartOne()
         {
-            return string.Empty;
+            return points
+                .GetAllCombos(2)
+                .Select(combo => {
+                    var pair = combo.ToArray();
+                    return (pair, area: (BigInteger.Abs(pair[0].a - pair[1].a) + 1) * (BigInteger.Abs(pair[0].b - pair[1].b) + 1));
+                })
+                .Max(itm => itm.area)
+                .ToString();
         }
 
         protected override string? SolvePartTwo()
